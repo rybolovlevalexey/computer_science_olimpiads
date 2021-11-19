3daea167fadd4712816866c6330d1b2c60e7d560
Rybolovlev Alexey <rybolovlevalexey@yandex.ru>
2021-11-19 11:13:35 +0500

    Задача 2 решена успешно

--- ВсОШ муниципальный этап/Задача 1.py
+++ ВсОШ муниципальный этап/Задача 1.py
@@ -3,14 +3,13 @@ b = int(input())
 c = int(input())
 d = int(input())
 e = int(input())
+sp = [a, b, c, d]
 if a == b == c == d == e:
     print(4)
 else:
-    cnt = 0
-    box_size = [e, e, e]
-    boxes = list(reversed([a, b, c, d]))
-    while len(boxes) > 0:
-        delet_index = list()
-        for elem in boxes:
-            if box_size == [e, e, e]:
-                pass
+    answer = 0
+    if e in sp:
+        answer += sp.count(e)
+        while e in sp:
+            sp.remove(e)
+    print(sp, answer)
