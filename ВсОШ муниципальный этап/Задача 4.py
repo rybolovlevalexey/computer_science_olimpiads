f64331c8b30a77790361c1e6b8c527a5ab906a23
Rybolovlev Alexey <rybolovlevalexey@yandex.ru>
2021-11-19 12:58:37 +0500

    Задача 4 работает на 70 баллов

---
+++ ВсОШ муниципальный этап/Задача 4.py
@@ -0,0 +1,24 @@
+n = int(input())  # количество поселений
+posel = list(map(int, input().split()))  # вместимость поселений
+m = int(input())  # количество рейсов
+for _ in range(m):
+    rashod = 0
+    x, k = map(int, input().split())  # номер поселения, количество колонистов на посадку
+    x -= 1
+    while k > 0:
+        can_take = posel[x]
+        if can_take == 0:
+            x += 1
+            rashod += k
+        elif can_take < k:
+            posel[x] = 0
+            k -= can_take
+            x += 1
+            rashod += k
+        elif can_take >= k:
+            posel[x] -= k
+            k = 0
+            break
+        if x == n:
+            x = 0
+    print(rashod)
