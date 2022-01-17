85ad4cac89d98bb323d0ac6c0055ee76ca5d57f5
Rybolovlev Alexey <rybolovlevalexey@yandex.ru>
2022-01-17 21:17:45 +0500

    норм

---
+++ подготовка к егэ/динамическое программирование/задание номер 1.py
@@ -0,0 +1,26 @@
+dp = [0] * 39
+dp[4] = 1
+for i in range(5, 12):
+    if i % 6 == 0:
+        dp[i] = dp[i - 1] + dp[i // 2] + dp[i // 3]
+    elif i % 2 == 0:
+        dp[i] = dp[i - 1] + dp[i // 2]
+    elif i % 3 == 0:
+        dp[i] = dp[i - 1] + dp[i // 3]
+    else:
+        dp[i] = dp[i - 1]
+for i in range(11):
+    dp[i] = 0
+print(dp)
+for i in range(12, 39):
+    if i == 18:
+        continue
+    if i % 6 == 0:
+        dp[i] = dp[i - 1] + dp[i // 2] + dp[i // 3]
+    elif i % 2 == 0:
+        dp[i] = dp[i - 1] + dp[i // 2]
+    elif i % 3 == 0:
+        dp[i] = dp[i - 1] + dp[i // 3]
+    else:
+        dp[i] = dp[i - 1]
+print(dp)
