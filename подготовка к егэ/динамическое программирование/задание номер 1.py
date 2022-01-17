4b43dae48d1f0b318f74bf82754c38b8101268ec
Rybolovlev Alexey <rybolovlevalexey@yandex.ru>
2022-01-17 21:33:52 +0500

    норм

--- подготовка к егэ/динамическое программирование/задание номер 1.py
+++ подготовка к егэ/динамическое программирование/задание номер 1.py
@@ -1,26 +1,11 @@
-dp = [0] * 35
-dp[4] = 1
-for i in range(5, 11):
+dp = [0] * 90
+for i in range(4, 90):
     if i % 6 == 0:
-        dp[i] = dp[i - 1] + dp[i // 2] + dp[i // 3]
-    elif i % 2 == 0:
-        dp[i] = dp[i - 1] + dp[i // 2]
+        dp[i] = min(dp[i - 1], dp[i // 2], dp[i // 3]) + 1
     elif i % 3 == 0:
-        dp[i] = dp[i - 1] + dp[i // 3]
-    else:
-        dp[i] = dp[i - 1]
-for i in range(10):
-    dp[i] = 0
-print(dp)
-for i in range(11, 35):
-    if i == 18:
-        continue
-    if i % 6 == 0:
-        dp[i] = dp[i - 1] + dp[i // 2] + dp[i // 3]
+        dp[i] = min(dp[i - 1], dp[i // 3]) + 1
     elif i % 2 == 0:
-        dp[i] = dp[i - 1] + dp[i // 2]
-    elif i % 3 == 0:
-        dp[i] = dp[i - 1] + dp[i // 3]
+        dp[i] = min(dp[i - 1], dp[i // 2]) + 1
     else:
-        dp[i] = dp[i - 1]
+        dp[i] = dp[i - 1] + 1
 print(dp)
