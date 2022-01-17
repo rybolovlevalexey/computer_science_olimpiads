644adff8addbcf0fbe86914f9c4d8d617c2e97fd
Rybolovlev Alexey <rybolovlevalexey@yandex.ru>
2022-01-17 21:57:18 +0500

    норм

--- подготовка к егэ/задание 23/задание номер 1.py
+++ подготовка к егэ/задание 23/задание номер 1.py
@@ -1,14 +1,27 @@
-dp = [0] * 10
-i = 2
-while True:
-    if (i - 1) % 2 == 0:
-        dp[i] = max(dp[i - 3], dp[(i - 1) // 2]) + 1
+dp = [0] * 39
+dp[3] = 1
+for i in range(4, 13):
+    if i % 6 == 0:
+        dp[i] = dp[i - 1] + dp[i // 2] + dp[i // 3]
+    elif i % 2 == 0:
+        dp[i] = dp[i - 1] + dp[i // 2]
+    elif i % 3 == 0:
+        dp[i] = dp[i - 1] + dp[i // 3]
     else:
-        dp[i] = dp[i - 3] + 1
-    dp.append(0)
-    if dp[i - 3] > 13 and dp[(i - 1) // 2] > 13:
-        break
-    i += 1
-print(len(dp))
+        dp[i] = dp[i - 1]
 print(dp)
-print(dp.count(13))
+for i in range(12):
+    dp[i] = 0
+print(dp)
+for i in range(13, 39):
+    if i == 30:
+        continue
+    if i % 6 == 0:
+        dp[i] = dp[i - 1] + dp[i // 2] + dp[i // 3]
+    elif i % 2 == 0:
+        dp[i] = dp[i - 1] + dp[i // 2]
+    elif i % 3 == 0:
+        dp[i] = dp[i - 1] + dp[i // 3]
+    else:
+        dp[i] = dp[i - 1]
+print(dp)
