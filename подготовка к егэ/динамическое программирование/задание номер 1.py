3c4202138e2d79e18604f3b1eeeccd2d28d4d9a4
Rybolovlev Alexey <rybolovlevalexey@yandex.ru>
2022-01-17 21:21:37 +0500

    норм

--- подготовка к егэ/динамическое программирование/задание номер 1.py
+++ подготовка к егэ/динамическое программирование/задание номер 1.py
@@ -1,6 +1,6 @@
-dp = [0] * 39
+dp = [0] * 35
 dp[4] = 1
-for i in range(5, 12):
+for i in range(5, 11):
     if i % 6 == 0:
         dp[i] = dp[i - 1] + dp[i // 2] + dp[i // 3]
     elif i % 2 == 0:
@@ -9,10 +9,10 @@ for i in range(5, 12):
         dp[i] = dp[i - 1] + dp[i // 3]
     else:
         dp[i] = dp[i - 1]
-for i in range(11):
+for i in range(10):
     dp[i] = 0
 print(dp)
-for i in range(12, 39):
+for i in range(11, 35):
     if i == 18:
         continue
     if i % 6 == 0:
