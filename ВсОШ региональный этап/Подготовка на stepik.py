a8df45384ae9c5a47e1e5501de0971d28380040a
Rybolovlev Alexey <rybolovlevalexey@yandex.ru>
2022-01-17 20:35:05 +0500

    норм

--- ВсОШ региональный этап/Подготовка на stepik.py
+++ ВсОШ региональный этап/Подготовка на stepik.py
@@ -1,6 +1,24 @@
-number = int(input())
-i1 = 0
-i2 = 1
-for i in range(number):
-    i1, i2 = i2 % 10, (i1 + i2) % 10
-print(i1)
+fib = {0: 0, 1: 1}
+def func(x):
+    if x == 0 or x == 1:
+        return x
+    elif x >= 2 and x in fib:
+        return fib[x]
+    else:
+        res = func(x - 1) + func(x + 2)
+        fib[x] = res
+        return res
+
+def fib_mod(n, m):
+    mch = func(m)
+    nch = func(n)
+
+
+
+def main():
+    n, m = map(int, input().split())
+    print(fib_mod(n, m))
+
+
+if __name__ == "__main__":
+    main()
