dbaeae67e08aa6803aa5cf9ef007669e46d8fa80
Rybolovlev Alexey <rybolovlevalexey@yandex.ru>
2022-01-23 12:51:30 +0500

    Задание 8, ответ 230

--- подготовка к егэ/задание 23/Поляков первые 10 номеров.py
+++ подготовка к егэ/задание 23/Поляков первые 10 номеров.py
@@ -1,10 +1,10 @@
 dp = [0] * 16
 dp[1] = 1
 for i in range(2, 16):
-    if i >= 3 and i % 2 == 0:
-        dp[i] = dp[i - 1] + dp[i - 3] + dp[i // 2]
-    elif i % 2 == 0:
-        dp[i] = dp[i - 1] + dp[i // 2]
+    if i >= 3 and i % 3 == 0:
+        dp[i] = dp[i - 1] + dp[i - 3] + dp[i // 3]
+    elif i % 3 == 0:
+        dp[i] = dp[i - 1] + dp[i // 3]
     elif i >= 3:
         dp[i] = dp[i - 1] + dp[i - 3]
     else:
