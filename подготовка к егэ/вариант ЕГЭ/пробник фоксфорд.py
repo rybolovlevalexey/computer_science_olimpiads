d272444ebe45d3d818879f3270502390599b2851
Rybolovlev Alexey <rybolovlevalexey@yandex.ru>
2022-01-26 21:08:00 +0500

    пятое задание

--- подготовка к егэ/вариант ЕГЭ/пробник фоксфорд.py
+++ подготовка к егэ/вариант ЕГЭ/пробник фоксфорд.py
@@ -1,8 +1,12 @@
-print('x y z w res')
-for x in [True, False]:
-    for y in [True, False]:
-        for z in [True, False]:
-            for w in [True, False]:
-                res = not w != x or z and not x or y and w and z
-                if res == False:
-                    print(int(x), int(y), int(z), int(w), int(res))
+for n in range(1000):
+    r = bin(n)[2:]
+    if n % 2 != 0:
+        r += '10'
+    else:
+        r += '01'
+    s = sum(map(int, list(r))) % 2
+    r += str(s)
+    r = int(r, 2)
+    if r > 147:
+        print(r)
+        break
