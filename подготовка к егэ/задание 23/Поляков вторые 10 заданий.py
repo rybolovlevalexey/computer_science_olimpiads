b6e3e4e2c2df7a899ea7cee60545d05336ddf236
Rybolovlev Alexey <rybolovlevalexey@yandex.ru>
2022-01-26 10:03:54 +0500

    Задание 14, ответ 2

--- подготовка к егэ/задание 23/Поляков вторые 10 заданий.py
+++ подготовка к егэ/задание 23/Поляков вторые 10 заданий.py
@@ -1,8 +1,10 @@
-dp = [0] * 50
+dp = [0] * 28
 dp[5] = 1
-for i in range(6, 50):
-    if i % 3 == 0:
-        dp[i] = dp[i - 1] + dp[i // 3]
-    else:
-        dp[i] = dp[i - 1]
+for i in range(6, 28):
+    if i % 3 == 0 and i - 3 >= 0:
+        dp[i] = dp[i - 3] + dp[i // 3]
+    elif i - 3 >= 0:
+        dp[i] = dp[i - 3]
+    elif i % 3 == 0:
+        dp[i] = dp[i // 3]
 print(dp)
