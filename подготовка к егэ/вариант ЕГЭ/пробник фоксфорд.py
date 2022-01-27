743c68725302f6d7157102d4cb7183ad4835516e
Rybolovlev Alexey <rybolovlevalexey@yandex.ru>
2022-01-27 20:39:51 +0500

    семнадцатое задание

--- подготовка к егэ/вариант ЕГЭ/пробник фоксфорд.py
+++ подготовка к егэ/вариант ЕГЭ/пробник фоксфорд.py
@@ -1,11 +1,13 @@
-def f(n):
-    if n == 1:
-        return 1
-    elif n == 2:
-        return 1
-    elif n % 2 == 0 and n > 2:
-        return n * f(n - 1) + 2
-    elif n % 2 != 0 and n > 2:
-        return 1 + f(n - 2) + f(n - 1)
-
-print(f(9))
+file = open('Zadanie-260232.txt', 'r')
+sp = file.read().strip().split('\n')
+sp = list(map(int, sp))
+cnt = 0
+maximum = None
+for i in range(len(sp) - 1):
+    i1 = sp[i]
+    i2 = sp[i + 1]
+    if (i1 + i2) % 3 == 0 and (i1 + i2) % 6 != 0:
+        cnt += 1
+        if maximum is None or (i1 + i2) > maximum:
+            maximum = i1 + i2
+print(cnt, maximum)
