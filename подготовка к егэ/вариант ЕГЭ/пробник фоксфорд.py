b4079ccac0934c1357e14ce78a35dd1f1a11d8b2
Rybolovlev Alexey <rybolovlevalexey@yandex.ru>
2022-01-27 20:28:03 +0500

    пятнадцатое задание

--- подготовка к егэ/вариант ЕГЭ/пробник фоксфорд.py
+++ подготовка к егэ/вариант ЕГЭ/пробник фоксфорд.py
@@ -1,8 +1,11 @@
-num = 3**75 + 27**17 + 9**6 - 17
-cnt = 0
-while num > 0:
-    ost = num % 3
-    num //= 3
-    if ost == 2:
-        cnt += 1
-print(cnt)
+for a in range(10000, 1, -1):
+    flag = True
+    for x in range(1, 10000):
+        if x % a == 0 or not x % 16 == 0 or not x % 24 == 0:
+            pass
+        else:
+            flag = False
+            break
+    if flag:
+        print(a)
+        break
