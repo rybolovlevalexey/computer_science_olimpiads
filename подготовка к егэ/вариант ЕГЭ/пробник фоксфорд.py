996abfa8ddb31a792855d619a02dfafc4245fc45
Rybolovlev Alexey <rybolovlevalexey@yandex.ru>
2022-01-27 21:00:24 +0500

    двадцать третье задание

--- подготовка к егэ/вариант ЕГЭ/пробник фоксфорд.py
+++ подготовка к егэ/вариант ЕГЭ/пробник фоксфорд.py
@@ -1,11 +1,14 @@
-for x in range(201, 10000):
-    l = 2 * x - 15
-    m = 2 * x + 30
-    while l != m:
-        if l > m:
-            l = l - m
-        else:
-            m = m - l
-    if m == 45:
-        print(x)
-        break
+dp = [0] * 23
+dp[5] = 1
+for i in range(6, 12):
+    dp[i] = dp[i - 1] + dp[i - 3] + dp[i - 4]
+print(dp)
+res = dp[11]
+dp[:11] = [0] * 11
+print(dp)
+for i in range(12, 23):
+    if i == 14:
+        continue
+    dp[i] = dp[i - 1] + dp[i - 3] + dp[i - 4]
+print(dp)
+print(dp[-1])
