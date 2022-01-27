ec559bac4888828e3fabb79cf9a058fc395c1ee0
Rybolovlev Alexey <rybolovlevalexey@yandex.ru>
2022-01-27 21:20:33 +0500

    двадцать пятое задание

--- подготовка к егэ/вариант ЕГЭ/пробник фоксфорд.py
+++ подготовка к егэ/вариант ЕГЭ/пробник фоксфорд.py
@@ -1,16 +1,12 @@
-file = open('Zadanie-260283.txt', 'r')
-st = file.readline().strip()
-sp = list()
-for i in range(len(st)):
-    if st[i] == 'F':
-        sp.append(i)
-i1 = 0
-i2 = sp[3]
-ans = None
-for i in range(len(sp) - 4):
-    i1 = sp[i]
-    i2 = sp[i + 4]
-    lenght = i2 - i1 - 1
-    if ans is None or lenght > ans:
-        ans = lenght
-print(ans)
+def is_prost(num):
+    if num % 2 == 0:
+        return False
+    for d in range(3, int(num**0.5), 2):
+        if num % d == 0:
+            return False
+    return True
+
+
+for elem in range(4542345, 4542419 + 1):
+    if is_prost(elem):
+        print(elem)
