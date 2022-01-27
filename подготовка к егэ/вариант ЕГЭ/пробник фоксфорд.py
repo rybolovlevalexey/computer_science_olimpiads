2fa26586821c50d033a283d88a0a4012c56c581b
Rybolovlev Alexey <rybolovlevalexey@yandex.ru>
2022-01-27 20:24:47 +0500

    четырнадцатое задание

--- подготовка к егэ/вариант ЕГЭ/пробник фоксфорд.py
+++ подготовка к егэ/вариант ЕГЭ/пробник фоксфорд.py
@@ -1,7 +1,8 @@
-st = '2' * 124 + '1' * 19
-while '222' in st or '111' in st:
-    if '222' in st:
-        st = st.replace('222', '1', 1)
-    if '111' in st:
-        st = st.replace('111', '2', 1)
-print(st)
+num = 3**75 + 27**17 + 9**6 - 17
+cnt = 0
+while num > 0:
+    ost = num % 3
+    num //= 3
+    if ost == 2:
+        cnt += 1
+print(cnt)
