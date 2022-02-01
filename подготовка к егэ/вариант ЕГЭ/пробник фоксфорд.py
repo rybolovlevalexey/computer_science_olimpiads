78d3864ad5c8c067987e3d61337571fd019eb8bf
Rybolovlev Alexey <rybolovlevalexey@yandex.ru>
2022-02-01 21:39:24 +0500

    двадцать шестое задание

--- подготовка к егэ/вариант ЕГЭ/пробник фоксфорд.py
+++ подготовка к егэ/вариант ЕГЭ/пробник фоксфорд.py
@@ -1,12 +1,22 @@
-def is_prost(num):
-    if num % 2 == 0:
-        return False
-    for d in range(3, int(num**0.5), 2):
-        if num % d == 0:
-            return False
-    return True
-
-
-for elem in range(4542345, 4542419 + 1):
-    if is_prost(elem):
-        print(elem)
+file = open('Zadanie-260361.txt', 'r')
+sp = file.read().strip().split('\n')
+s, n = map(int, sp[0].split())
+del sp[0]
+sp = list(sorted(map(int, sp)))
+sp = list(filter(lambda x: x % 3 == 0, sp))
+sp = sp[10:]
+s1 = 0
+i = 0
+m = 0
+while True:
+    if s1 + sp[i] + sp[i + 1] > s:
+        break
+    s1 += sp[i]
+    i += 1
+    m += 1
+print(len(sp), i)
+for j in range(i, len(sp)):
+    if s1 + sp[j] > s:
+        print(sp[j - 1])
+        break
+print(s1, m)
