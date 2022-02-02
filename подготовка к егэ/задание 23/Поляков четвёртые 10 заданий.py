fad701d642ad10748532fd3b4e4e87b8ef4a2511
Rybolovlev Alexey <rybolovlevalexey@yandex.ru>
2022-02-02 09:39:26 +0500

    Задание 32, Ответ 32

--- подготовка к егэ/задание 23/Поляков четвёртые 10 заданий.py
+++ подготовка к егэ/задание 23/Поляков четвёртые 10 заданий.py
@@ -1,10 +1,8 @@
-dp = [0] * 56
+dp = [0] * 21
 dp[1] = 1
-for i in range(2, 56):
-    if i - 2 >= 0 and i % 3 == 0:
-        dp[i] = dp[i - 2] + dp[i // 3]
-    elif i % 3 == 0:
-        dp[i] = dp[i // 3]
-    elif i - 2 >= 0:
-        dp[i] = dp[i - 2]
+for i in range(2, 21):
+    if i % 1.5 == 0:
+        dp[i] = dp[i - 1] + dp[int(i // 1.5)]
+    else:
+        dp[i] = dp[i - 1]
 print(dp)
