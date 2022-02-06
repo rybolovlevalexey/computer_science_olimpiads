d93514ac550eff3943c048d30245a45e2e273259
Rybolovlev Alexey <rybolovlevalexey@yandex.ru>
2022-02-06 10:12:44 +0500

    Добавлены обработчики кнопок

--- Проект в школу/first programm.py
+++ Проект в школу/first programm.py
@@ -1,16 +1,48 @@
 import tkinter as tk
 
+
+def handle_click1(event):
+    st = entry.get()
+    if len(st) == 0:
+        lbl = tk.Label(text='Вы ничего не ввели')
+        lbl.pack()
+    else:
+        lbl = tk.Label(text=f'Приветствую, {st}')
+        lbl.pack()
+
+
+def handle_click2(event):
+    st = entry.get()
+    if len(st) == 0:
+        lbl = tk.Label(text='Вы ничего не ввели')
+        lbl.pack()
+    else:
+        lbl = tk.Label(text=f'Всего доброго, {st}')
+        lbl.pack()
+
+
 window = tk.Tk()
-greeting = tk.Label(text="Привет, Tkinter!")
+window.geometry('400x400+500+500')
+greeting = tk.Label(text="Запишите имя пользователя в поле для ввода")
 greeting.pack()
-button = tk.Button(
-    text="Нажми на меня!",
-    width=25,
-    height=10,
+button1 = tk.Button(
+    text="Приветствие\nпользователя",
+    width=15,
+    height=5,
     bg="white",
     fg="black",
 )
-button.pack()
-entry = tk.Entry(fg="black", bg="white", width=50)
-entry.pack()
+button1.bind("<Button-1>", handle_click1)
+button1.pack(side=tk.LEFT)
+button2 = tk.Button(
+    text="Прощание\nс пользователем",
+    width=15,
+    height=5,
+    bg="white",
+    fg="black",
+)
+button2.bind("<Button-1>", handle_click2)
+button2.pack(side=tk.RIGHT)
+entry = tk.Entry(fg="black", bg="white", width=500)
+entry.pack(side=tk.BOTTOM)
 window.mainloop()
