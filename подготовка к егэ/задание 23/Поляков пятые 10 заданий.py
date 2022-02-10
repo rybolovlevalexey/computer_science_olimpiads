8633f30650256b8eb3ccf73d27214b55beeaa72c
Rybolovlev Alexey <rybolovlevalexey@yandex.ru>
2022-02-10 19:07:45 +0500

    Задание 43, Ответ 247

--- подготовка к егэ/задание 23/Поляков пятые 10 заданий.py
+++ подготовка к егэ/задание 23/Поляков пятые 10 заданий.py
@@ -1,11 +1,11 @@
-dp = [0] * 16
-dp[5] = 1
-for i in range(6, 11):
-    dp[i] = dp[i - 1] + dp[i - 2]
+dp = [0] * 21
+dp[3] = 1
+for i in range(4, 13):
+    dp[i] = dp[i - 1] + dp[i - 3]
 print(dp)
-for i in range(0, 10):
+for i in range(0, 12):
     dp[i] = 0
 print(dp)
-for i in range(11, 16):
-    dp[i] = dp[i - 1] + dp[i - 2]
+for i in range(13, 21):
+    dp[i] = dp[i - 1] + dp[i - 3]
 print(dp)
