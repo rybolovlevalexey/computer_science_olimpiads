2d4103b88e0a6c334aa20c42712a4e789b805faf
Rybolovlev Alexey <rybolovlevalexey@yandex.ru>
2022-02-16 10:03:27 +0500

    Задание 46, Ответ 52

--- подготовка к егэ/задание 23/Поляков четвёртые 10 заданий.py
+++ подготовка к егэ/задание 23/Поляков четвёртые 10 заданий.py
@@ -1,5 +1,25 @@
-dp = [0] * 52
-dp[31] = 1
-for i in range(32, 52):
-    dp[i] = dp[i - 2] + dp[i - 4] + dp[i - 5]
+dp = [0] * 29
+dp[2] = 1
+for i in range(3, 8):
+    if i % 6 == 0:
+        dp[i] = dp[i - 1] + dp[i // 2] + dp[i // 3]
+    elif i % 3 == 0:
+        dp[i] = dp[i - 1] + dp[i // 3]
+    elif i % 2 == 0:
+        dp[i] = dp[i - 1] + dp[i // 2]
+    else:
+        dp[i] = dp[i - 1]
+print(dp)
+for i in range(7):
+    dp[i] = 0
+print(dp)
+for i in range(8, 29):
+    if i % 6 == 0:
+        dp[i] = dp[i - 1] + dp[i // 2] + dp[i // 3]
+    elif i % 3 == 0:
+        dp[i] = dp[i - 1] + dp[i // 3]
+    elif i % 2 == 0:
+        dp[i] = dp[i - 1] + dp[i // 2]
+    else:
+        dp[i] = dp[i - 1]
 print(dp)
