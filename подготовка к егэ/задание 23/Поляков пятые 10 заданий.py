93b9d43e73c5eb5ade5b7cf8ab4e50480adc59a7
Rybolovlev Alexey <rybolovlevalexey@yandex.ru>
2022-02-17 17:34:51 +0500

    Задание 49, Ответ 17

--- подготовка к егэ/задание 23/Поляков пятые 10 заданий.py
+++ подготовка к егэ/задание 23/Поляков пятые 10 заданий.py
@@ -1,8 +1,8 @@
-dp = [0] * 11
-dp[3] = 1
-for i in range(4, 11):
+dp = [0] * 12
+dp[2] = 1
+for i in range(3, 12):
     if (i + 1) % 2 == 0:
-        dp[i] = dp[i - 1] + dp[i - 2] + dp[(i + 1) // 2]
+        dp[i] = dp[i - 2] + dp[i - 3] + dp[(i + 1) // 2]
     else:
-        dp[i] = dp[i - 1] + dp[i - 2]
+        dp[i] = dp[i - 2] + dp[i - 3]
 print(dp)
