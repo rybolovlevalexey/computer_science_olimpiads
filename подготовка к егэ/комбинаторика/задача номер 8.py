eafed34ea3bb225edbaad2b65a5fdcdeda67fd16
Rybolovlev Alexey <rybolovlevalexey@yandex.ru>
2022-02-19 14:25:17 +0500

    норм

--- подготовка к егэ/комбинаторика/задача номер 8.py
+++ подготовка к егэ/комбинаторика/задача номер 8.py
@@ -1,2 +1,15 @@
 from itertools import permutations
-sp = set('ВЫИГРЫВАТЬ')
+sp = set(permutations('ВЫИГРЫВАТЬ'))
+glas = ['Ы', 'И', 'А']
+ans = set()
+print(len(sp))
+for elem in sp:
+    elem = ''.join(elem)
+    flag = True
+    for j in range(len(elem) - 1):
+        if elem[j] in glas and elem[j + 1] in glas:
+            flag = False
+            break
+    if elem[0] != 'Ь' and flag:
+        ans.add(elem)
+print(len(ans))
