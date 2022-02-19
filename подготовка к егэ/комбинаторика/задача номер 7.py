b2f6d959b6195ad56e5efe95bfb9320587fb1d96
Rybolovlev Alexey <rybolovlevalexey@yandex.ru>
2022-02-19 14:12:00 +0500

    норм

--- подготовка к егэ/комбинаторика/задача номер 7.py
+++ подготовка к егэ/комбинаторика/задача номер 7.py
@@ -1,18 +1,17 @@
-sp = ['к', 'р', 'ы', 'ш', 'а']
-glas = ['ы', 'а']
-sogl = ['к', 'р', 'ш']
 ans = set()
+sp = ['c', 'о', 'л', 'о', 'в', 'е', 'й']
 for i1 in sp:
     for i2 in sp:
         for i3 in sp:
-            for i4 in sp + ['']:
-                for i5 in sp + ['']:
-                    st = i1 + i2 + i3 + i4 + i5
-                    flag = True
-                    for i in st[1:]:
-                        if i in sogl:
-                            flag = False
-                            break
-                    if st.count('ы') <= 2 and st.count('а') <= 2 and flag:
-                        ans.add(st)
+            for i4 in sp:
+                for i5 in sp:
+                    for i6 in sp:
+                        st = i1 + i2 + i3 + i4 + i5 + i6
+                        if st.count('й') <= 1 and i1 != 'й' and i6 != 'й':
+                            if 'й' in st:
+                                ind = st.index('й')
+                                if st[ind - 1] != 'е' and st[ind + 1] != 'е':
+                                    ans.add(st)
+                            elif 'й' not in st:
+                                ans.add(st)
 print(len(ans))
