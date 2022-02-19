4820d0bf52e1a80dcaeffbf1f20dd2c71719628d
Rybolovlev Alexey <rybolovlevalexey@yandex.ru>
2022-02-19 15:20:17 +0500

    норм

--- подготовка к егэ/логические выражения/задача номер 6.py
+++ подготовка к егэ/логические выражения/задача номер 6.py
@@ -3,6 +3,6 @@ for x in [True, False]:
     for y in [True, False]:
         for z in [True, False]:
             for w in [True, False]:
-                res = ((not x or y) == (not y or w)) and z and not x
-                if res:
+                res = (x and z) or ((not w or x) == (not z or y))
+                if not res:
                     print(int(x), int(y), int(z), int(w), int(res))
