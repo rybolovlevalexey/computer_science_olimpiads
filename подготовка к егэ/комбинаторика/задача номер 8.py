770cffa10b670fadaf57d1603277965bd391ad79
Rybolovlev Alexey <rybolovlevalexey@yandex.ru>
2022-02-19 14:40:48 +0500

    норм

--- подготовка к егэ/комбинаторика/задача номер 8.py
+++ подготовка к егэ/комбинаторика/задача номер 8.py
@@ -1,15 +1,21 @@
 from itertools import permutations
 sp = set(permutations('ШУРУМБУРУМ'))
 soglas = ['Ш', 'Р', 'М', 'Б']
+glas = ['У']
 ans = set()
 print(len(sp))
 for elem in sp:
     elem = ''.join(elem)
-    flag = True
+    flag1 = True
     for j in range(len(elem) - 2):
         if elem[j] in soglas and elem[j + 1] in soglas and elem[j + 2] in soglas:
-            flag = False
+            flag1 = False
             break
-    if flag:
+    flag2 = True
+    for j in range(len(elem) - 1):
+        if elem[j] in glas and elem[j + 1] in glas:
+            flag2 = False
+            break
+    if flag1 and flag2:
         ans.add(elem)
 print(len(ans))
