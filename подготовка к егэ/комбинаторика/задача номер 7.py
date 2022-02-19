a72e5d60366b45120272534e0061613bc9aeffae
Rybolovlev Alexey <rybolovlevalexey@yandex.ru>
2022-02-19 14:18:49 +0500

    норм

--- подготовка к егэ/комбинаторика/задача номер 7.py
+++ подготовка к егэ/комбинаторика/задача номер 7.py
@@ -1,17 +1,8 @@
+from itertools import permutations
+sp = set(permutations('кукуруза'))
 ans = set()
-sp = ['c', 'о', 'л', 'о', 'в', 'е', 'й']
-for i1 in sp:
-    for i2 in sp:
-        for i3 in sp:
-            for i4 in sp:
-                for i5 in sp:
-                    for i6 in sp:
-                        st = i1 + i2 + i3 + i4 + i5 + i6
-                        if st.count('й') <= 1 and i1 != 'й' and i6 != 'й':
-                            if 'й' in st:
-                                ind = st.index('й')
-                                if st[ind - 1] != 'е' and st[ind + 1] != 'е':
-                                    ans.add(st)
-                            elif 'й' not in st:
-                                ans.add(st)
+for elem in sp:
+    elem = ''.join(elem)
+    if elem.count('уу') == 0 and elem.count('ау') == 0 and elem.count('уа') == 0:
+        ans.add(elem)
 print(len(ans))
