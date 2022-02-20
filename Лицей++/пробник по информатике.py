8d2e6f676f70a0186c69e3f441aa49f38ba653b0
Rybolovlev Alexey <rybolovlevalexey@yandex.ru>
2022-02-20 20:42:38 +0500

    норм

--- Лицей++/пробник по информатике.py
+++ Лицей++/пробник по информатике.py
@@ -1,11 +1,17 @@
-for a in range(500000, 1, -1):
-    flag = True
-    for x in range(1, 1000000):
-        if (x % a == 0) or (x % 21 != 0) or (x % 14 == 0):
-            pass
-        else:
-            flag = False
-            break
-    if flag:
-        print(a)
-        break
+dp = [0] * 37
+dp[10] = 1
+for i in range(11, 19):
+    if i % 2 == 0:
+        dp[i] = dp[i - 3] + dp[i - 2] + dp[i // 2]
+    else:
+        dp[i] = dp[i - 3] + dp[i - 2]
+print(dp)
+for i in range(18):
+    dp[i] = 0
+print(dp)
+for i in range(19, 37):
+    if i % 2 == 0:
+        dp[i] = dp[i // 2] + dp[i - 2] + dp[i - 3]
+    else:
+        dp[i] = dp[i - 3] + dp[i - 2]
+print(dp)
