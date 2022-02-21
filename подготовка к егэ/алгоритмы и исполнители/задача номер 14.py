3bb0408125667531c31680a0a7321e8d5cc7817e
Rybolovlev Alexey <rybolovlevalexey@yandex.ru>
2022-02-22 00:40:56 +0500

    норм

--- подготовка к егэ/алгоритмы и исполнители/задача номер 14.py
+++ подготовка к егэ/алгоритмы и исполнители/задача номер 14.py
@@ -1,17 +1,14 @@
-def g(n):
-    sp.append(n + 2)
-    if n > 1:
-        sp.append(n + 6)
-        g(n - 1)
-        g(n - 2)
+def f(n):
+    if n == 30:
+        return 8713233
+    elif n == 31:
+        return 14098309
+    elif n == 32:
+        return 22811545
+    elif n >= 1:
+        return (1 + 1 + f(n - 1) + f(n - 2) + 1)
+    else:
+        return 1
 
-sp = list()
-g(19)
-print(sum(sp))
-for i in range(30):
-    sp = list()
-    g(i)
-    res = (sum(sp))
-    if res > 120000:
-        print(i)
-        break
+
+print(f(35))
