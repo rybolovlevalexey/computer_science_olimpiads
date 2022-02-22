8c3b7ef8b530b522ac89017c2278081843f237c1
Rybolovlev Alexey <rybolovlevalexey@yandex.ru>
2022-02-22 19:59:48 +0500

    №24.1, ответ: 0

---
+++ подготовка к егэ/задание 24/нарешивание полякова.py
@@ -0,0 +1,12 @@
+st = input()
+dl = 0
+max_dl = None
+for i in st:
+    if i == 'C':
+        dl += 1
+    else:
+        if max_dl is None or max_dl < dl:
+            max_dl = dl
+        dl = 0
+print(max_dl)
+print(dl)
