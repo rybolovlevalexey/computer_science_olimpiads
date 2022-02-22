b4ad08185c9fd4bf3a9b1326f38e28a4882eedf2
Rybolovlev Alexey <rybolovlevalexey@yandex.ru>
2022-02-22 20:25:38 +0500

    №24.29, ответ: 31

--- подготовка к егэ/задание 24/нарешивание полякова.py
+++ подготовка к егэ/задание 24/нарешивание полякова.py
@@ -1,11 +1,19 @@
 st = input()
+print(st.count('BAFEBAFEBAFEBAFEBAFEBAFEBAFEBAF'))
+print(len('BAFEBAFEBAFEBAFEBAFEBAFEBAFEBAF'))
 dl = 0
-max_dl = None
-for i in st:
-    if i == 'C':
+max_dl = -1
+let = {0: 'B', 1: 'A', 2: 'F', 3: 'E'}
+letter = 0  # B-0; A-1; F-2; E-3
+for i in range(len(st)):
+    if st[i] == let[letter]:
         dl += 1
+        if max_dl < dl:
+            max_dl = dl
     else:
-        if max_dl is None or max_dl < dl:
+        if max_dl < dl:
             max_dl = dl
         dl = 0
+    letter = (letter + 1) % 4
 print(max_dl)
+print(dl)
