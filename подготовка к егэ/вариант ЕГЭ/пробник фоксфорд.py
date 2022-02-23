de7879861945330489643e785804a3526e5d143e
Rybolovlev Alexey <rybolovlevalexey@yandex.ru>
2022-02-23 15:31:43 +0500

    норм

--- подготовка к егэ/вариант ЕГЭ/пробник фоксфорд.py
+++ подготовка к егэ/вариант ЕГЭ/пробник фоксфорд.py
@@ -1,15 +1,10 @@
-file = open('Zadanie-260370B.txt', 'r')
-sp = file.read().strip().split('\n')
-dlina = int(sp[0])
-del sp[0]
-s = 0
-cnt = 0
-for i in range(dlina):
-    i1, i2, i3 = map(int, sp[i].split())
-    i1, i2, i3 = min(i1, i2, i3), sum([i1, i2, i3]) - max(i1, i2, i3) - min(i1, i2, i3), max(i1, i2, i3)
-    if i3 - i2 == 1 and cnt < 2:
-        cnt += 1
-        s += i2
-    else:
-        s += i3
-print(s)
+def f(n):
+    if n < 4:
+        return n - 1
+    elif n >= 4 and n % 3 == 0:
+        return n + 2 * f(n - 1)
+    elif n >= 4 and n % 3 != 0:
+        return f(n - 2) + f(n - 3)
+
+
+print(f(25))
