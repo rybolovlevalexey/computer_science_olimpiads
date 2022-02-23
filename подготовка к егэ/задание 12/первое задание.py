1bec43c3bb164e97a19c006303bfae7cdbe781a5
Rybolovlev Alexey <rybolovlevalexey@yandex.ru>
2022-02-23 11:01:55 +0500

    норм

--- подготовка к егэ/задание 12/первое задание.py
+++ подготовка к егэ/задание 12/первое задание.py
@@ -1,10 +1,7 @@
-for i1 in range(1, 50):
-    for i2 in range(1, 50):
-        for i3 in range(1, 50):
-            st = '0' + '1' * i1 + '2' * i2 + '3' * i3
-            while '01' in st or '02' in st or '03' in st:
-                st = st.replace('01', '210', 1)
-                st = st.replace('02', '3101', 1)
-                st = st.replace('03', '2302', 1)
-            if st.count('1') == 52 and st.count('2') == 33 and st.count('3') == 23:
-                print(i2)
+st = '>' + '1' * 20 + '2' * 20 + '3' * 20
+while '>1' in st or '>2' in st or '>3' in st:
+    st = st.replace('>1', '22>3', 1)
+    st = st.replace('>2', '33>', 1)
+    st = st.replace('>3', '11>2', 1)
+print(st)
+print(sum(map(int, list(st[:-1]))))
