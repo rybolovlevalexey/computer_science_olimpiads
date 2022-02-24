fcf94c55a6dfa5b5bef6e5641b32e24ba1220703
Rybolovlev Alexey <rybolovlevalexey@yandex.ru>
2022-02-24 21:16:43 +0500

    задание 16

--- подготовка к егэ/вариант ЕГЭ/24.02.py
+++ подготовка к егэ/вариант ЕГЭ/24.02.py
@@ -1,14 +1,8 @@
-for a in range(1000, 0, -1):
-    flag = True
-    for x in range(10000):
-        for y in range(10000):
-            if (2*x + 3*y < 30) or (x + y >= a):
-                pass
-            else:
-                flag = False
-                break
-        if not flag:
-            break
-    if flag:
-        print(a)
-        break
+def f(n):
+    if n > 0:
+        f(n // 4)
+        print(n, end='')
+        f(n - 1)
+
+
+f(5)
