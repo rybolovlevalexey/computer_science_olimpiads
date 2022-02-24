a39cf645f5859d16cae597b946c51d31acb251b3
Rybolovlev Alexey <rybolovlevalexey@yandex.ru>
2022-02-24 20:28:59 +0500

    задание 6

--- подготовка к егэ/вариант ЕГЭ/24.02.py
+++ подготовка к егэ/вариант ЕГЭ/24.02.py
@@ -1,8 +1,6 @@
-print('x y z w res')
-for x in [True, False]:
-    for y in [True, False]:
-        for z in [True, False]:
-            for w in [True, False]:
-                res = ((not x or y) == (not z or w)) or (x and w)
-                if not res:
-                    print(int(x), int(y), int(z), int(w), int(res))
+s = 0
+k = 1
+while s < 66:
+    k += 3
+    s += k
+print(k)
