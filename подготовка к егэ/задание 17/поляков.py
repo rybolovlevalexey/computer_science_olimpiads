1990593b4f0eb42f9e13f12ae7384faa64727ad3
Rybolovlev Alexey <rybolovlevalexey@yandex.ru>
2022-02-24 12:49:01 +0500

    №17.52, ответ: 45 15610

--- подготовка к егэ/задание 17/поляков.py
+++ подготовка к егэ/задание 17/поляков.py
@@ -1,5 +1,19 @@
 ans = list()
-for x in range(117649, 823542 + 1):
-    if x % 3 == 2 and x % 8 != 3 and x % 12 != 5:
+for x in range(1000, 70000 + 1):
+    d1 = 0
+    num = x
+    while num > 0:
+        num //= 8
+        d1 += 1
+    if d1 != 5:
+        continue
+    d2 = 0
+    num = x
+    while num > 0:
+        num //= 5
+        d2 += 1
+    if d2 != 6:
+        continue
+    if x % 16 == 10 and x // 16 % 16 == 15:
         ans.append(x)
 print(len(ans), max(ans))
