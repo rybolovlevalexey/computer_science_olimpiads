b051a0f052b7a607274a091f59331e400bffc427
Rybolovlev Alexey <rybolovlevalexey@yandex.ru>
2022-02-24 11:54:48 +0500

    №17.3, ответ: 1178 10992

--- подготовка к егэ/задание 17/поляков.py
+++ подготовка к егэ/задание 17/поляков.py
@@ -1,5 +1,5 @@
 ans = []
-for x in range(3201, 12876 + 1):
-    if x % 4 == 0 and x % 11 != 0 and x % 13 != 0 and x % 7 != 0 and x % 19 != 0:
+for x in range(1100, 11000 + 1):
+    if x % 6 == 0 and x % 7 != 0 and x % 13 != 0 and x % 17 != 0 and x % 23 != 0:
         ans.append(x)
 print(len(ans), max(ans))
