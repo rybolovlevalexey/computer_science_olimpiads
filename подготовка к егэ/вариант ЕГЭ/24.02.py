23d7dc6bcd04d1e3c2820fa87a383ecd547b3491
Rybolovlev Alexey <rybolovlevalexey@yandex.ru>
2022-02-24 21:33:45 +0500

    задание 17

--- подготовка к егэ/вариант ЕГЭ/24.02.py
+++ подготовка к егэ/вариант ЕГЭ/24.02.py
@@ -1,8 +1,12 @@
-def f(n):
-    if n > 0:
-        f(n // 4)
-        print(n, end='')
-        f(n - 1)
-
-
-f(5)
+file = open('17.txt', 'r')
+sp = file.read().strip().split('\n')
+sp = list(map(int, sp))
+sr = sum(filter(lambda x: x % 2 == 0, sp)) / len(list(filter(lambda x: x % 2 == 0, sp)))
+cnt = 0
+ans = -1
+for i in range(len(sp) - 1):
+    if (sp[i] % 3 == 0 or sp[i + 1] % 3 == 0) and (sp[i] < sr or sp[i + 1] < sr):
+        cnt += 1
+        if sp[i] + sp[i + 1] > ans:
+            ans = sp[i] + sp[i + 1]
+print(cnt, ans)
