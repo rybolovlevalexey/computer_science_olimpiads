b5ec13eddb3acc5f83a31c1a9335e7d4c19cb6b7
Rybolovlev Alexey <rybolovlevalexey@yandex.ru>
2022-02-25 09:36:33 +0500

    задание 24

--- подготовка к егэ/вариант ЕГЭ/24.02.py
+++ подготовка к егэ/вариант ЕГЭ/24.02.py
@@ -1,17 +1,19 @@
-dp = [0] * 13
-dp[3] = 1
-for i in range(4, 11):
-    if i % 2 == 0:
-        dp[i] = dp[i - 1] + dp[i - 2] + dp[i // 2]
-    else:
-        dp[i] = dp[i - 1] + dp[i - 2]
-print(dp)
-for i in range(10):
-    dp[i] = 0
-print(dp)
-for i in range(11, 13):
-    if i % 2 == 0:
-        dp[i] = dp[i - 1] + dp[i - 2] + dp[i // 2]
-    else:
-        dp[i] = dp[i - 1] + dp[i - 2]
-print(dp)
+file = open('24.txt', 'r')
+st = file.read().strip()
+indexes = list()
+for i in range(len(st) - 3):
+    if st[i:i+4] == 'XZZY':
+        indexes.append(i)
+ans = -1
+indexes = indexes[::-1]
+for j in range(len(indexes) - 1):
+    delta = indexes[j] - indexes[j + 1] - 4
+    if delta > ans:
+        ans = delta
+print(ans)
+maximum = -1
+for i in range(len(indexes) - 1):
+    delta = abs(indexes[i] - indexes[i + 1])
+    if delta > maximum:
+        maximum = delta
+print(maximum)
