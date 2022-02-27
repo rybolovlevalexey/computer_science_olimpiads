bef9d2539a11b6493bb316e42888f12d00e0e576
Rybolovlev Alexey <rybolovlevalexey@yandex.ru>
2022-02-27 21:35:20 +0500

    №23.52, ответ: 55

--- подготовка к егэ/задание 23/Поляков с 51 по 60.py
+++ подготовка к егэ/задание 23/Поляков с 51 по 60.py
@@ -1,27 +1,16 @@
-dp = [0] * 33
-dp[5] = 1
-num_sp = {5: ['']}
-for i in range(6, 33):
-    if i % 2 == 0:
-        dp[i] = dp[i - 1] + dp[i // 2]
-        num_sp[i] = list()
-        for elem in num_sp.get(i - 1):
-            if i not in num_sp:
-                num_sp[i] = [elem + '1']
-            else:
-                num_sp[i] += [elem + '1']
-        for elem in num_sp.get(i // 2, []):
-            num_sp[i] += [elem + '2']
-    else:
-        num_sp[i] = list()
-        for elem in num_sp[i - 1]:
-            if i not in num_sp:
-                num_sp[i] = [elem + '1']
-            else:
-                num_sp[i] += [elem + '1']
-        dp[i] = dp[i - 1]
+dp = [0] * 15
+dp[4] = 1
+num_sp = {4: ['']}
+for i in range(5, 15):
+    dp[i] = dp[i - 1] + dp[i - 2]
+    num_sp[i] = list()
+    for elem in num_sp.get(i - 1, []):
+        num_sp[i] += [elem + '1']
+    for elem in num_sp.get(i - 2, []):
+        num_sp[i] += [elem + '2']
+
 ans = 0
-for elem in num_sp[32]:
+for elem in num_sp[14]:
     if elem[-2] == '1':
         ans += 1
 print(ans)
