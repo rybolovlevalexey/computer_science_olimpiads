634e14ecf266834e036b0872066a4e0840f5c7c7
Rybolovlev Alexey <rybolovlevalexey@yandex.ru>
2022-02-27 21:36:23 +0500

    №23.53, ответ: 377

--- подготовка к егэ/задание 23/Поляков с 51 по 60.py
+++ подготовка к егэ/задание 23/Поляков с 51 по 60.py
@@ -1,7 +1,7 @@
-dp = [0] * 15
-dp[4] = 1
-num_sp = {4: ['']}
-for i in range(5, 15):
+dp = [0] * 19
+dp[3] = 1
+num_sp = {3: ['']}
+for i in range(4, 19):
     dp[i] = dp[i - 1] + dp[i - 2]
     num_sp[i] = list()
     for elem in num_sp.get(i - 1, []):
@@ -10,7 +10,7 @@ for i in range(5, 15):
         num_sp[i] += [elem + '2']
 
 ans = 0
-for elem in num_sp[14]:
-    if elem[-2] == '1':
+for elem in num_sp[18]:
+    if elem[-2] == '2':
         ans += 1
 print(ans)
