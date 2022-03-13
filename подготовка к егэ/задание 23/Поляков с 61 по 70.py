7ad8ce9d31beaddaf0ea5b220cb4f5aee02582da
Rybolovlev Alexey <rybolovlevalexey@yandex.ru>
2022-03-13 11:57:32 +0500

    №23.63, ответ: 38

--- подготовка к егэ/задание 23/Поляков с 61 по 70.py
+++ подготовка к егэ/задание 23/Поляков с 61 по 70.py
@@ -1,17 +1,17 @@
-dp = [0] * 56
-dp[3] = 1
-for i in range(4, 19):
-    if i == 12:
-        continue
+dp = [0] * 61
+dp[5] = 1
+for i in range(6, 9):
     if i % 2 == 0:
         dp[i] = dp[i // 2] + dp[i - 1]
     else:
         dp[i] = dp[i - 1]
 print(dp)
-for i in range(18):
+for i in range(8):
     dp[i] = 0
 print(dp)
-for i in range(19, 56):
+for i in range(9, 61):
+    if i == 22:
+        continue
     if i % 2 == 0:
         dp[i] = dp[i // 2] + dp[i - 1]
     else:
