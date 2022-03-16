6a83abcccf0fc7a3766f105884fbf019a3825748
Rybolovlev Alexey <rybolovlevalexey@yandex.ru>
2022-03-16 19:14:35 +0500

    №23.78, ответ: 120

--- подготовка к егэ/задание 23/Поляков с 71 по 80.py
+++ подготовка к егэ/задание 23/Поляков с 71 по 80.py
@@ -1,8 +1,15 @@
-dp = [0] * 17
+dp = [0] * 14
 dp[2] = 1
-for i in range(3, 17):
-    if i == 14:
-        continue
+for i in range(3, 11):
+    if i % 3 == 0:
+        dp[i] = dp[i - 1] + dp[i - 2] + dp[i // 3]
+    else:
+        dp[i] = dp[i - 1] + dp[i - 2]
+print(dp)
+for i in range(10):
+    dp[i] = 0
+print(dp)
+for i in range(11, 14):
     if i % 3 == 0:
         dp[i] = dp[i - 1] + dp[i - 2] + dp[i // 3]
     else:
