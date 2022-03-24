697edb643747c148ad008d50b7ce5848e019f718
Rybolovlev Alexey <rybolovlevalexey@yandex.ru>
2022-03-24 21:09:33 +0500

    норм

--- подготовка к егэ/вариант ЕГЭ/24.03.py
+++ подготовка к егэ/вариант ЕГЭ/24.03.py
@@ -1,8 +1,7 @@
-print('x y z w result')
-for x in [True, False]:
-    for y in [True, False]:
-        for z in [True, False]:
-            for w in [True, False]:
-                result = not(w) and z and (not(y) or x)
-                if result:
-                    print(int(x), int(y), int(z), int(w), int(result))
+for num in range(10000, 99999 + 1):
+    st1 = int(str(num)[0]) + int(str(num)[2]) + int(str(num)[4])
+    st2 = int(str(num)[1]) + int(str(num)[3])
+    ans = str(min(st1, st2)) + str(max(st1, st2))
+    if ans == '621':
+        print(num)
+        break
