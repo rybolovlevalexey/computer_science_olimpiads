4e9111cbcb9ab37655eaa0a3ca5aa5c88cab363d
Rybolovlev Alexey <rybolovlevalexey@yandex.ru>
2022-03-24 21:11:53 +0500

    норм

--- подготовка к егэ/вариант ЕГЭ/24.03.py
+++ подготовка к егэ/вариант ЕГЭ/24.03.py
@@ -1,7 +1,6 @@
-for num in range(10000, 99999 + 1):
-    st1 = int(str(num)[0]) + int(str(num)[2]) + int(str(num)[4])
-    st2 = int(str(num)[1]) + int(str(num)[3])
-    ans = str(min(st1, st2)) + str(max(st1, st2))
-    if ans == '621':
-        print(num)
-        break
+n = 1
+s = 26
+while s <= 205:
+    s += 20
+    n *= 2
+print(n)
