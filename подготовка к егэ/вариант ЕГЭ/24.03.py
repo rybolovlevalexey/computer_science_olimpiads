37e5b1877ae99f4fedf4e2a7272f6f1555d24ce1
Rybolovlev Alexey <rybolovlevalexey@yandex.ru>
2022-03-24 21:30:05 +0500

    норм

--- подготовка к егэ/вариант ЕГЭ/24.03.py
+++ подготовка к егэ/вариант ЕГЭ/24.03.py
@@ -1,6 +1,12 @@
-n = 1
-s = 26
-while s <= 205:
-    s += 20
-    n *= 2
-print(n)
+sp = ['б', 'о', 'р', 'и', 'с']
+ans = 0
+for i1 in sp:
+    for i2 in sp:
+        for i3 in sp:
+            for i4 in sp:
+                for i5 in sp:
+                    for i6 in sp:
+                        st = i1 + i2 + i3 + i4 + i5 + i6
+                        if st.count('б') == 1 and st.count('р') == 1 and (st.count('с') == 1 or st.count('с') == 0):
+                            ans += 1
+print(ans)
