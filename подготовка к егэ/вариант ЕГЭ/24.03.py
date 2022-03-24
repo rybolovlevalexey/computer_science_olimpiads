6e5c49a8982350c3e317977140ba50ad83ac4ea6
Rybolovlev Alexey <rybolovlevalexey@yandex.ru>
2022-03-24 22:12:30 +0500

    норм

--- подготовка к егэ/вариант ЕГЭ/24.03.py
+++ подготовка к егэ/вариант ЕГЭ/24.03.py
@@ -1,6 +1,8 @@
-st = '1' + '9' * 98
-while '19' in st or '299' in st or '3999' in st:
-    st = st.replace('19', '2', 1)
-    st = st.replace('299', '3', 1)
-    st = st.replace('3999', '1', 1)
-print(st)
+def f(n):
+    if n > 3:
+        return f(n - 2) + f(n // 2)
+    else:
+        return n
+
+
+print(f(9))
