13c0c25c6a049c18d246c0d920eb0140dd0f5634
Rybolovlev Alexey <rybolovlevalexey@yandex.ru>
2022-03-25 11:14:54 +0500

    норм

--- подготовка к егэ/вариант ЕГЭ/24.03.py
+++ подготовка к егэ/вариант ЕГЭ/24.03.py
@@ -1,8 +1,11 @@
-def f(n):
-    if n > 3:
-        return f(n - 2) + f(n // 2)
-    else:
-        return n
-
-
-print(f(9))
+file = open('17.txt', 'r')
+sp = list(map(int, file.read().strip().split('\n')))
+sr = sum(sp) / len(sp)
+cnt = 0
+ans = -1
+for i in range(len(sp) - 1):
+    if (sp[i] % 5 == 0 or sp[i + 1] % 5 == 0) and (sp[i] < sr or sp[i + 1] < sr):
+        cnt += 1
+        if sp[i] + sp[i + 1] > ans:
+            ans = sp[i] + sp[i + 1]
+print(cnt, ans)
