a69086303d7ae6cb6d60c28f2c49535e0d7ee0e2
Rybolovlev Alexey <rybolovlevalexey@yandex.ru>
2022-03-25 13:20:31 +0500

    норм

--- подготовка к егэ/вариант ЕГЭ/24.03.py
+++ подготовка к егэ/вариант ЕГЭ/24.03.py
@@ -1,11 +1,9 @@
-file = open('17.txt', 'r')
-sp = list(map(int, file.read().strip().split('\n')))
-sr = sum(sp) / len(sp)
-cnt = 0
-ans = -1
-for i in range(len(sp) - 1):
-    if (sp[i] % 5 == 0 or sp[i + 1] % 5 == 0) and (sp[i] < sr or sp[i + 1] < sr):
-        cnt += 1
-        if sp[i] + sp[i + 1] > ans:
-            ans = sp[i] + sp[i + 1]
-print(cnt, ans)
+for elem in range(0, 10000):
+    x = elem
+    r = 0
+    while x > 0:
+        d = x % 10
+        r = 10 * r + d
+        x = x // 10
+    if len(str(r)) == 2 and int(str(r)[0]) + int(str(r)[1]) == 16:
+        print(elem)
