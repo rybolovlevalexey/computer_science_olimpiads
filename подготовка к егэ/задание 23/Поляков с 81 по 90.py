73a4940ab288ad800f3c887e2a25672ff49c0152
Rybolovlev Alexey <rybolovlevalexey@yandex.ru>
2022-03-31 20:48:43 +0500

    №23.86, ответ: 60

--- подготовка к егэ/задание 23/Поляков с 81 по 90.py
+++ подготовка к егэ/задание 23/Поляков с 81 по 90.py
@@ -1,19 +1,19 @@
-dp = [0] * 31
+dp = [0] * 29
 dp[2] = 1
-for i in range(3, 16):
+for i in range(3, 13):
     if i % 6 == 0:
-        dp[i] = dp[i - 1] + dp[i // 3] + dp[i // 2]
+        dp[i] = dp[i - 1] + dp[i // 2] + dp[i // 3]
     elif i % 3 == 0:
         dp[i] = dp[i - 1] + dp[i // 3]
     elif i % 2 == 0:
         dp[i] = dp[i - 1] + dp[i // 2]
     else:
         dp[i] = dp[i - 1]
-for i in range(15):
+for i in range(12):
     dp[i] = 0
-for i in range(16, 31):
+for i in range(13, 29):
     if i % 6 == 0:
-        dp[i] = dp[i - 1] + dp[i // 3] + dp[i // 2]
+        dp[i] = dp[i - 1] + dp[i // 2] + dp[i // 3]
     elif i % 3 == 0:
         dp[i] = dp[i - 1] + dp[i // 3]
     elif i % 2 == 0:
