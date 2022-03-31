013432434d8290f9653a1cd8811e1e1416a19241
Rybolovlev Alexey <rybolovlevalexey@yandex.ru>
2022-03-31 21:03:43 +0500

    №23.89, ответ: 180

--- подготовка к егэ/задание 23/Поляков с 81 по 90.py
+++ подготовка к егэ/задание 23/Поляков с 81 по 90.py
@@ -1,38 +1,38 @@
-dp = [0] * 14
+dp = [0] * 13
 dp[1] = 1
-for i in range(2, 5):
-    if i % 2 == 0 and i - 3 >= 0:
-        dp[i] = dp[i - 1] + dp[i - 3] + dp[i // 2]
+for i in range(2, 8):
+    if i % 2 == 0 and i - 2 >= 0:
+        dp[i] = dp[i - 1] + dp[i - 2] + dp[i // 2]
     elif i % 2 == 0:
         dp[i] = dp[i - 1] + dp[i // 2]
-    elif i - 3 >= 0:
-        dp[i] = dp[i - 1] + dp[i - 3]
+    elif i - 2 >= 0:
+        dp[i] = dp[i - 1] + dp[i - 2]
     else:
         dp[i] = dp[i - 1]
 print(dp)
-for i in range(4):
+for i in range(7):
     dp[i] = 0
 print(dp)
-for i in range(5, 10):
-    if i % 2 == 0 and i - 3 >= 0:
-        dp[i] = dp[i - 1] + dp[i - 3] + dp[i // 2]
+for i in range(8, 11):
+    if i % 2 == 0 and i - 2 >= 0:
+        dp[i] = dp[i - 1] + dp[i - 2] + dp[i // 2]
     elif i % 2 == 0:
         dp[i] = dp[i - 1] + dp[i // 2]
-    elif i - 3 >= 0:
-        dp[i] = dp[i - 1] + dp[i - 3]
+    elif i - 2 >= 0:
+        dp[i] = dp[i - 1] + dp[i - 2]
     else:
         dp[i] = dp[i - 1]
 print(dp)
-for i in range(9):
+for i in range(10):
     dp[i] = 0
 print(dp)
-for i in range(10, 14):
-    if i % 2 == 0 and i - 3 >= 0:
-        dp[i] = dp[i - 1] + dp[i - 3] + dp[i // 2]
+for i in range(11, 13):
+    if i % 2 == 0 and i - 2 >= 0:
+        dp[i] = dp[i - 1] + dp[i - 2] + dp[i // 2]
     elif i % 2 == 0:
         dp[i] = dp[i - 1] + dp[i // 2]
-    elif i - 3 >= 0:
-        dp[i] = dp[i - 1] + dp[i - 3]
+    elif i - 2 >= 0:
+        dp[i] = dp[i - 1] + dp[i - 2]
     else:
         dp[i] = dp[i - 1]
 print(dp)
