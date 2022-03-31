9a8d23fe6aa2fdd123a44b134cbda45e952113bd
Rybolovlev Alexey <rybolovlevalexey@yandex.ru>
2022-03-31 21:19:45 +0500

    №17.7, ответ: 776 200

--- подготовка к егэ/задание 17/поляков с 6 по 20.py
+++ подготовка к егэ/задание 17/поляков с 6 по 20.py
@@ -1,7 +1,7 @@
 ans = 0
 flag = True
-for num in range(1200, 11200 + 1):
-    if num % 5 == 0 and num % 7 != 0 and num % 13 != 0 and num % 17 != 0 and num % 19 != 0:
+for num in range(200, 9120 + 1):
+    if num % 8 == 0 and num % 7 != 0 and num % 11 != 0 and num % 17 != 0 and num % 19 != 0:
         ans += 1
         if flag:
             flag = False
