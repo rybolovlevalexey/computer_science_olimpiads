63a6bc57ffde529f17124cd20293a5607b652cc9
Rybolovlev Alexey <rybolovlevalexey@yandex.ru>
2022-04-03 17:55:37 +0500

    норм

--- подготовка к егэ/вариант ЕГЭ/03.04 (вар 1).py
+++ подготовка к егэ/вариант ЕГЭ/03.04 (вар 1).py
@@ -1,3 +1,15 @@
-for i in range(1, 18):
-    if i % 3 == i // 3 % 3:
-        print(i)
+ans = list()
+
+for a in range(100):
+    flag = True
+    for x in range(1000):
+        for y in range(1000):
+            if not ((x >= 6 or x**2 < a) and (y**2 > a or y <= 6)):
+                flag = False
+                break
+        if not flag:
+            break
+    if flag:
+        print(a)
+        ans.append(a)
+print(len(ans))
