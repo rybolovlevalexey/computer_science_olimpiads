9b3ecb21e5a6a967d73df753b2885a115cb226fc
Rybolovlev Alexey <rybolovlevalexey@yandex.ru>
2022-04-06 21:02:13 +0500

    №17.9, ответ: 791 1339

--- подготовка к егэ/задание 17/поляков с 6 по 20.py
+++ подготовка к егэ/задание 17/поляков с 6 по 20.py
@@ -1,9 +1,9 @@
 ans = 0
-flag = True
-for num in range(1107, 9504 + 1):
-    if num % 9 == 0 and num % 7 != 0 and num % 15 != 0 and num % 17 != 0 and num % 19 != 0:
+flag = False
+for elem in range(1325, 15367 + 1):
+    if elem % 13 == 0 and elem % 7 != 0 and elem % 17 != 0 and elem % 19 != 0 and elem % 23 != 0:
         ans += 1
-        if flag:
-            flag = False
-            print(num)
+        if not flag:
+            flag = True
+            print(elem)
 print(ans)
