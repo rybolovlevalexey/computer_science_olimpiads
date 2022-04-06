6338c2fabf6f11a9a0196bd9ee257143462333ee
Rybolovlev Alexey <rybolovlevalexey@yandex.ru>
2022-04-06 21:03:22 +0500

    №17.9, ответ: 4358 1098

--- подготовка к егэ/задание 17/поляков с 6 по 20.py
+++ подготовка к егэ/задание 17/поляков с 6 по 20.py
@@ -1,7 +1,7 @@
 ans = 0
 flag = False
-for elem in range(1325, 15367 + 1):
-    if elem % 13 == 0 and elem % 7 != 0 and elem % 17 != 0 and elem % 19 != 0 and elem % 23 != 0:
+for elem in range(1098, 13765 + 1):
+    if elem % 2 == 0 and elem % 7 != 0 and elem % 11 != 0 and elem % 13 != 0 and elem % 23 != 0:
         ans += 1
         if not flag:
             flag = True
