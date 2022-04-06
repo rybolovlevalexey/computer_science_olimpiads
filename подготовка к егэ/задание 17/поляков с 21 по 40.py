705a244bbf6185dcede4e522c244590714c53bf9
Rybolovlev Alexey <rybolovlevalexey@yandex.ru>
2022-04-06 21:08:55 +0500

    №17.21, ответ: 1323 2478

--- подготовка к егэ/задание 17/поляков с 21 по 40.py
+++ подготовка к егэ/задание 17/поляков с 21 по 40.py
@@ -1,7 +1,10 @@
 ans = 0
 cnt = 0
-for i in range(1221, 9763 + 1):
-    if i % 7 == 0 and i % 2 != 0 and i % 5 != 0 and i % 11 != 0 and i % 49 != 0:
-        ans = i
+flag = True
+for i in range(2477, 7849 + 1):
+    if i % 2 == 0 and i % 5 != 0 and i % 8 != 0 and i % 9 != 0 and i % 13 != 0:
         cnt += 1
+        if flag:
+            ans = i
+            flag = False
 print(cnt, ans)
