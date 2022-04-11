1baf8075893a2e2c93c345d4f52b1a3a2751b580
Rybolovlev Alexey <rybolovlevalexey@yandex.ru>
2022-04-11 23:03:05 +0500

    number 14

--- подготовка к егэ/вариант ЕГЭ/11.04 (вар 2).py
+++ подготовка к егэ/вариант ЕГЭ/11.04 (вар 2).py
@@ -1,13 +1,5 @@
-for a in range(1, 100):
-    flag = True
-    for x in range(1000):
-        for y in range(1000):
-            res = (y + 2 * x < a) or (x > 30) or (y > 20)
-            if not res:
-                flag = False
-                break
-        if not flag:
+for x in range(2, 10):
+    for y in range(2, 10):
+        if 2*x**2 + 2*x + 5 == 4*y**2 + 5:
+            print(x)
             break
-    if flag:
-        print(a)
-        break
