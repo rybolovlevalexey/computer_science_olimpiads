168a99d15f1b4d3fcc3d02a4c168cc42b7d44354
Rybolovlev Alexey <rybolovlevalexey@yandex.ru>
2022-04-11 22:52:21 +0500

    number 15

--- подготовка к егэ/вариант ЕГЭ/11.04 (вар 2).py
+++ подготовка к егэ/вариант ЕГЭ/11.04 (вар 2).py
@@ -1,11 +1,13 @@
-sp = list()
-
-def f(n):
-    sp.append(n)
-    if n < 5:
-        f(n + 1)
-        f(n + 3)
-
-f(1)
-print(sp)
-print(sum(sp))
+for a in range(1, 100):
+    flag = True
+    for x in range(1000):
+        for y in range(1000):
+            res = (y + 2 * x < a) or (x > 30) or (y > 20)
+            if not res:
+                flag = False
+                break
+        if not flag:
+            break
+    if flag:
+        print(a)
+        break
