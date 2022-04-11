d2186c2c50da77bce1440a6f105b725d11044ec9
Rybolovlev Alexey <rybolovlevalexey@yandex.ru>
2022-04-11 22:39:51 +0500

    №17.13, ответ: 3707 10986

--- подготовка к егэ/задание 17/поляков с 6 по 20.py
+++ подготовка к егэ/задание 17/поляков с 6 по 20.py
@@ -1,5 +1,5 @@
 sp = list()
-for i in range(1156, 12209 + 1):
-    if (i % 2 == 0 or i % 5 == 0) and i % 7 != 0 and i % 17 != 0 and i % 13 != 0 and i % 23 != 0:
+for i in range(2320, 10987 + 1):
+    if (i % 2 == 0 or i % 7 == 0) and i % 11 != 0 and i % 13 != 0 and i % 17 != 0 and i % 19 != 0:
         sp.append(i)
 print(len(sp), max(sp))
