c29e0419b4d116f267b058043c138acf22497260
Rybolovlev Alexey <rybolovlevalexey@yandex.ru>
2022-04-12 18:35:29 +0500

    №17.35, ответ: 1455 4237

--- подготовка к егэ/задание 17/поляков с 21 по 40.py
+++ подготовка к егэ/задание 17/поляков с 21 по 40.py
@@ -1,10 +1,10 @@
 sp = list()
-for num in range(1000, 9999 + 1):
+for num in range(1476, 7039 + 1):
     n = 0
     i = num
     while i > 0:
         i //= 4
         n += 1
-    if num % 3 != 0 and num % 17 != 0 and num % 19 != 0 and n == 6:
+    if num % 2 == 0 and num % 16 != 0 and num % 100 // 10 >= 4:
         sp.append(num)
-print(min(sp), max(sp))
+print(len(sp), (max(sp) + min(sp)) // 2)
