0962bb2c26ec297e2edc08fa58527c84bc2ca3c1
Rybolovlev Alexey <rybolovlevalexey@yandex.ru>
2022-04-12 18:32:05 +0500

    №17.34, ответ: 1024 4094

--- подготовка к егэ/задание 17/поляков с 21 по 40.py
+++ подготовка к егэ/задание 17/поляков с 21 по 40.py
@@ -3,8 +3,8 @@ for num in range(1000, 9999 + 1):
     n = 0
     i = num
     while i > 0:
-        i //= 3
+        i //= 4
         n += 1
-    if num % 5 != 0 and num % 7 != 0 and num % 11 != 0 and n == 8:
+    if num % 3 != 0 and num % 17 != 0 and num % 19 != 0 and n == 6:
         sp.append(num)
 print(min(sp), max(sp))
