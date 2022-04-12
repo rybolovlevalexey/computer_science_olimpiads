57e5f27a16000612a39eaafa8ff44d73d845d8a5
Rybolovlev Alexey <rybolovlevalexey@yandex.ru>
2022-04-12 21:11:39 +0500

    task 23

--- подготовка к егэ/вариант ЕГЭ/12.04 (вар 3).py
+++ подготовка к егэ/вариант ЕГЭ/12.04 (вар 3).py
@@ -1,12 +1,8 @@
-x = int(input())
-q = 6
-l = 0
-while x >= q:
-    l += 1
-    x = x - q
-M = x
-if M < l:
-    M = l
-    l = x
-print(l)
-print(M)
+dp = [0] * 10
+dp[2] = 1
+for i in range(3, 10):
+    if (i + 1) % 2 == 0:
+        dp[i] = dp[i - 1] + dp[i - 2] + dp[(i + 1) // 2]
+    else:
+        dp[i] = dp[i - 1] + dp[i - 2]
+print(dp)
