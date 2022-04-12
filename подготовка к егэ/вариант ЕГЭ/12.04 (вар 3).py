90d52cc081dc5209a96980b7e4d800c547168f0b
Rybolovlev Alexey <rybolovlevalexey@yandex.ru>
2022-04-12 20:50:33 +0500

    task 22

--- подготовка к егэ/вариант ЕГЭ/12.04 (вар 3).py
+++ подготовка к егэ/вариант ЕГЭ/12.04 (вар 3).py
@@ -1,13 +1,12 @@
-sp = list()
-for _ in range(10000):
-    sp.append(int(input()))
-print('ok')
-cnt = 0
-max_sum = 0
-for i in range(10000):
-    for j in range(i + 1, 10000):
-        if sp[i] * sp[j] % 10 == 0:
-            cnt += 1
-            if sp[i] + sp[j] > max_sum:
-                max_sum = sp[i] + sp[j]
-print(cnt, max_sum)
+x = int(input())
+q = 6
+l = 0
+while x >= q:
+    l += 1
+    x = x - q
+M = x
+if M < l:
+    M = l
+    l = x
+print(l)
+print(M)
