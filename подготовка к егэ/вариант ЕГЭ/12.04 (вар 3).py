74eaba7a74ace70252108bfc736213a70b2ac932
Rybolovlev Alexey <rybolovlevalexey@yandex.ru>
2022-04-12 19:45:04 +0500

    task 6

--- подготовка к егэ/вариант ЕГЭ/12.04 (вар 3).py
+++ подготовка к егэ/вариант ЕГЭ/12.04 (вар 3).py
@@ -1,14 +1,6 @@
-for n in range(0, 256):
-    x = bin(n)
-    x = x[2:]
-    while len(x) < 8:
-        x = '0' + x
-    y = ''
-    for elem in x:
-        if elem == '0':
-            y += '1'
-        else:
-            y += '0'
-    res = int(y, 2)
-    if res - n == 111:
-        print(n)
+s = 0
+k = 0
+while k < 30:
+    k += 3
+    s += k
+print(s)
