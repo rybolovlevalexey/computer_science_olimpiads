3d52285d3b0fa120c6cab2c20003e8050ef6a5ba
Rybolovlev Alexey <rybolovlevalexey@yandex.ru>
2022-04-12 21:19:56 +0500

    task 24

--- подготовка к егэ/вариант ЕГЭ/12.04 (вар 3).py
+++ подготовка к егэ/вариант ЕГЭ/12.04 (вар 3).py
@@ -1,8 +1,13 @@
-dp = [0] * 10
-dp[2] = 1
-for i in range(3, 10):
-    if (i + 1) % 2 == 0:
-        dp[i] = dp[i - 1] + dp[i - 2] + dp[(i + 1) // 2]
-    else:
-        dp[i] = dp[i - 1] + dp[i - 2]
-print(dp)
+file = open('24.txt', 'r')
+st = file.read().strip()
+all_indexes = list()
+max_dlina = 0
+for i in range(len(st)):
+    if st[i] == 'D':
+        all_indexes.append(i)
+for i1 in range(len(all_indexes) - 2):
+    i2 = i1 + 2
+    dlina = all_indexes[i2] - all_indexes[i1] - 1
+    if dlina > max_dlina:
+        max_dlina = dlina
+print(max_dlina)
