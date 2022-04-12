aa79ff648911233dbffa814a1cd27a82c333a367
Rybolovlev Alexey <rybolovlevalexey@yandex.ru>
2022-04-12 19:49:54 +0500

    task 12

--- подготовка к егэ/вариант ЕГЭ/12.04 (вар 3).py
+++ подготовка к егэ/вариант ЕГЭ/12.04 (вар 3).py
@@ -1,6 +1,19 @@
-s = 0
-k = 0
-while k < 30:
-    k += 3
-    s += k
-print(s)
+sp = [0, 1, 0, 1, 1, 1, 0, 0, 1, 1, 1, 0]
+ind = 10
+while sp[ind] == 1:
+    ind -= 1
+while sp[ind] == 0:
+    ind -= 1
+ind -= 1
+while sp[ind] == 1:
+    ind += 1
+    sp[ind] = 0
+while sp[ind] == 0:
+    ind += 1
+ind -= 1
+sp[ind] = 1
+ind -= 1
+while sp[ind] == 0:
+    ind -= 1
+ind -= 1
+print(sp)
