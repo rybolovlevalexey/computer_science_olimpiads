242ed656fc5575ef4f3c320e635386cc667686d0
Rybolovlev Alexey <rybolovlevalexey@yandex.ru>
2022-04-12 19:58:30 +0500

    task 14

--- подготовка к егэ/вариант ЕГЭ/12.04 (вар 3).py
+++ подготовка к егэ/вариант ЕГЭ/12.04 (вар 3).py
@@ -1,19 +1,8 @@
-sp = [0, 1, 0, 1, 1, 1, 0, 0, 1, 1, 1, 0]
-ind = 10
-while sp[ind] == 1:
-    ind -= 1
-while sp[ind] == 0:
-    ind -= 1
-ind -= 1
-while sp[ind] == 1:
-    ind += 1
-    sp[ind] = 0
-while sp[ind] == 0:
-    ind += 1
-ind -= 1
-sp[ind] = 1
-ind -= 1
-while sp[ind] == 0:
-    ind -= 1
-ind -= 1
-print(sp)
+for n in range(2, 50):
+    cnt = 0
+    num1 = num2 = 338
+    while num1 > 0:
+        cnt += 1
+        num1 //= n
+    if num2 % n == 2 and cnt == 3:
+        print(n)
