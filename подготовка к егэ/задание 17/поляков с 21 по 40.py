d56a09c7e2b665172ba97a1c94ad58707acaa924
Rybolovlev Alexey <rybolovlevalexey@yandex.ru>
2022-04-13 20:48:31 +0500

    №17.40, ответ: 494 1874

--- подготовка к егэ/задание 17/поляков с 21 по 40.py
+++ подготовка к егэ/задание 17/поляков с 21 по 40.py
@@ -1,5 +1,11 @@
 sp = list()
-for i in range(2461, 9719 + 1):
-    if 3 <= i % 100 // 10 <= 7 and str(i)[-3] not in ['1', '9']:
+for i in range(1871, 9197 + 1):
+    n1 = 0
+    i1 = i
+    while i1 > 0:
+        n1 += 1
+        i1 //= 16
+    if n1 != len(str(i)) and i % 9 in [2, 4]:
         sp.append(i)
-print(len(sp), max(sp))
+
+print(len(sp), min(sp))
