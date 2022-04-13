4c6b89311eda646ce7b30da9bc3fe59b31c04759
Rybolovlev Alexey <rybolovlevalexey@yandex.ru>
2022-04-13 21:07:27 +0500

    №23.100, ответ: 40

--- подготовка к егэ/задание 23/Поляков 91-100.py
+++ подготовка к егэ/задание 23/Поляков 91-100.py
@@ -1,23 +1,23 @@
-dp = [0] * 53
-dp[5] = 1
-for i in range(6, 16):
-    if i % 6 == 0:
-        dp[i] = dp[i - 1] + dp[i // 2] + dp[i // 3]
-    elif i % 2 == 0:
-        dp[i] = dp[i - 1] + dp[i // 2]
+dp = [0] * 61
+dp[2] = 1
+for i in range(3, 17):
+    if i % 12 == 0:
+        dp[i] = dp[i - 1] + dp[i // 4] + dp[i // 3]
+    elif i % 4 == 0:
+        dp[i] = dp[i - 1] + dp[i // 4]
     elif i % 3 == 0:
         dp[i] = dp[i - 1] + dp[i // 3]
     else:
         dp[i] = dp[i - 1]
-for i in range(15):
+for i in range(16):
     dp[i] = 0
-for i in range(16, 53):
-    if i == 29:
+for i in range(17, 61):
+    if i == 21:
         continue
-    if i % 6 == 0:
-        dp[i] = dp[i - 1] + dp[i // 2] + dp[i // 3]
-    elif i % 2 == 0:
-        dp[i] = dp[i - 1] + dp[i // 2]
+    if i % 12 == 0:
+        dp[i] = dp[i - 1] + dp[i // 4] + dp[i // 3]
+    elif i % 4 == 0:
+        dp[i] = dp[i - 1] + dp[i // 4]
     elif i % 3 == 0:
         dp[i] = dp[i - 1] + dp[i // 3]
     else:
