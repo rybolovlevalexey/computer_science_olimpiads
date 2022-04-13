a17273c5b0ae43903509a9cb1b95cfa0254c9d26
Rybolovlev Alexey <rybolovlevalexey@yandex.ru>
2022-04-13 10:39:27 +0500

    №23.94, ответ: 1873

--- подготовка к егэ/задание 23/Поляков 91-100.py
+++ подготовка к егэ/задание 23/Поляков 91-100.py
@@ -1,12 +1,15 @@
 dp = [0] * 23
 dp[22] = 1
 for i in range(21, 1, -1):
-    i1 = 4 * i // 3
-    if i + 3 <= 22:
-        res = dp[i + 1] + dp[i + 3]
+    if 0 <= i + 3 <= 22:
+        dp[i] = dp[i + 3] + dp[i + 1]
     else:
-        res = dp[i + 1]
-    if 0 <= i1 <= 22:
-        res += dp[i1]
-    dp[i] = res
+        dp[i] = dp[i + 1]
 print(dp)
+res = dp[2]
+for i in range(5, len(dp)):
+    if i % 4 == 2:
+        res += dp[i]
+    elif i % 4 == 3:
+        res += dp[i]
+print(res)
