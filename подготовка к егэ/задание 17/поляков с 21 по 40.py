d1371f11b492bd1d7e89769104da77f6f1cfc9d8
Rybolovlev Alexey <rybolovlevalexey@yandex.ru>
2022-04-13 20:40:58 +0500

    №17.38, ответ: 2919 9679

--- подготовка к егэ/задание 17/поляков с 21 по 40.py
+++ подготовка к егэ/задание 17/поляков с 21 по 40.py
@@ -1,5 +1,5 @@
 sp = list()
-for i in range(3905, 7998 + 1):
-    if i % 100 // 10 not in [0, 5] and 2 <= i % 1000 // 100 <= 6:
+for i in range(2461, 9719 + 1):
+    if 3 <= i % 100 // 10 <= 7 and str(i)[-3] not in ['1', '9']:
         sp.append(i)
-print(len(sp), min(sp))
+print(len(sp), max(sp))
