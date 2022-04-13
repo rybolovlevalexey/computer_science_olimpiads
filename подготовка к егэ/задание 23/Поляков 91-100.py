e7755c14fca5c4e7b1b172bc08994c6b20b2426b
Rybolovlev Alexey <rybolovlevalexey@yandex.ru>
2022-04-13 20:56:04 +0500

    №23.96, ответ: 156

--- подготовка к егэ/задание 23/Поляков 91-100.py
+++ подготовка к егэ/задание 23/Поляков 91-100.py
@@ -1,18 +1,18 @@
-dp = [0] * 19
+dp = [0] * 21
 dp[2] = 1
-for i in range(3, 10):
-    if 0 <= i - 3 <= 19:
+for i in range(3, 11):
+    if 0 <= i - 3 <= 20:
         dp[i] = dp[i - 1] + dp[i - 3]
     else:
         dp[i] = dp[i - 1]
 print(dp)
-for i in range(9):
+for i in range(10):
     dp[i] = 0
 print(dp)
-for i in range(10, 19):
-    if i == 14:
+for i in range(11, 21):
+    if i == 15:
         continue
-    if 0 <= i - 3 <= 19:
+    if 0 <= i - 3 <= 20:
         dp[i] = dp[i - 1] + dp[i - 3]
     else:
         dp[i] = dp[i - 1]
