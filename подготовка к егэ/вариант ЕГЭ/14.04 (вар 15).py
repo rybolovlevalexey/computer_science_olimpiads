e9c69feb836b6e733315df5dbae39e3ae8c87aea
Rybolovlev Alexey <rybolovlevalexey@yandex.ru>
2022-04-14 13:07:36 +0500

    норм

--- подготовка к егэ/вариант ЕГЭ/14.04 (вар 15).py
+++ подготовка к егэ/вариант ЕГЭ/14.04 (вар 15).py
@@ -1,6 +1,18 @@
-n = 1
-s = 0
-while s <= 1024:
-    s += 128
-    n *= 2
-print(n)
+sp = set()
+i1 = '*2+1'
+num = eval('1' + i1)
+print(num)
+for i1 in ['*2', '*2+1']:
+    for i2 in ['*2', '*2+1']:
+        for i3 in ['*2', '*2+1']:
+            for i4 in ['*2', '*2+1']:
+                for i5 in ['*2', '*2+1']:
+                    for i6 in ['*2', '*2+1']:
+                        for i7 in ['*2', '*2+1']:
+                            for i8 in ['*2', '*2+1']:
+                                for i9 in ['*2', '*2+1']:
+                                    for i10 in ['*2', '*2+1']:
+                                        num = eval(str(eval(str(eval(str(eval(str(eval(str(eval(str(eval(str(eval(str(eval(str(eval('1' + i1)) + i2)) + i3)) + i4)) + i5)) + i6)) + i7)) + i8)) + i9)) + i10)
+                                        sp.add(num)
+print(sp)
+print(len(sp))
