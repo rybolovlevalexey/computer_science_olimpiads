37a7ce12fc70d54818615309e89ca37a7dd5b8a7
Rybolovlev Alexey <rybolovlevalexey@yandex.ru>
2022-04-14 12:50:51 +0500

    норм

--- подготовка к егэ/вариант ЕГЭ/14.04 (вар 15).py
+++ подготовка к егэ/вариант ЕГЭ/14.04 (вар 15).py
@@ -1,8 +1,6 @@
-print('x y z w res')
-for x in [True, False]:
-    for y in [True, False]:
-        for z in [True, False]:
-            for w in [True, False]:
-                res = not ((not z or w) and (not x == y)) + (x and z)
-                if not res:
-                    print(int(x), int(y), int(z), int(w), int(res))
+n = 1
+s = 0
+while s <= 1024:
+    s += 128
+    n *= 2
+print(n)
