d3f19536d051fc372d5329f45dabbfb07b2a5018
Rybolovlev Alexey <rybolovlevalexey@yandex.ru>
2022-04-14 18:27:10 +0500

    норм

--- подготовка к егэ/вариант ЕГЭ/14.04 (вар 15).py
+++ подготовка к егэ/вариант ЕГЭ/14.04 (вар 15).py
@@ -1,9 +1,13 @@
-for i in range(0, 10000000):
-    x = i
-    a, b = 0, 1
-    while x > 0:
-        a += 1
-        b = b * (x % 100)
-        x = x // 100
-    if a == 2 and b == 5:
-        print(i)
+file = open('zadanie24_2.txt', 'r')
+st = file.read().strip()
+max_dlina = 0
+dlina = 0
+for i in range(len(st)):
+    elem = st[i]
+    if elem == 'D':
+        dlina += 1
+    else:
+        if dlina > 0 and max_dlina < dlina:
+            max_dlina = dlina
+        dlina = 0
+print(max_dlina)
