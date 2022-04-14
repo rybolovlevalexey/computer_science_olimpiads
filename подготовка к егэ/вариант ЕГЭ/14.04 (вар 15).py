88bd758dddd114b1230d3381be8e6353c00826f4
Rybolovlev Alexey <rybolovlevalexey@yandex.ru>
2022-04-14 18:21:17 +0500

    норм

--- подготовка к егэ/вариант ЕГЭ/14.04 (вар 15).py
+++ подготовка к егэ/вариант ЕГЭ/14.04 (вар 15).py
@@ -1,13 +1,9 @@
-file = open('17 (2).txt', 'r')
-sp = file.read().strip().split('\n')
-sp = list(map(int, sp))
-ans = 0
-max_sum = 0
-for i in range(len(sp) - 1):
-    i1 = sp[i]
-    i2 = sp[i + 1]
-    if (i1 % 5 == 0 or i2 % 5 == 0) and (i1 + i2) % 7 == 0:
-        ans += 1
-        if i1 + i2 > max_sum:
-            max_sum = i1 + i2
-print(ans, max_sum)
+for i in range(0, 10000000):
+    x = i
+    a, b = 0, 1
+    while x > 0:
+        a += 1
+        b = b * (x % 100)
+        x = x // 100
+    if a == 2 and b == 5:
+        print(i)
