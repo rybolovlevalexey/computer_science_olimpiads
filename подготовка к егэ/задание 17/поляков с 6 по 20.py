6641b37e68b8d74d3216c86bf6a6d5e79f149dab
Rybolovlev Alexey <rybolovlevalexey@yandex.ru>
2022-04-15 12:10:00 +0500

    №17.19, ответ: 1644 1308

--- подготовка к егэ/задание 17/поляков с 6 по 20.py
+++ подготовка к егэ/задание 17/поляков с 6 по 20.py
@@ -1,5 +1,5 @@
 sp = list()
-for num in range(1056, 7563 + 1):
-    if (num % 3 == 0 or num % 11 == 0) and num % 13 != 0 and num % 17 != 0 and num % 19 != 0 and num % 23 != 0:
+for num in range(1305, 7850 + 1):
+    if (num % 4 == 0 or num % 7 == 0) and num % 11 != 0 and num % 17 != 0 and num % 19 != 0 and num % 21 != 0:
         sp.append(num)
 print(len(sp), min(sp))
