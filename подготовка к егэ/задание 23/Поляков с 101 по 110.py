4c465a06fd07f0f0849e18419c2b017eff436551
Rybolovlev Alexey <rybolovlevalexey@yandex.ru>
2022-04-16 11:24:59 +0500

    №23.102, ответ: 174

--- подготовка к егэ/задание 23/Поляков с 101 по 110.py
+++ подготовка к егэ/задание 23/Поляков с 101 по 110.py
@@ -1,27 +1,27 @@
-dp = [0] * 71
+dp = [0] * 25
 dp[2] = 1
-for i in range(3, 9):
-    if i % 12 == 0:
-        dp[i] = dp[i - 1] + dp[i // 3] + dp[i // 4]
-    elif i % 3 == 0:
-        dp[i] = dp[i - 1] + dp[i // 3]
+for i in range(3, 7):
+    if i % 4 == 0 and 0 <= i - 4 <= 24:
+        dp[i] = dp[i - 1] + dp[i - 4] + dp[i // 4]
     elif i % 4 == 0:
         dp[i] = dp[i - 1] + dp[i // 4]
+    elif 0 <= i - 4 <= 24:
+        dp[i] = dp[i - 1] + dp[i - 4]
     else:
         dp[i] = dp[i - 1]
 print(dp)
-for i in range(8):
+for i in range(6):
     dp[i] = 0
 print(dp)
-for i in range(9, 71):
-    if i == 35:
+for i in range(7, 25):
+    if i == 8:
         continue
-    if i % 12 == 0:
-        dp[i] = dp[i - 1] + dp[i // 3] + dp[i // 4]
-    elif i % 3 == 0:
-        dp[i] = dp[i - 1] + dp[i // 3]
+    if i % 4 == 0 and 0 <= i - 4 <= 24:
+        dp[i] = dp[i - 1] + dp[i - 4] + dp[i // 4]
     elif i % 4 == 0:
         dp[i] = dp[i - 1] + dp[i // 4]
+    elif 0 <= i - 4 <= 24:
+        dp[i] = dp[i - 1] + dp[i - 4]
     else:
         dp[i] = dp[i - 1]
 print(dp)
