b2f4d631c8c1863b0f1b57ba055e970cda49fdba
Rybolovlev Alexey <rybolovlevalexey@yandex.ru>
2022-04-16 11:47:14 +0500

    №23.105, ответ: 53

--- подготовка к егэ/задание 23/Поляков с 101 по 110.py
+++ подготовка к егэ/задание 23/Поляков с 101 по 110.py
@@ -1,20 +1,37 @@
-ways = {3: ['']}
-dp = [0] * 23
-dp[3] = 1
-for i in range(4, 23):
-    dp[i] = dp[i - 1] + dp[i - 2] + dp[i - 3]
+ways = {2: ['']}
+dp = [0] * 26
+dp[2] = 1
+for i in range(3, 26):
     ways[i] = list()
-    for elem in ways.get(i - 1, []):
-        ways[i] += [elem + '1']
-    for elem in ways.get(i - 2, []):
-        ways[i] += [elem + '2']
-    for elem in ways.get(i - 3, []):
-        ways[i] += [elem + '3']
+    if i % 2 == 0 and 0 <= i - 3 <= 25:
+        dp[i] = dp[i - 1] + dp[i - 3] + dp[i // 2]
+        for elem in ways.get(i - 1, []):
+            ways[i] += [elem + '1']
+        for elem in ways.get(i - 3, []):
+            ways[i] += [elem + '2']
+        for elem in ways.get(i // 2, []):
+            ways[i] += [elem + '3']
+    elif i % 2 == 0:
+        dp[i] = dp[i - 1] + dp[i // 2]
+        for elem in ways.get(i - 1, []):
+            ways[i] += [elem + '1']
+        for elem in ways.get(i // 2, []):
+            ways[i] += [elem + '3']
+    elif 0 <= i - 3 <= 25:
+        dp[i] = dp[i - 1] + dp[i - 3]
+        for elem in ways.get(i - 1, []):
+            ways[i] += [elem + '1']
+        for elem in ways.get(i - 3, []):
+            ways[i] += [elem + '2']
+    else:
+        dp[i] = dp[i - 1]
+        for elem in ways.get(i - 1, []):
+            ways[i] += [elem + '1']
 
 print(dp)
-print(len(ways[22]))
+print(len(ways[25]))
 cnt = 0
-for elem in ways[22]:
+for elem in ways[25]:
     if len(elem) == 7:
         cnt += 1
 print(cnt)
