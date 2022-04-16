24068dfa6559d0d8b41ba5a3a6a7da7058be39e7
Rybolovlev Alexey <rybolovlevalexey@yandex.ru>
2022-04-16 11:33:44 +0500

    №23.103, ответ: 36

--- подготовка к егэ/задание 23/Поляков с 101 по 110.py
+++ подготовка к егэ/задание 23/Поляков с 101 по 110.py
@@ -1,27 +1,27 @@
-dp = [0] * 25
-dp[2] = 1
-for i in range(3, 7):
-    if i % 4 == 0 and 0 <= i - 4 <= 24:
-        dp[i] = dp[i - 1] + dp[i - 4] + dp[i // 4]
-    elif i % 4 == 0:
-        dp[i] = dp[i - 1] + dp[i // 4]
-    elif 0 <= i - 4 <= 24:
-        dp[i] = dp[i - 1] + dp[i - 4]
+ways = {1: ['']}
+dp = [0] * 21
+dp[1] = 1
+for i in range(2, 21):
+    if i % 2 == 0:
+        dp[i] = dp[i - 1] + dp[i - 2] + dp[i // 2]
+        ways[i] = list()
+        for elem in ways.get(i - 1, []):
+            ways[i] += [elem + '1']
+        for elem in ways.get(i - 2, []):
+            ways[i] += [elem + '2']
+        for elem in ways.get(i // 2, []):
+            ways[i] += [elem + '3']
     else:
-        dp[i] = dp[i - 1]
+        dp[i] = dp[i - 1] + dp[i - 2]
+        ways[i] = list()
+        for elem in ways.get(i - 1, []):
+            ways[i] += [elem + '1']
+        for elem in ways.get(i - 2, []):
+            ways[i] += [elem + '2']
 print(dp)
-for i in range(6):
-    dp[i] = 0
-print(dp)
-for i in range(7, 25):
-    if i == 8:
-        continue
-    if i % 4 == 0 and 0 <= i - 4 <= 24:
-        dp[i] = dp[i - 1] + dp[i - 4] + dp[i // 4]
-    elif i % 4 == 0:
-        dp[i] = dp[i - 1] + dp[i // 4]
-    elif 0 <= i - 4 <= 24:
-        dp[i] = dp[i - 1] + dp[i - 4]
-    else:
-        dp[i] = dp[i - 1]
-print(dp)
+print(len(ways[20]))
+cnt = 0
+for elem in ways[20]:
+    if len(elem) == 6:
+        cnt += 1
+print(cnt)
