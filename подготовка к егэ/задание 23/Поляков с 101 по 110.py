3dbb7cf2adda7798851804e0a9be3a5c9ef8a7dc
Rybolovlev Alexey <rybolovlevalexey@yandex.ru>
2022-04-16 11:37:44 +0500

    №23.104, ответ: 28

--- подготовка к егэ/задание 23/Поляков с 101 по 110.py
+++ подготовка к егэ/задание 23/Поляков с 101 по 110.py
@@ -1,27 +1,20 @@
-ways = {1: ['']}
-dp = [0] * 21
-dp[1] = 1
-for i in range(2, 21):
-    if i % 2 == 0:
-        dp[i] = dp[i - 1] + dp[i - 2] + dp[i // 2]
-        ways[i] = list()
-        for elem in ways.get(i - 1, []):
-            ways[i] += [elem + '1']
-        for elem in ways.get(i - 2, []):
-            ways[i] += [elem + '2']
-        for elem in ways.get(i // 2, []):
-            ways[i] += [elem + '3']
-    else:
-        dp[i] = dp[i - 1] + dp[i - 2]
-        ways[i] = list()
-        for elem in ways.get(i - 1, []):
-            ways[i] += [elem + '1']
-        for elem in ways.get(i - 2, []):
-            ways[i] += [elem + '2']
+ways = {3: ['']}
+dp = [0] * 23
+dp[3] = 1
+for i in range(4, 23):
+    dp[i] = dp[i - 1] + dp[i - 2] + dp[i - 3]
+    ways[i] = list()
+    for elem in ways.get(i - 1, []):
+        ways[i] += [elem + '1']
+    for elem in ways.get(i - 2, []):
+        ways[i] += [elem + '2']
+    for elem in ways.get(i - 3, []):
+        ways[i] += [elem + '3']
+
 print(dp)
-print(len(ways[20]))
+print(len(ways[22]))
 cnt = 0
-for elem in ways[20]:
-    if len(elem) == 6:
+for elem in ways[22]:
+    if len(elem) == 7:
         cnt += 1
 print(cnt)
