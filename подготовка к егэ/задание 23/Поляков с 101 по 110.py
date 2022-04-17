8569c23b2eaad986f674100e2876385d5b291fd0
Rybolovlev Alexey <rybolovlevalexey@yandex.ru>
2022-04-17 16:32:46 +0500

    №23.107, ответ: 37

--- подготовка к егэ/задание 23/Поляков с 101 по 110.py
+++ подготовка к егэ/задание 23/Поляков с 101 по 110.py
@@ -1,36 +1,26 @@
-dp = [0] * 35
-dp[1] = 1
-deyst = {1: ['']}
-for i in range(2, 35):
+dp = [0] * 28
+dp[3] = 1
+deyst = {3: ['']}
+for i in range(4, 28):
     deyst[i] = list()
-    if i % 6 == 0:
-        dp[i] = dp[i - 1] + dp[i // 2] + dp[i // 3]
+    if i % 2 == 0:
+        dp[i] = dp[i - 1] + dp[i - 4] + dp[i // 2]
         for elem in deyst.get(i - 1, []):
             deyst[i] += [elem + '1']
-        for elem in deyst.get(i // 2, []):
+        for elem in deyst.get(i - 4, []):
             deyst[i] += [elem + '2']
-        for elem in deyst.get(i // 3, []):
-            deyst[i] += [elem + '3']
-    elif i % 2 == 0:
-        dp[i] = dp[i - 1] + dp[i // 2]
-        for elem in deyst.get(i - 1, []):
-            deyst[i] += [elem + '1']
         for elem in deyst.get(i // 2, []):
-            deyst[i] += [elem + '2']
-    elif i % 3 == 0:
-        dp[i] = dp[i - 1] + dp[i // 3]
-        for elem in deyst.get(i - 1, []):
-            deyst[i] += [elem + '1']
-        for elem in deyst.get(i // 3, []):
             deyst[i] += [elem + '3']
     else:
-        dp[i] = dp[i - 1]
+        dp[i] = dp[i - 1] + dp[i - 4]
         for elem in deyst.get(i - 1, []):
             deyst[i] += [elem + '1']
+        for elem in deyst.get(i - 4, []):
+            deyst[i] += [elem + '2']
 print(dp)
-print(len(deyst[34]))
+print(len(deyst[27]))
 cnt = 0
-for elem in deyst[34]:
-    if len(elem) == 8:
+for elem in deyst[27]:
+    if len(elem) == 7:
         cnt += 1
 print(cnt)
