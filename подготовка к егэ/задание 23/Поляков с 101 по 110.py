d8d97c16aaf4fcff131da10beb60ce2d3f27197b
Rybolovlev Alexey <rybolovlevalexey@yandex.ru>
2022-04-17 16:29:01 +0500

    №23.106, ответ: 21

--- подготовка к егэ/задание 23/Поляков с 101 по 110.py
+++ подготовка к егэ/задание 23/Поляков с 101 по 110.py
@@ -1,37 +1,36 @@
-ways = {2: ['']}
-dp = [0] * 26
-dp[2] = 1
-for i in range(3, 26):
-    ways[i] = list()
-    if i % 2 == 0 and 0 <= i - 3 <= 25:
-        dp[i] = dp[i - 1] + dp[i - 3] + dp[i // 2]
-        for elem in ways.get(i - 1, []):
-            ways[i] += [elem + '1']
-        for elem in ways.get(i - 3, []):
-            ways[i] += [elem + '2']
-        for elem in ways.get(i // 2, []):
-            ways[i] += [elem + '3']
+dp = [0] * 35
+dp[1] = 1
+deyst = {1: ['']}
+for i in range(2, 35):
+    deyst[i] = list()
+    if i % 6 == 0:
+        dp[i] = dp[i - 1] + dp[i // 2] + dp[i // 3]
+        for elem in deyst.get(i - 1, []):
+            deyst[i] += [elem + '1']
+        for elem in deyst.get(i // 2, []):
+            deyst[i] += [elem + '2']
+        for elem in deyst.get(i // 3, []):
+            deyst[i] += [elem + '3']
     elif i % 2 == 0:
         dp[i] = dp[i - 1] + dp[i // 2]
-        for elem in ways.get(i - 1, []):
-            ways[i] += [elem + '1']
-        for elem in ways.get(i // 2, []):
-            ways[i] += [elem + '3']
-    elif 0 <= i - 3 <= 25:
-        dp[i] = dp[i - 1] + dp[i - 3]
-        for elem in ways.get(i - 1, []):
-            ways[i] += [elem + '1']
-        for elem in ways.get(i - 3, []):
-            ways[i] += [elem + '2']
+        for elem in deyst.get(i - 1, []):
+            deyst[i] += [elem + '1']
+        for elem in deyst.get(i // 2, []):
+            deyst[i] += [elem + '2']
+    elif i % 3 == 0:
+        dp[i] = dp[i - 1] + dp[i // 3]
+        for elem in deyst.get(i - 1, []):
+            deyst[i] += [elem + '1']
+        for elem in deyst.get(i // 3, []):
+            deyst[i] += [elem + '3']
     else:
         dp[i] = dp[i - 1]
-        for elem in ways.get(i - 1, []):
-            ways[i] += [elem + '1']
-
+        for elem in deyst.get(i - 1, []):
+            deyst[i] += [elem + '1']
 print(dp)
-print(len(ways[25]))
+print(len(deyst[34]))
 cnt = 0
-for elem in ways[25]:
-    if len(elem) == 7:
+for elem in deyst[34]:
+    if len(elem) == 8:
         cnt += 1
 print(cnt)
