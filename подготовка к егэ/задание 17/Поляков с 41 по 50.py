b70afc27f4420dfbb8639e63a151b5d49c8a28b7
Rybolovlev Alexey <rybolovlevalexey@yandex.ru>
2022-04-18 21:00:15 +0500

    №17.45, ответ: 723 -3115

--- подготовка к егэ/задание 17/Поляков с 41 по 50.py
+++ подготовка к егэ/задание 17/Поляков с 41 по 50.py
@@ -1,5 +1,7 @@
 sp = list()
-for num in range(3912, 9193 + 1):
-    if sum(map(int, list(str(num)))) % 9 == 0 and (num % 16 != 1 or num // 16 % 16 != 2):
+n = 0
+for num in range(-9563, -3102 - 1):
+    x = abs(num)
+    if x % 7 == 0 and x % 11 != 0 and x % 23 != 0 and x % 10 != 8:
         sp.append(num)
 print(len(sp), max(sp))
