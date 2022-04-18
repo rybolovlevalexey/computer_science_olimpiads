db79f760be7a9dc0bf6772ebef1986f095b66e17
Rybolovlev Alexey <rybolovlevalexey@yandex.ru>
2022-04-18 20:44:07 +0500

    №17.42, ответ: 26 2586

--- подготовка к егэ/задание 17/Поляков с 41 по 50.py
+++ подготовка к егэ/задание 17/Поляков с 41 по 50.py
@@ -1,5 +1,5 @@
 sp = list()
-for num in range(2371, 9432 + 1):
-    if (num % 8 == 5 or num % 8 == 7) and num // 8 % 8 == 1 and num % 3 != 0 and num % 5 != 0:
+for num in range(2495, 7083 + 1):
+    if (num % 16 == 10 or num % 16 == 15) and num // 16 % 16 == 1 and num % 5 != 0 and num % 9 != 0:
         sp.append(num)
-print(len(sp), max(sp))
+print(len(sp), min(sp))
