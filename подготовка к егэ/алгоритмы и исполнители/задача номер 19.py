278243e919a3b4159af92833040d5bffac1e7da8
Rybolovlev Alexey <rybolovlevalexey@yandex.ru>
2022-04-19 10:56:13 +0500

    норм

--- подготовка к егэ/алгоритмы и исполнители/задача номер 19.py
+++ подготовка к егэ/алгоритмы и исполнители/задача номер 19.py
@@ -1,16 +1,15 @@
-for num in range(100000, 10000, -1):
-    x = num
+sp = list()
+for num in range(1000000):
     a = 0
     b = 0
+    x = num
     while x > 0:
-        y = x % 10
-        if y > 3:
-            a = a + 1
-        else:
-            b = b - 1
-        if y < 8:
-            b = b + 1
+        d = x % 10
+        if d > 4:
+            a += 1
+        if d < 7:
+            b += 1
         x = x // 10
-    if a == 2 and b == 1:
-        print(num)
-        break
+    if a == 2 and b == 3:
+        sp.append(num)
+print(max(sp))
