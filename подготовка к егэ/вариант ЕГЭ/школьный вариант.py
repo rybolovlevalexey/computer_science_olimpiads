f28f1cc95af417f1f7263176ecb9bb7adb55ae82
Rybolovlev Alexey <rybolovlevalexey@yandex.ru>
2022-04-20 11:05:46 +0500

    норм

--- подготовка к егэ/вариант ЕГЭ/школьный вариант.py
+++ подготовка к егэ/вариант ЕГЭ/школьный вариант.py
@@ -1,9 +1,8 @@
-for num in range(-1000, 1000):
-    s = num
-    n = 105
-    while n > s:
-        s += 3
-        n -= 2
-    if n == 67:
-        print(num)
-        break
+n = 1
+for i1 in ['К', 'О', 'Т']:
+    for i2 in ['К', 'О', 'Т']:
+        for i3 in ['К', 'О', 'Т']:
+            st = i1 + i2 + i3
+            if n == 22:
+                print(st)
+            n += 1
