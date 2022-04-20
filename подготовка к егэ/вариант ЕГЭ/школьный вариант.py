f9526e8411df3caed582cb0abd87e0e74b5c6641
Rybolovlev Alexey <rybolovlevalexey@yandex.ru>
2022-04-20 15:57:43 +0500

    норм

--- подготовка к егэ/вариант ЕГЭ/школьный вариант.py
+++ подготовка к егэ/вариант ЕГЭ/школьный вариант.py
@@ -1,14 +1,10 @@
-def f(n):
-    if n == 1:
-        return 1
-    else:
-        return 3*f(n - 1) + g(n - 1) - n + 5
-
-def g(n):
-    if n == 1:
-        return 1
-    else:
-        return f(n - 1) + 3*g(n - 1) - 3*n
-
-
-print(f(14)+g(14))
+file = open('zadanie_17.txt', 'r')
+sp = list(map(int, file.read().strip().split('\n')))
+cnt = 0
+mxsm = 0
+for i in range(len(sp) - 1):
+    if sp[i] % 16 == 14 or sp[i + 1] == 14:
+        cnt += 1
+        if sp[i] + sp[i + 1] > mxsm:
+            mxsm = sp[i] + sp[i + 1]
+print(cnt, mxsm)
