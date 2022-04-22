ab9ff829dda2b7cb9bcda2cbd8ed036a9fc65bad
Rybolovlev Alexey <rybolovlevalexey@yandex.ru>
2022-04-22 23:12:00 +0500

    норм

--- подготовка к егэ/вариант ЕГЭ/22.04 (вар 4).py
+++ подготовка к егэ/вариант ЕГЭ/22.04 (вар 4).py
@@ -1,8 +1,6 @@
-print('x y z res')
-for x in [True, False]:
-    for y in [True, False]:
-        for z in [True, False]:
-            res = (not x and y and z) or (not x and not y and z) or (not x and not y and not z)
-            if res:
-                print(int(x), int(y), int(z), int(res))
-
+s = 0
+k = 0
+while s < 80:
+    s += 2*k
+    k += 4
+print(s)
