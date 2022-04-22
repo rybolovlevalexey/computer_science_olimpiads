29a55f5d3bcea3d0c95e5b4bd6e33c98f4d51515
Rybolovlev Alexey <rybolovlevalexey@yandex.ru>
2022-04-22 23:21:23 +0500

    норм

--- подготовка к егэ/вариант ЕГЭ/22.04 (вар 4).py
+++ подготовка к егэ/вариант ЕГЭ/22.04 (вар 4).py
@@ -1,15 +1,10 @@
-cnt = 0
-for a in range(-100, 100):
-    flag = True
-    for x in range(1000):
-        for y in range(1000):
-            if not((x >= 5 or x**2 < a) and (y**2 > a or y <= 5)):
-                flag = False
-                break
-        if not flag:
-            break
-    if flag:
-        cnt += 1
-        print(a)
-print()
-print(cnt)
+ans = list()
+def f(n):
+    if n > 0:
+        ans.append(n)
+        f(n - 3)
+        f(n // 3)
+
+f(9)
+print(ans)
+print(''.join(list(map(str, ans))))
