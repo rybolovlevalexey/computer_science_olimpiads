0b3435af3a5f43ba24caf3cc8639e5d809b6b79a
Rybolovlev Alexey <rybolovlevalexey@yandex.ru>
2022-04-22 23:14:54 +0500

    норм

--- подготовка к егэ/вариант ЕГЭ/22.04 (вар 4).py
+++ подготовка к егэ/вариант ЕГЭ/22.04 (вар 4).py
@@ -1,6 +1,9 @@
-s = 0
-k = 0
-while s < 80:
-    s += 2*k
-    k += 4
-print(s)
+cnt = 0
+for num in range(31):
+    x = num
+    while x > 0:
+        if x // 5 == 0:
+            if x == 3:
+                cnt += 1
+        x //= 5
+print(cnt)
