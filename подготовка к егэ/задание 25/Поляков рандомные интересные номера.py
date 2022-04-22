d772dfb1d672067561f0cf9d6c770e06b354e291
Rybolovlev Alexey <rybolovlevalexey@yandex.ru>
2022-04-22 22:45:49 +0500

    норм

--- подготовка к егэ/задание 25/Поляков рандомные интересные номера.py
+++ подготовка к егэ/задание 25/Поляков рандомные интересные номера.py
@@ -1,13 +1,6 @@
-num = 2
-while num <= 263000:
-    s = 1 + num
+for num in range(300, 350 + 1):
+    sp = [1]
     for d in range(2, num // 2 + 1):
         if num % d == 0:
-            s += d
-    su = 1 + s
-    for d in range(2, s // 2 + 1):
-        if s % d == 0:
-            su += d
-    if num * 2 == su:
-        print(num)
-    num *= 2
+            sp.append(d)
+    sums = sp[0]
