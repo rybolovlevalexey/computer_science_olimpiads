d7a9132a7659adaf6875aeb98c7becc34fe2b9ac
Rybolovlev Alexey <rybolovlevalexey@yandex.ru>
2022-04-26 14:50:51 +0500

    норм

--- подготовка к егэ/вариант ЕГЭ/23.03 (вар 5).py
+++ подготовка к егэ/вариант ЕГЭ/23.03 (вар 5).py
@@ -1,8 +1,15 @@
-num = 9**8 + 3**5 - 9
-print(num)
-cnt = 0
-while num > 0:
-    if num % 3 == 2:
-        cnt += 1
-    num //= 3
-print(cnt)
+p = [2, 4, 6, 8, 10, 12, 14, 16, 18, 20]
+q = [3, 6, 9, 12, 15, 18, 21, 24, 27, 30]
+a = list()
+for num in range(1, 1000):
+    flag = True
+    for x in range(0, 10000):
+        if (not x == num or x in p) and (not x in q or x != num):
+            pass
+        else:
+            flag = False
+            break
+    if flag:
+        a.append(num)
+    print(a)
+print(len(a))
