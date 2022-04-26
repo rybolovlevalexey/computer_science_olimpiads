ede595a21a1f80d76c542d4f2cd50b7a7664db35
Rybolovlev Alexey <rybolovlevalexey@yandex.ru>
2022-04-26 08:55:37 +0500

    норм

--- подготовка к егэ/вариант ЕГЭ/23.03 (вар 5).py
+++ подготовка к егэ/вариант ЕГЭ/23.03 (вар 5).py
@@ -1,9 +1,6 @@
-for num in range(1, 1000):
-    n = num
-    r = bin(n)[2:]
-    r += str(sum(map(int, list(r))) % 2)
-    r += str(sum(map(int, list(r))) % 2)
-    r = int(r, 2)
-    if r > 97:
-        print(num)
-        break
+s = 3
+k = 1
+while k < 25:
+    s += k
+    k += 2
+print(s)
