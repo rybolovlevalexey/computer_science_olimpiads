5b704593f4e79975d30a4b6cebafcd90ba8dbaa1
Rybolovlev Alexey <rybolovlevalexey@yandex.ru>
2022-04-26 20:41:45 +0500

    норм

--- подготовка к егэ/вариант ЕГЭ/23.03 (вар 5).py
+++ подготовка к егэ/вариант ЕГЭ/23.03 (вар 5).py
@@ -1,12 +1,18 @@
-def is_prost(x):
-    for d in range(2, x // 2 + 2):
-        if x % d == 0:
-            return False
-    return True
+file = open('26.txt', 'r')
+sp = file.read().strip().split('\n')
+n = int(sp[0])
+del sp[0]
+rady = dict()
+for elem in sp:
+    ryd, place = map(int, elem.split())
+    if ryd in rady:
+        rady[ryd] += [place]
+    else:
+        rady[ryd] = [place]
 
-
-cnt = 1
-for num in range(2422000, 2422080 + 1):
-    if is_prost(num):
-        print(cnt, num)
-        cnt += 1
+for key in sorted(rady.keys())[::-1]:
+    if len(rady[key]) >= 2:
+        longryad = sorted(rady[key])
+        for i in range(len(longryad) - 1):
+            if longryad[i + 1] - longryad[i] - 1 == 2:
+                print(key, longryad[i] + 1)
