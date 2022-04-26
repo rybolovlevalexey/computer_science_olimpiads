1a723fff2d01b943194e3f901ad41e472f2082a6
Rybolovlev Alexey <rybolovlevalexey@yandex.ru>
2022-04-26 14:52:21 +0500

    норм

--- подготовка к егэ/вариант ЕГЭ/23.03 (вар 5).py
+++ подготовка к егэ/вариант ЕГЭ/23.03 (вар 5).py
@@ -1,15 +1,14 @@
-p = [2, 4, 6, 8, 10, 12, 14, 16, 18, 20]
-q = [3, 6, 9, 12, 15, 18, 21, 24, 27, 30]
-a = list()
-for num in range(1, 1000):
-    flag = True
-    for x in range(0, 10000):
-        if (not x == num or x in p) and (not x in q or x != num):
-            pass
-        else:
-            flag = False
-            break
-    if flag:
-        a.append(num)
-    print(a)
-print(len(a))
+def f(n):
+    if n > 2:
+        return f(n - 1) + g(n - 2)
+    else:
+        return 1
+
+def g(n):
+    if n > 2:
+        return g(n - 1) + f(n - 2)
+    else:
+        return 1
+
+
+print(f(7))
