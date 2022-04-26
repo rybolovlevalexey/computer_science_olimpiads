926d085dbc35e160f3b0e5abd5ca2b6daf1b5ec4
Rybolovlev Alexey <rybolovlevalexey@yandex.ru>
2022-04-26 09:22:09 +0500

    норм

--- подготовка к егэ/вариант ЕГЭ/23.03 (вар 5).py
+++ подготовка к егэ/вариант ЕГЭ/23.03 (вар 5).py
@@ -1,13 +1,8 @@
+num = 9**8 + 3**5 - 9
+print(num)
 cnt = 0
-sp = ['л', 'е', 'в', 'и', 'й']
-for i1 in sp:
-    for i2 in sp:
-        for i3 in sp:
-            for i4 in sp:
-                for i5 in sp:
-                    st = i1 + i2 + i3 + i4 + i5
-                    if st.count('л') == 1 and st.count('е') == 1 and st.count('в') == 1 and \
-                            st.count('и') == 1 and st.count('й') == 1 and st[0] != 'й' and \
-                            st.count('еи') == 0:
-                        cnt += 1
+while num > 0:
+    if num % 3 == 2:
+        cnt += 1
+    num //= 3
 print(cnt)
