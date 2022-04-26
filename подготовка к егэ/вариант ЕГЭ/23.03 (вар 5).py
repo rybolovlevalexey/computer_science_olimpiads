37b4448355f45b363c7decd5c12a98395f8e15c9
Rybolovlev Alexey <rybolovlevalexey@yandex.ru>
2022-04-26 09:01:45 +0500

    норм

--- подготовка к егэ/вариант ЕГЭ/23.03 (вар 5).py
+++ подготовка к егэ/вариант ЕГЭ/23.03 (вар 5).py
@@ -1,6 +1,13 @@
-s = 3
-k = 1
-while k < 25:
-    s += k
-    k += 2
-print(s)
+cnt = 0
+sp = ['л', 'е', 'в', 'и', 'й']
+for i1 in sp:
+    for i2 in sp:
+        for i3 in sp:
+            for i4 in sp:
+                for i5 in sp:
+                    st = i1 + i2 + i3 + i4 + i5
+                    if st.count('л') == 1 and st.count('е') == 1 and st.count('в') == 1 and \
+                            st.count('и') == 1 and st.count('й') == 1 and st[0] != 'й' and \
+                            st.count('еи') == 0:
+                        cnt += 1
+print(cnt)
