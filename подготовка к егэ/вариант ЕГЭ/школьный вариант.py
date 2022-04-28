df9487e075d931534610fee110639b99a0d01462
Rybolovlev Alexey <rybolovlevalexey@yandex.ru>
2022-04-28 21:49:58 +0500

    норм

--- подготовка к егэ/вариант ЕГЭ/школьный вариант.py
+++ подготовка к егэ/вариант ЕГЭ/школьный вариант.py
@@ -1,24 +1,10 @@
-file = open('zadanie_26.txt', 'r')
-sp = file.read().strip().split('\n')
-s, n = map(int, sp[0].split())
+# i and i+5
+file = open('zadanie_27_B.txt', 'r')
+sp = list(map(int, file.read().strip().split('\n')))
+n = sp[0]
 del sp[0]
-sp = sorted(map(int, sp))
-cnt = 0
-summa = 0
-
-for i in range(len(sp)):
-    elem = sp[i]
-    if summa + elem > s:
-        break
-    else:
-        summa += elem
-    cnt += 1
-print(sp[i])
-print(cnt, summa)
-
-mxsm = summa - sp[i]
-for j in range(i, len(sp)):
-    if sp[j] + mxsm <= s and sp[j + 1] + mxsm > s:
-        print(sp[j])
-        break
-print(mxsm + sp[j])
+sp1 = sorted(sp)
+print(n)
+print(sp1[:100])
+print(sp1[0], sp1[1], sp1[2], sp1[3])
+print(sp.index(sp1[0]), sp.index(sp1[1]), sp.index(sp1[2]), sp.index(sp1[3]))
