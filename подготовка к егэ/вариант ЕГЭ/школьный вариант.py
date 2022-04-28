4320f9b2851909020caf144947c529c44ca65a51
Rybolovlev Alexey <rybolovlevalexey@yandex.ru>
2022-04-28 20:50:16 +0500

    норм

--- подготовка к егэ/вариант ЕГЭ/школьный вариант.py
+++ подготовка к егэ/вариант ЕГЭ/школьный вариант.py
@@ -1,12 +1,24 @@
-for num in range(190061, 190080 + 1):
-    delit = set()
-    delit.add(1)
-    if num % 2 != 0:
-        delit.add(num)
-    for d in range(3, num // 2 + 1, 2):
-        if num % d == 0:
-            delit.add(d)
-        if len(delit) > 5:
-            break
-    if len(delit) == 4:
-        print(num, delit)
+file = open('zadanie_26.txt', 'r')
+sp = file.read().strip().split('\n')
+s, n = map(int, sp[0].split())
+del sp[0]
+sp = sorted(map(int, sp))
+cnt = 0
+summa = 0
+
+for i in range(len(sp)):
+    elem = sp[i]
+    if summa + elem > s:
+        break
+    else:
+        summa += elem
+    cnt += 1
+print(sp[i])
+print(cnt, summa)
+
+mxsm = summa - sp[i]
+for j in range(i, len(sp)):
+    if sp[j] + mxsm <= s and sp[j + 1] + mxsm > s:
+        print(sp[j])
+        break
+print(mxsm + sp[j])
