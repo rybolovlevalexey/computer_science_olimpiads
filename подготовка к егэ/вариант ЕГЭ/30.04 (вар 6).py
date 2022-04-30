1718ef67e467c7fde1ec3515b9478e44afeac7ec
Rybolovlev Alexey <rybolovlevalexey@yandex.ru>
2022-04-30 23:13:31 +0500

    норм

--- подготовка к егэ/вариант ЕГЭ/30.04 (вар 6).py
+++ подготовка к егэ/вариант ЕГЭ/30.04 (вар 6).py
@@ -1,3 +1,10 @@
-for num in range(1, 100):
-    if num % 9 == 4:
-        print(num, num % 3)
+sp = list()
+def f(n):
+    sp.append(n)
+    if n > 2:
+        f(n - 3)
+        f(n - 2)
+        f(n - 1)
+
+f(4)
+print(sp)
