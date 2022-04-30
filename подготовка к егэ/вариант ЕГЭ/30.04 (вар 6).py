a1f6cb8ab558d5b19ed0ff58e1b4438dba25bac3
Rybolovlev Alexey <rybolovlevalexey@yandex.ru>
2022-04-30 23:09:38 +0500

    норм

--- подготовка к егэ/вариант ЕГЭ/30.04 (вар 6).py
+++ подготовка к егэ/вариант ЕГЭ/30.04 (вар 6).py
@@ -1,7 +1,3 @@
-st = 54 * '5' + '7'
-while '722' in st or '557' in st:
-    if '722' in st:
-        st = st.replace('722', '57', 1)
-    else:
-        st = st.replace('557', '72', 1)
-print(st)
+for num in range(1, 100):
+    if num % 9 == 4:
+        print(num, num % 3)
