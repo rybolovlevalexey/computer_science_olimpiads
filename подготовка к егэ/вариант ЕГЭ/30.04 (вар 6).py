f0d21d31f2373694f6317b618651ebb973c26ad0
Rybolovlev Alexey <rybolovlevalexey@yandex.ru>
2022-04-30 22:42:47 +0500

    норм

--- подготовка к егэ/вариант ЕГЭ/30.04 (вар 6).py
+++ подготовка к егэ/вариант ЕГЭ/30.04 (вар 6).py
@@ -1,6 +1,10 @@
-s = 5
-k = 0
-while k < 15:
-    k += 2
-    s += k
-print(s)
+cnt = 0
+for i1 in [1, 2, 3, 4]:
+    for i2 in [1, 2, 3, 4]:
+        for i3 in [1, 2, 3, 4]:
+            for i4 in [1, 2, 3, 4]:
+                for i5 in [1, 2, 3, 4]:
+                    st = str(i1) + str(i2) + str(i3) + str(i4) + str(i5)
+                    if st.count('1') == 2:
+                        cnt += 1
+print(cnt)
