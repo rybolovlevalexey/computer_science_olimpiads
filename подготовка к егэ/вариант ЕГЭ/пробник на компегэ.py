a2610e83badf4d9e374d1c1e03db7a7a3e9ce0fb
Rybolovlev Alexey <rybolovlevalexey@yandex.ru>
2022-05-02 18:21:15 +0500

    норм

--- подготовка к егэ/вариант ЕГЭ/пробник на компегэ.py
+++ подготовка к егэ/вариант ЕГЭ/пробник на компегэ.py
@@ -1,9 +1,10 @@
-print('x y z w res')
-for x in [True, False]:
-    for y in [True, False]:
-        for z in [True, False]:
-            for w in [True, False]:
-                res = (y and x) == (not y or w) or not z
-                sp = [int(x), int(y), int(z), int(w)]
-                if not res and sum(sp) <= 2:
-                    print(int(x), int(y), int(z), int(w), int(res))
+for num in range(1, 1000):
+    n = num
+    r = bin(n)[2:]
+    if r.count('1') % 2 == 0:
+        r = '1' + r + '00'
+    else:
+        r = '11' + r
+    if int(r, 2) >= 412:
+        print(num)
+        break
