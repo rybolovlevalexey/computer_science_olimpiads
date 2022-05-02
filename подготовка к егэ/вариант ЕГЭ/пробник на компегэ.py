ffe23312bbb19e30312ff5c20ac6fd038c2c35a9
Rybolovlev Alexey <rybolovlevalexey@yandex.ru>
2022-05-02 18:28:55 +0500

    норм

--- подготовка к егэ/вариант ЕГЭ/пробник на компегэ.py
+++ подготовка к егэ/вариант ЕГЭ/пробник на компегэ.py
@@ -1,10 +1,10 @@
-for num in range(1, 1000):
-    n = num
-    r = bin(n)[2:]
-    if r.count('1') % 2 == 0:
-        r = '1' + r + '00'
-    else:
-        r = '11' + r
-    if int(r, 2) >= 412:
+for num in range(-1000, 10000):
+    s = num
+    s = (s + 31) // 26
+    n = 813
+    while s > 0:
+        n = n // 3
+        s = s - n
+    if n == 30:
         print(num)
         break
