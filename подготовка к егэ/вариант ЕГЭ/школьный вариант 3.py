2e1aabd3701e304e61dc780be998fa0c6445f675
Rybolovlev Alexey <rybolovlevalexey@yandex.ru>
2022-05-04 17:39:55 +0500

    норм

--- подготовка к егэ/вариант ЕГЭ/школьный вариант 3.py
+++ подготовка к егэ/вариант ЕГЭ/школьный вариант 3.py
@@ -1,10 +1,12 @@
-file = open('zadanie_17.txt', 'r')
-sp = map(int, file.read().strip().split('\n'))
-cnt = 0
 ans = 0
-for num in sp:
-    if num % 5 == 0 and num % 6 != 0 and num % 10 != 0 and num % 15 != 0 and num % 16 != 0:
-        cnt += 1
-        if ans == 0 or ans < num:
-            ans = num
-print(cnt, ans)
+for num in range(1, 100000):
+    x = num
+    a = b = 0
+    while x > 0:
+        a += 1
+        b = b + (x % 100)
+        x = x // 100
+    if a == 2 and b == 15:
+        ans = num
+        print(num)
+print(ans)
