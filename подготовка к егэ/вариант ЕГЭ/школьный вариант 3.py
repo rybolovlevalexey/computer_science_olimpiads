b25e807cc67cec02bfc9fd195585d3510d7245ca
Rybolovlev Alexey <rybolovlevalexey@yandex.ru>
2022-05-04 11:11:09 +0500

    норм

--- подготовка к егэ/вариант ЕГЭ/школьный вариант 3.py
+++ подготовка к егэ/вариант ЕГЭ/школьный вариант 3.py
@@ -1,13 +1,11 @@
-for a in range(0, 1000):
-    flag = True
-    for x in range(0, 10000):
-        for y in range(0, 10000):
-            res = (3*x + 5*y < a) or (x >= y) or (y > 8)
-            if not res:
-                flag = False
-                break
-        if not flag:
-            break
-    if flag:
-        print(a)
-        break
+def f(n):
+    if n < 4:
+        return n - 1
+    elif n >= 4 and n % 3 == 0:
+        return n + 2*f(n - 1)
+    elif n >= 4 and n % 3 != 0:
+        return f(n - 2) + f(n - 3)
+
+res = f(25)
+print(res)
+print(sum(list(map(int, list(str(res))))))
