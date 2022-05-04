9fe5b3abf02312ad4972f11cb08eeb89f0acd584
Rybolovlev Alexey <rybolovlevalexey@yandex.ru>
2022-05-04 11:01:16 +0500

    норм

--- подготовка к егэ/вариант ЕГЭ/школьный вариант 3.py
+++ подготовка к егэ/вариант ЕГЭ/школьный вариант 3.py
@@ -1,11 +1,5 @@
-cnt = 0
-sp = ['а', 'р', 'б', 'у', 'з']
-for i1 in sp:
-    for i2 in sp:
-        for i3 in sp:
-            for i4 in sp:
-                for i5 in sp:
-                    st = i1 + i2 + i3 + i4 + i5
-                    if st.count('р') >= 2:
-                        cnt += 1
-print(cnt)
+st = 121 * '563'
+while '56' in st or '3333' in st:
+    st = st.replace('56', '3', 1)
+    st = st.replace('3333', '3', 1)
+print(st)
