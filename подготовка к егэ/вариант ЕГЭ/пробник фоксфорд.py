b4044d1dbc6b7f7af0d2338bf9f39ae06a3b5fa0
Rybolovlev Alexey <rybolovlevalexey@yandex.ru>
2022-05-09 13:43:58 +0500

    норм

--- подготовка к егэ/вариант ЕГЭ/пробник фоксфорд.py
+++ подготовка к егэ/вариант ЕГЭ/пробник фоксфорд.py
@@ -1,8 +1,13 @@
-num = 36**30 + 6**22 + 3 * 6**6 - 25
-cnt = 0
-print(num)
-while num > 0:
-    if num % 6 == 5:
-        cnt += 1
-    num //= 6
-print(cnt)
+for a in range(0, 100):
+    flag = True
+    for x in range(1000):
+        for y in range(1000):
+            res = (x > 4 or x**2 <= a) and (y**2 > a or y <= 4)
+            if not res:
+                flag = False
+                break
+        if not flag:
+            break
+    if flag:
+        print(a)
+        break
