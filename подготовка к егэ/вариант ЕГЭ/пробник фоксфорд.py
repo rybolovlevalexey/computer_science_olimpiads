e1d455275db2dfde901d571cba0f41a3b6c9fb5b
Rybolovlev Alexey <rybolovlevalexey@yandex.ru>
2022-05-09 13:47:33 +0500

    норм

--- подготовка к егэ/вариант ЕГЭ/пробник фоксфорд.py
+++ подготовка к егэ/вариант ЕГЭ/пробник фоксфорд.py
@@ -1,13 +1,10 @@
-for a in range(0, 100):
-    flag = True
-    for x in range(1000):
-        for y in range(1000):
-            res = (x > 4 or x**2 <= a) and (y**2 > a or y <= 4)
-            if not res:
-                flag = False
-                break
-        if not flag:
-            break
-    if flag:
-        print(a)
-        break
+def f(n):
+    if n == 1 or n == 2 or n == 3:
+        return n
+    elif n % 3 == 0 and n > 3:
+        return n + f(n - 1) + f(n - 2) + 3
+    elif n > 3 and n % 3 != 0:
+        return 1 + f(n - 2) + f(n - 1) + n
+
+
+print(f(13))
