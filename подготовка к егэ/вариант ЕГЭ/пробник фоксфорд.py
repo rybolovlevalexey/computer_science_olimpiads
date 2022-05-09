70d4a414f2e40dcd0605e3a608f26850ff584325
Rybolovlev Alexey <rybolovlevalexey@yandex.ru>
2022-05-09 17:05:04 +0500

    норм

--- подготовка к егэ/вариант ЕГЭ/пробник фоксфорд.py
+++ подготовка к егэ/вариант ЕГЭ/пробник фоксфорд.py
@@ -1,27 +1,13 @@
-dp = [0] * 61
-dp[6] = 1
-for i in range(7, 15):
-    if i == 9:
-        continue
-    if i % 6 == 0:
-        dp[i] = dp[i - 1] + dp[i // 2] + dp[i // 3]
-    elif i % 3 == 0:
-        dp[i] = dp[i - 1] + dp[i // 3]
-    elif i % 2 == 0:
-        dp[i] = dp[i - 1] + dp[i // 2]
+file = open('273802.txt', 'r')
+st = file.read().strip()
+
+dl = 1
+mxdl = 0
+for i in range(len(st) - 1):
+    if st[i] != st[i + 1]:
+        dl += 1
     else:
-        dp[i] = dp[i - 1]
-print(dp)
-for i in range(14):
-    dp[i] = 0
-print(dp)
-for i in range(15, 61):
-    if i % 6 == 0:
-        dp[i] = dp[i - 1] + dp[i // 2] + dp[i // 3]
-    elif i % 3 == 0:
-        dp[i] = dp[i - 1] + dp[i // 3]
-    elif i % 2 == 0:
-        dp[i] = dp[i - 1] + dp[i // 2]
-    else:
-        dp[i] = dp[i - 1]
-print(dp)
+        if dl > mxdl:
+            mxdl = dl
+        dl = 1
+print(mxdl, dl)
