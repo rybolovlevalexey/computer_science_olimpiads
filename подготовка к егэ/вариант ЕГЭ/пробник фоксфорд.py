29a9959838279d822e892b71ce1fdbbac93a6678
Rybolovlev Alexey <rybolovlevalexey@yandex.ru>
2022-05-09 16:58:36 +0500

    норм

--- подготовка к егэ/вариант ЕГЭ/пробник фоксфорд.py
+++ подготовка к егэ/вариант ЕГЭ/пробник фоксфорд.py
@@ -1,13 +1,27 @@
-for num in range(1000, 9999):
-    x = num
-    a = 0
-    b = 1
-    while x > 0:
-        if x % 2 == 0:
-            a += x % 8
-        else:
-            b *= x % 8
-        x //= 8
-    if a == 12 and b == 1:
-        print(num)
-        break
+dp = [0] * 61
+dp[6] = 1
+for i in range(7, 15):
+    if i == 9:
+        continue
+    if i % 6 == 0:
+        dp[i] = dp[i - 1] + dp[i // 2] + dp[i // 3]
+    elif i % 3 == 0:
+        dp[i] = dp[i - 1] + dp[i // 3]
+    elif i % 2 == 0:
+        dp[i] = dp[i - 1] + dp[i // 2]
+    else:
+        dp[i] = dp[i - 1]
+print(dp)
+for i in range(14):
+    dp[i] = 0
+print(dp)
+for i in range(15, 61):
+    if i % 6 == 0:
+        dp[i] = dp[i - 1] + dp[i // 2] + dp[i // 3]
+    elif i % 3 == 0:
+        dp[i] = dp[i - 1] + dp[i // 3]
+    elif i % 2 == 0:
+        dp[i] = dp[i - 1] + dp[i // 2]
+    else:
+        dp[i] = dp[i - 1]
+print(dp)
