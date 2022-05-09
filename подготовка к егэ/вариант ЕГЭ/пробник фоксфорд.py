75eb3ad0d76b01e3d096002eb8025a9231bba66b
Rybolovlev Alexey <rybolovlevalexey@yandex.ru>
2022-05-09 13:08:03 +0500

    норм

--- подготовка к егэ/вариант ЕГЭ/пробник фоксфорд.py
+++ подготовка к егэ/вариант ЕГЭ/пробник фоксфорд.py
@@ -1,12 +1,9 @@
-for num in range(1, 1000):
-    n = num
-    r = bin(n)[2:]
-    if n % 2 != 0:
-        r += '11'
-    else:
-        r += '00'
-    r += str(sum(map(int, list(r))) % 2)
-    r = int(r, 2)
-    if r > 177:
-        print(r)
+for num in range(1000000, 0, -1):
+    w = num
+    n = 1
+    while w < 120:
+        w += 15
+        n = n * 2 + 2
+    if n == 94:
+        print(num)
         break
