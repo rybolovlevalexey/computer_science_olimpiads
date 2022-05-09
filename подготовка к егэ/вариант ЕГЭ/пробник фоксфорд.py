cfcfac437e190436b4beee01d9bb4b3d7a135b66
Rybolovlev Alexey <rybolovlevalexey@yandex.ru>
2022-05-09 17:28:58 +0500

    норм

--- подготовка к егэ/вариант ЕГЭ/пробник фоксфорд.py
+++ подготовка к егэ/вариант ЕГЭ/пробник фоксфорд.py
@@ -1,13 +1,12 @@
-file = open('273802.txt', 'r')
-st = file.read().strip()
-
-dl = 1
-mxdl = 0
-for i in range(len(st) - 1):
-    if st[i] != st[i + 1]:
-        dl += 1
-    else:
-        if dl > mxdl:
-            mxdl = dl
-        dl = 1
-print(mxdl, dl)
+n = 0
+num = 700001
+while n < 3:
+    su = 0
+    for d in range(2, int(num**0.5) + 1):
+        if num % d == 0:
+            su += d
+            su += (num // d)
+    if su > 1000000:
+        print(num, su)
+        n += 1
+    num += 1
