e14262f5dbc6329f6bbe07c8b42de3e03d6a2e0a
Rybolovlev Alexey <rybolovlevalexey@yandex.ru>
2022-05-10 12:56:31 +0500

    норм

--- подготовка к егэ/вариант ЕГЭ/10.05 (вар 1).py
+++ подготовка к егэ/вариант ЕГЭ/10.05 (вар 1).py
@@ -1,12 +1,27 @@
-for num in range(101, 1000):
-    x = num
-    L = 2*x - 30
-    M = 2*x + 30
-    while L != M:
-        if L > M:
-            L = L - M
-        else:
-            M = M - L
-    if M == 30:
-        print(num)
-        break
+dp = [0] * 45
+dp[2] = 1
+for i in range(3, 14):
+    if i % 6 == 0:
+        dp[i] = dp[i - 1] + dp[i // 2] + dp[i // 3]
+    elif i % 3 == 0:
+        dp[i] = dp[i - 1] + dp[i // 3]
+    elif i % 2 == 0:
+        dp[i] = dp[i - 1] + dp[i // 2]
+    else:
+        dp[i] = dp[i - 1]
+print(dp)
+for i in range(13):
+    dp[i] = 0
+print(dp)
+for i in range(14, 45):
+    if i == 29:
+        continue
+    if i % 6 == 0:
+        dp[i] = dp[i - 1] + dp[i // 2] + dp[i // 3]
+    elif i % 3 == 0:
+        dp[i] = dp[i - 1] + dp[i // 3]
+    elif i % 2 == 0:
+        dp[i] = dp[i - 1] + dp[i // 2]
+    else:
+        dp[i] = dp[i - 1]
+print(dp)
