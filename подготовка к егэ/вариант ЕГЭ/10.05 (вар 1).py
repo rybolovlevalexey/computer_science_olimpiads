62763ebf7198dd4e775a4892b48c2c97d8ca6d88
Rybolovlev Alexey <rybolovlevalexey@yandex.ru>
2022-05-10 12:45:51 +0500

    норм

--- подготовка к егэ/вариант ЕГЭ/10.05 (вар 1).py
+++ подготовка к егэ/вариант ЕГЭ/10.05 (вар 1).py
@@ -1,13 +1,12 @@
-file = open('17 (6).txt', 'r')
-sp = list(map(int, file.read().strip().split('\n')))
-cnt = 0
-mxsm = 0
-for i in range(len(sp) - 2):
-    c = max(sp[i], sp[i + 1], sp[i + 2])
-    a = min(sp[i], sp[i + 1], sp[i + 2])
-    b = sum([sp[i], sp[i + 1], sp[i + 2]]) - a - c
-    if c**2 < a**2 + b**2:
-        cnt += 1
-        if (a + b + c) > mxsm:
-            mxsm = a + b + c
-print(cnt, mxsm)
+for num in range(101, 1000):
+    x = num
+    L = 2*x - 30
+    M = 2*x + 30
+    while L != M:
+        if L > M:
+            L = L - M
+        else:
+            M = M - L
+    if M == 30:
+        print(num)
+        break
