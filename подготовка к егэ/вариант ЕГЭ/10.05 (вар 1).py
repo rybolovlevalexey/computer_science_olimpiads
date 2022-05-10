c43b357b6e981672551ac04cbf91cd753dfe653f
Rybolovlev Alexey <rybolovlevalexey@yandex.ru>
2022-05-10 09:57:37 +0500

    норм

--- подготовка к егэ/вариант ЕГЭ/10.05 (вар 1).py
+++ подготовка к егэ/вариант ЕГЭ/10.05 (вар 1).py
@@ -1,10 +1,11 @@
-for num in range(100000, 0, -1):
-    s = num
-    s = s // 10
-    n = 1
-    while s < 51:
-        s += 5
-        n = n * 2
-    if n == 64:
-        print(num)
-        break
+sp = ['и', 'в', 'а', 'н']
+ans = 0
+for i1 in sp:
+    for i2 in sp:
+        for i3 in sp:
+            for i4 in sp:
+                for i5 in sp:
+                    st = i1 + i2 + i3 + i4 + i5
+                    if st.count('и') >= 1:
+                        ans += 1
+print(ans)
