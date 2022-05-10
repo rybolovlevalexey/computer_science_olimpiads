4fafa6fe15d1e1595758cb452e350e99912095fc
Rybolovlev Alexey <rybolovlevalexey@yandex.ru>
2022-05-10 11:08:25 +0500

    норм

--- подготовка к егэ/вариант ЕГЭ/10.05 (вар 1).py
+++ подготовка к егэ/вариант ЕГЭ/10.05 (вар 1).py
@@ -1,12 +1,12 @@
-for a in range(1000):
-    flag = True
-    for x in range(1, 1000):
-        for y in range(1, 1000):
-            res = (x > 9 or x**2 <= a) and (y**2 > a or y <= 9)
-            if not res:
-                flag = False
-                break
-        if not flag:
-            break
-    if flag:
-        print(a)
+sp = list()
+
+
+def f(n):
+    if n > 0:
+        f(n // 3)
+        sp.append(n)
+        f(n - 3)
+
+
+f(9)
+print(''.join(map(str, sp)))
