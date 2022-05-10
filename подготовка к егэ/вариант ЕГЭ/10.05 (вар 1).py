072423ebcd640d302d8a6c1a8fdf1a72dd9f170d
Rybolovlev Alexey <rybolovlevalexey@yandex.ru>
2022-05-10 17:19:11 +0500

    норм

--- подготовка к егэ/вариант ЕГЭ/10.05 (вар 1).py
+++ подготовка к егэ/вариант ЕГЭ/10.05 (вар 1).py
@@ -1,4 +1,4 @@
-file = open('27-A.txt', 'r')
+file = open('27-B.txt', 'r')
 sp = file.read().strip().split('\n')
 n = int(sp[0])
 del sp[0]
@@ -6,9 +6,11 @@ sp = sorted(list(map(int, sp)))[::-1]
 ostatki = list()
 for elem in sp:
     ostatki.append(elem % 3)
-#print(ostatki)
-#print(sp)
+print(ostatki[:8])
+print(sp[:8])
 d = dict()
 for i in range(n):
     d[sp[i]] = ostatki[i]
-print(d)
+#print(d)
+print(sp[0] + sp[2] + sp[5], (sp[0] + sp[2] + sp[5]) % 3)
+print(sp[1] + sp[3] + sp[4], (sp[1] + sp[3] + sp[4]) % 3)
