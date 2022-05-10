d4911420fed01638b17621443b509a1895ac3c33
Rybolovlev Alexey <rybolovlevalexey@yandex.ru>
2022-05-10 10:58:33 +0500

    норм

--- подготовка к егэ/вариант ЕГЭ/10.05 (вар 1).py
+++ подготовка к егэ/вариант ЕГЭ/10.05 (вар 1).py
@@ -1,9 +1,12 @@
-for osn in range(2, 10):
-    st = ''
-    num = 50
-    while num > 0:
-        st += str(num % osn)
-        num //= osn
-    if len(st) == 3:
-        print(osn)
-        break
+for a in range(1000):
+    flag = True
+    for x in range(1, 1000):
+        for y in range(1, 1000):
+            res = (x > 9 or x**2 <= a) and (y**2 > a or y <= 9)
+            if not res:
+                flag = False
+                break
+        if not flag:
+            break
+    if flag:
+        print(a)
