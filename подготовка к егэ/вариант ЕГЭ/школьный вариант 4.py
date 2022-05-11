117b69491742b1c4a4dee0a265c5a5928c7dc01f
Rybolovlev Alexey <rybolovlevalexey@yandex.ru>
2022-05-11 11:22:38 +0500

    норм

--- подготовка к егэ/вариант ЕГЭ/школьный вариант 4.py
+++ подготовка к егэ/вариант ЕГЭ/школьный вариант 4.py
@@ -1,10 +1,15 @@
-for a in range(1, 100):
-    flag = True
-    for x in range(1, 10000):
-        res = (x % a != 0) or (x % 28 != 0) or (x % 42 == 0)
-        if not res:
-            flag = False
-            break
-    if flag:
-        print(a)
-        break
+def f(n):
+    if n < 14:
+        return 3*n + 2*n + 1
+    elif n > 13 and n % 3 == 0:
+        return f(n - 1) + 4*n - 3
+    else:
+        return f(n - 2) + 3*n + 6
+
+
+ans = 0
+for num in range(1, 1000 + 1):
+    res = f(num)
+    if len(list(filter(lambda x: x % 2 == 1, map(int, list(str(res)))))) == len(str(res)):
+        ans += 1
+print(ans)
