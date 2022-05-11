d6d1da81a0c7bbcf3aaf5967e175539b24aa1997
Rybolovlev Alexey <rybolovlevalexey@yandex.ru>
2022-05-11 10:24:32 +0500

    норм

--- подготовка к егэ/вариант ЕГЭ/школьный вариант 4.py
+++ подготовка к егэ/вариант ЕГЭ/школьный вариант 4.py
@@ -1,6 +1,16 @@
-print('a b c res')
-for a in [True, False]:
-    for b in [True, False]:
-        for c in [True, False]:
-            res = not a or (b and not c)
-            print(int(a), int(b), int(c), int(res))
+for ans in range(10000):
+    n = ans
+    r = bin(n)[2:]
+    if len(r) < 8:
+        r = '0' * (8 - len(r)) + r
+    num = ''
+    for elem in r:
+        if elem == '1':
+            num += '0'
+        else:
+            num += '1'
+    num = int(num, 2)
+    res = num - n
+    if res == 113:
+        print(ans)
+        break
