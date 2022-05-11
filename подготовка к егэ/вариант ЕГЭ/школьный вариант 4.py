2e9f3c0cc6c4694d649f25c09ed5113a05c2643b
Rybolovlev Alexey <rybolovlevalexey@yandex.ru>
2022-05-11 11:05:04 +0500

    норм

--- подготовка к егэ/вариант ЕГЭ/школьный вариант 4.py
+++ подготовка к егэ/вариант ЕГЭ/школьный вариант 4.py
@@ -1,11 +1,7 @@
-sp = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h']
-num = 1
-for i1 in sp:
-    for i2 in sp:
-        for i3 in sp:
-            for i4 in sp:
-                st = i1 + i2 + i3 + i4
-                if st == 'ddfa':
-                    print(num)
-                    break
-                num += 1
+st = 156 * '8'
+while '77' in st or '888' in st:
+    if '77' in st:
+        st = st.replace('77', '88', 1)
+    else:
+        st = st.replace('888', '7', 1)
+print(st)
