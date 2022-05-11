c46a1d64a3d21eae552c4080880c72fe6fcac45a
Rybolovlev Alexey <rybolovlevalexey@yandex.ru>
2022-05-11 10:33:28 +0500

    норм

--- подготовка к егэ/вариант ЕГЭ/школьный вариант 4.py
+++ подготовка к егэ/вариант ЕГЭ/школьный вариант 4.py
@@ -1,9 +1,11 @@
-for num in range(100000, 0, -1):
-    s = num
-    n = 1
-    while s < 208:
-        s += 20
-        n *= 2
-    if n == 256:
-        print(num)
-        break
+sp = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h']
+num = 1
+for i1 in sp:
+    for i2 in sp:
+        for i3 in sp:
+            for i4 in sp:
+                st = i1 + i2 + i3 + i4
+                if st == 'ddfa':
+                    print(num)
+                    break
+                num += 1
