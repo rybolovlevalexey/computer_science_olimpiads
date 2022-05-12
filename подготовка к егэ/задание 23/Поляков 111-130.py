ac7c2e85f17ab57404af3dcdc1184c178afade66
Rybolovlev Alexey <rybolovlevalexey@yandex.ru>
2022-05-12 22:10:31 +0500

    №23.114, ответ: 3

--- подготовка к егэ/задание 23/Поляков 111-130.py
+++ подготовка к егэ/задание 23/Поляков 111-130.py
@@ -1,33 +1,37 @@
-dp = [0] * 228
+dp = [0] * 29
 dp[1] = 1
 program = {1: ['']}
-for i in range(2, 228):
+for i in range(2, 29):
     program[i] = []
-    if i % 3 == 0 and i - 5 >= 1:
-        dp[i] = dp[i - 1] + dp[i - 5] + dp[i // 3]
+    if i - 2 >= 1 and i % 2 == 0:
+        dp[i] = dp[i - 1] + dp[i - 2] + dp[i // 2]
         for elem in program[i - 1]:
             program[i] += [elem + '1']
-        for elem in program[i - 5]:
+        for elem in program[i - 2]:
             program[i] += [elem + '2']
-        for elem in program[i // 3]:
+        for elem in program[i // 2]:
             program[i] += [elem + '3']
-    elif i % 3 == 0:
-        dp[i] = dp[i - 1] + dp[i // 3]
+    elif i - 2 >= 1:
+        dp[i] = dp[i - 1] + dp[i - 2]
         for elem in program[i - 1]:
             program[i] += [elem + '1']
-        for elem in program[i // 3]:
-            program[i] += [elem + '3']
-    elif i - 5 >= 1:
-        dp[i] = dp[i - 1] + dp[i - 5]
-        for elem in program[i - 1]:
-            program[i] += [elem + '1']
-        for elem in program[i - 5]:
+        for elem in program[i - 2]:
             program[i] += [elem + '2']
+    elif i % 2 == 0:
+        dp[i] = dp[i - 1] + dp[i // 2]
+        for elem in program[i - 1]:
+            program[i] += [elem + '1']
+        for elem in program[i // 2]:
+            program[i] += [elem + '3']
     else:
         dp[i] = dp[i - 1]
         for elem in program[i - 1]:
             program[i] += [elem + '1']
-    program[i] = [min(program[i], key=lambda x: len(x))]
 print(dp)
-print(program)
-print(len(program[227][0]))
+mndl = len(min(program[28], key=lambda x: len(x)))
+print(mndl)
+cnt = 0
+for elem in program[28]:
+    if len(elem) == mndl:
+        cnt += 1
+print(cnt)
