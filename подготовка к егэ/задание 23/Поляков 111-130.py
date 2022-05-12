0c7647cd22fc8acd8ae0b4c406bb4a13650a7c0e
Rybolovlev Alexey <rybolovlevalexey@yandex.ru>
2022-05-12 21:48:35 +0500

    №23.112, ответ: 1157

--- подготовка к егэ/задание 23/Поляков 111-130.py
+++ подготовка к егэ/задание 23/Поляков 111-130.py
@@ -1,16 +1,16 @@
-dp = [0] * 24
-dp[4] = 1
-for i in range(5, 9):
+dp = [0] * 27
+dp[5] = 1
+for i in range(6, 12):
     if i % 3 == 0:
         dp[i] = dp[i - 1] + dp[i - 2] + dp[i // 3]
     else:
         dp[i] = dp[i - 1] + dp[i - 2]
 print(dp)
-for i in range(8):
+for i in range(11):
     dp[i] = 0
 print(dp)
-for i in range(9, 24):
-    if i == 11 or i == 18:
+for i in range(12, 27):
+    if i == 13 or i == 15:
         continue
     if i % 3 == 0:
         dp[i] = dp[i - 1] + dp[i - 2] + dp[i // 3]
