637d18df3e66934f47489ecfac2fc2bcb8eb11e4
Rybolovlev Alexey <rybolovlevalexey@yandex.ru>
2022-05-16 19:18:56 +0500

    №23.116, ответ: 43

--- подготовка к егэ/задание 23/Поляков 111-130.py
+++ подготовка к егэ/задание 23/Поляков 111-130.py
@@ -1,45 +1,27 @@
-dp = [0] * 112
-dp[1] = 1
-program = {1: ['']}
-for i in range(2, 112):
-    program[i] = []
-    if i - 5 >= 1 and i % 3 == 0:
-        dp[i] = dp[i - 1] + dp[i - 5] + dp[i // 3]
-        for elem in program[i - 1]:
-            program[i] += [elem + '1']
-        for elem in program[i - 5]:
-            program[i] += [elem + '2']
-        for elem in program[i // 3]:
-            program[i] += [elem + '3']
-    elif i - 5 >= 1:
-        dp[i] = dp[i - 1] + dp[i - 5]
-        for elem in program[i - 1]:
-            program[i] += [elem + '1']
-        for elem in program[i - 5]:
-            program[i] += [elem + '2']
-    elif i % 3 == 0:
-        dp[i] = dp[i - 1] + dp[i // 3]
-        for elem in program[i - 1]:
-            program[i] += [elem + '1']
-        for elem in program[i // 3]:
-            program[i] += [elem + '3']
-    else:
-        dp[i] = dp[i - 1]
-        for elem in program[i - 1]:
-            program[i] += [elem + '1']
-    if i != 111:
-        mndl = len(min(program[i], key=lambda x: len(x)))
-        sp = list()
-        for elem in program[i]:
-            if len(elem) == mndl:
-                sp.append(elem)
-        program[i] = sp
-print(dp)
-mndl = len(min(program[111], key=lambda x: len(x)))
-print(mndl)
-cnt = 0
-for elem in program[111]:
-    if len(elem) == mndl:
-        cnt += 1
-        print(elem)
-print(cnt)
+deyst = {1: 1, 2: 5}
+results = set()
+
+for i1 in [1, 2, 3]:
+    for i2 in [1, 2, 3]:
+        for i3 in [1, 2, 3]:
+            for i4 in [1, 2, 3]:
+                num = 1
+                if i1 in [1, 2]:
+                    num += deyst[i1]
+                else:
+                    num *= 3
+                if i2 in [1, 2]:
+                    num += deyst[i2]
+                else:
+                    num *= 3
+                if i3 in [1, 2]:
+                    num += deyst[i3]
+                else:
+                    num *= 3
+                if i4 in [1, 2]:
+                    num += deyst[i4]
+                else:
+                    num *= 3
+                results.add(num)
+print(len(results))
+print(results)
