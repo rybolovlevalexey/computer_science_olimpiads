553116128fca386eb3b40628bb4e9ba2726b6973
Rybolovlev Alexey <rybolovlevalexey@yandex.ru>
2022-05-16 19:43:29 +0500

    №23.122, ответ: 56

--- подготовка к егэ/задание 23/Поляков 111-130.py
+++ подготовка к егэ/задание 23/Поляков 111-130.py
@@ -1,8 +1,8 @@
-dp = [0] * 100
-dp[5] = 1
-for i in range(6, len(dp)):
-    dp[i] = dp[i - 2] + dp[i - 5]
+dp = [0] * 100000
+dp[31] = 1
+for i in range(32, len(dp)):
+    dp[i] = dp[i - 2] + dp[i - 5] + dp[i - 4]
 
-    if dp[i] == 34:
+    if dp[i] == 1001:
         print(i)
         break
