20807c12db1ca78e9e195f159d8cddf05cc8c23d
Rybolovlev Alexey <rybolovlevalexey@yandex.ru>
2022-05-16 19:39:56 +0500

    №23.120, ответ: 19

--- подготовка к егэ/задание 23/Поляков 111-130.py
+++ подготовка к егэ/задание 23/Поляков 111-130.py
@@ -1,21 +1,15 @@
-deyst = {1: '+1', 2: '+5', 3: '*3'}
-results = set()
+dp = [0] * 1000
+dp[1] = 1
+for i in range(2, len(dp)):
+    if i - 5 >= 0 and i % 3 == 0:
+        dp[i] = dp[i - 1] + dp[i - 5] + dp[i // 3]
+    elif i - 5 >= 0:
+        dp[i] = dp[i - 1] + dp[i - 5]
+    elif i % 3 == 0:
+        dp[i] = dp[i - 1] + dp[i // 3]
+    else:
+        dp[i] = dp[i - 1]
 
-for i1 in [1, 2, 3]:
-    for i2 in [1, 2, 3]:
-        for i3 in [1, 2, 3]:
-            for i4 in [1, 2, 3]:
-                for i5 in [1, 2, 3]:
-                    for i6 in [1, 2, 3]:
-                        for i7 in [1, 2, 3]:
-                            for i8 in [1, 2, 3]:
-                                num = eval(str(eval(str(eval(str(eval(str(eval(str(eval(str(eval(str(eval('1' + deyst[i1])) + deyst[i2])) + deyst[i3])) + deyst[i4])) + deyst[i5])) + deyst[i6])) + deyst[i7])) + deyst[i8])
-                                results.add(num)
-print(len(results))
-print(results)
-cnt = 0
-for elem in results:
-    if 1000 <= elem <= 1024:
-        cnt += 1
-        print(elem)
-print(cnt)
+    if dp[i] == 175:
+        print(i)
+        break
