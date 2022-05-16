5a55ef471b8831c9569ebe6834215a7a315a8d85
Rybolovlev Alexey <rybolovlevalexey@yandex.ru>
2022-05-16 21:22:07 +0500

    вариант 4, №25, ответ: 1 249001 1 499 2 251001 7 501

--- подготовка к егэ/вариант ЕГЭ/школьный вариант 4.py
+++ подготовка к егэ/вариант ЕГЭ/школьный вариант 4.py
@@ -1,20 +1,16 @@
-file = open('zadanie_24.txt', 'r')
-st = file.read().strip()
-dl = 1
-mxdl = 0
-ans = 0
+ans = list()
+pornom = 1
 
-for i in range(1, len(st)):
-    if st[i - 1] == st[i]:
-        dl += 1
-    else:
-        if dl > mxdl:
-            mxdl = dl
-            ans = st[i - 1]
-        dl = 1
-print(mxdl, ans)
-if dl > mxdl:
-    mxdl = dl
-    ans = st[-1]
-print(mxdl, ans)
-print(st.count('LLLLL'))
+for num in range(248015, 251575 + 1, 2):
+    cntd = set()
+    midd = 0
+    for d in range(2, int(num**0.5) + 1):
+        if num % d == 0 and num // d != d:
+            cntd.add(num // d)
+            cntd.add(d)
+        elif d * d == num:
+            cntd.add(d)
+            midd = d
+    if len(cntd) % 2 == 1:
+        print(pornom, num, len(cntd), midd)
+        pornom += 1
