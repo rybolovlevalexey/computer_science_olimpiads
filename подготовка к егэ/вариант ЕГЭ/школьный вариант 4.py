4b41c6e1b0b6ff0cbef5cf4b3ad3f09073f9ba23
Rybolovlev Alexey <rybolovlevalexey@yandex.ru>
2022-05-16 21:12:24 +0500

    вариант 4, №24, ответ: L 5

--- подготовка к егэ/вариант ЕГЭ/школьный вариант 4.py
+++ подготовка к егэ/вариант ЕГЭ/школьный вариант 4.py
@@ -1,44 +1,20 @@
-sp = [0] * 51
-sp[50] = 1
-for i in range(49, 29, -1):
-    if i + 3 <= 50:
-        sp[i] = sp[i + 3] + sp[i + 2]
-    elif i + 2 <= 50:
-        sp[i] = sp[i + 2]
+file = open('zadanie_24.txt', 'r')
+st = file.read().strip()
+dl = 1
+mxdl = 0
+ans = 0
+
+for i in range(1, len(st)):
+    if st[i - 1] == st[i]:
+        dl += 1
     else:
-        sp[i] = 0
-
-    if i * 2 + 1 <= 50:
-        sp[i] += sp[i * 2 + 1]
-        if sp[i * 2 + 1] != 0:
-            print('i am here')
-    if i * 2 <= 50:
-        sp[i] += sp[i * 2]
-        if sp[i * 2] != 0:
-            print('i am here')
-print(sp)
-print(sp[30])
-for i in range(50, 30, -1):
-    sp[i] = 0
-print(sp)
-for i in range(29, 17, -1):
-    if i == 23:
-        continue
-    if i + 3 <= 50:
-        sp[i] = sp[i + 3] + sp[i + 2]
-    elif i + 2 <= 50:
-        sp[i] = sp[i + 2]
-    else:
-        sp[i] = 0
-
-    if i * 2 + 1 <= 50:
-        sp[i] += sp[i * 2 + 1]
-        if sp[i * 2 + 1] != 0:
-            print('i am here')
-    if i * 2 <= 50:
-        sp[i] += sp[i * 2]
-        if sp[i * 2] != 0:
-            print('i am here')
-
-print(sp)
-print(sp[18])
+        if dl > mxdl:
+            mxdl = dl
+            ans = st[i - 1]
+        dl = 1
+print(mxdl, ans)
+if dl > mxdl:
+    mxdl = dl
+    ans = st[-1]
+print(mxdl, ans)
+print(st.count('LLLLL'))
