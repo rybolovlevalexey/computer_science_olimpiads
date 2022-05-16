9055a3cddf2a8c09ca14986c20ae6b5f645d25bb
Rybolovlev Alexey <rybolovlevalexey@yandex.ru>
2022-05-16 19:35:48 +0500

    №23.119, ответ: 1

--- подготовка к егэ/задание 23/Поляков 111-130.py
+++ подготовка к егэ/задание 23/Поляков 111-130.py
@@ -1,3 +1,4 @@
+deyst = {1: '+1', 2: '+5', 3: '*3'}
 results = set()
 
 for i1 in [1, 2, 3]:
@@ -6,37 +7,15 @@ for i1 in [1, 2, 3]:
             for i4 in [1, 2, 3]:
                 for i5 in [1, 2, 3]:
                     for i6 in [1, 2, 3]:
-                        num = 1
-                        if i1 in [1, 2]:
-                            num += i1
-                        else:
-                            num *= 2
-                        if i2 in [1, 2]:
-                            num += i2
-                        else:
-                            num *= 2
-                        if i3 in [1, 2]:
-                            num += i3
-                        else:
-                            num *= 2
-                        if i4 in [1, 2]:
-                            num += i4
-                        else:
-                            num *= 2
-                        if i5 in [1, 2]:
-                            num += i5
-                        else:
-                            num *= 2
-                        if i6 in [1, 2]:
-                            num += i6
-                        else:
-                            num *= 2
-
-                        results.add(num)
+                        for i7 in [1, 2, 3]:
+                            for i8 in [1, 2, 3]:
+                                num = eval(str(eval(str(eval(str(eval(str(eval(str(eval(str(eval(str(eval('1' + deyst[i1])) + deyst[i2])) + deyst[i3])) + deyst[i4])) + deyst[i5])) + deyst[i6])) + deyst[i7])) + deyst[i8])
+                                results.add(num)
 print(len(results))
 print(results)
 cnt = 0
 for elem in results:
-    if 34 <= elem <= 59:
+    if 1000 <= elem <= 1024:
         cnt += 1
+        print(elem)
 print(cnt)
