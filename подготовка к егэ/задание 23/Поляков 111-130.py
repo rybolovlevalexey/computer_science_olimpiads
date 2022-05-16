c74a67b3bcff4e557cfb716a15e75e4b0bef472a
Rybolovlev Alexey <rybolovlevalexey@yandex.ru>
2022-05-16 19:22:03 +0500

    №23.117, ответ: 393

--- подготовка к егэ/задание 23/Поляков 111-130.py
+++ подготовка к егэ/задание 23/Поляков 111-130.py
@@ -5,23 +5,38 @@ for i1 in [1, 2, 3]:
     for i2 in [1, 2, 3]:
         for i3 in [1, 2, 3]:
             for i4 in [1, 2, 3]:
-                num = 1
-                if i1 in [1, 2]:
-                    num += deyst[i1]
-                else:
-                    num *= 3
-                if i2 in [1, 2]:
-                    num += deyst[i2]
-                else:
-                    num *= 3
-                if i3 in [1, 2]:
-                    num += deyst[i3]
-                else:
-                    num *= 3
-                if i4 in [1, 2]:
-                    num += deyst[i4]
-                else:
-                    num *= 3
-                results.add(num)
+                for i5 in [1, 2, 3]:
+                    for i6 in [1, 2, 3]:
+                        for i7 in [1, 2, 3]:
+                            num = 1
+                            if i1 in [1, 2]:
+                                num += deyst[i1]
+                            else:
+                                num *= 3
+                            if i2 in [1, 2]:
+                                num += deyst[i2]
+                            else:
+                                num *= 3
+                            if i3 in [1, 2]:
+                                num += deyst[i3]
+                            else:
+                                num *= 3
+                            if i4 in [1, 2]:
+                                num += deyst[i4]
+                            else:
+                                num *= 3
+                            if i5 in [1, 2]:
+                                num += deyst[i5]
+                            else:
+                                num *= 3
+                            if i6 in [1, 2]:
+                                num += deyst[i6]
+                            else:
+                                num *= 3
+                            if i7 in [1, 2]:
+                                num += deyst[i7]
+                            else:
+                                num *= 3
+                            results.add(num)
 print(len(results))
 print(results)
