c7668271e05c0576bb5cc246a8cba512c594955a
Rybolovlev Alexey <rybolovlevalexey@yandex.ru>
2022-05-18 21:43:30 +0500

    вариат 5

--- подготовка к егэ/вариант ЕГЭ/школьный вариант 5.py
+++ подготовка к егэ/вариант ЕГЭ/школьный вариант 5.py
@@ -1,14 +1,7 @@
-cnt = 1
-ans = ''
-sp = ['a', 'b', 'c', 'd', 'e', 'f', 'g']
-for i1 in sp:
-    for i2 in sp:
-        for i3 in sp:
-            for i4 in sp:
-                for i5 in sp:
-                    st = i1 + i2 + i3 + i4 + i5
-                    if cnt == 12769:
-                        ans = st
-                        print(ans)
-                    cnt += 1
-print(ans)
+st = 193 * '5'
+while '333' in st or '555' in st:
+    if '555' in st:
+        st = st.replace('555', '3', 1)
+    else:
+        st = st.replace('333', '5', 1)
+print(st)
