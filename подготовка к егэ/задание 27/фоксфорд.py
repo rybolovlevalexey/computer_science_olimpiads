a46e2775b6f8fd958b8fb9d6e304059c83d1a5ce
Rybolovlev Alexey <rybolovlevalexey@yandex.ru>
2022-05-18 22:05:53 +0500

    фоксфорд

--- подготовка к егэ/задание 27/фоксфорд.py
+++ подготовка к егэ/задание 27/фоксфорд.py
@@ -1,1 +1,34 @@
-file = open('27-48a.txt', 'r')
+file = open('27-48b (1).txt', 'r')
+sp = file.read().strip().split('\n')
+n = int(sp[0])
+del sp[0]
+chet = {1: 0, 0: 0}
+summa = 0
+i = 0
+mndelta = None
+flag = False
+for elem in sp:
+    a, b = map(int, elem.split())
+    if (mndelta is None or abs(a - b) < mndelta) and a % 2 != b % 2:
+        mndelta = abs(a - b)
+print(mndelta)
+
+for elem in sp:
+    a, b = map(int, elem.split())
+    if not flag and mndelta == abs(a - b):
+        summa += min(a, b)
+        if min(a, b) % 2 == 0:
+            chet[0] += 1
+        else:
+            chet[1] += 1
+        flag = True
+    else:
+        summa += max(a, b)
+        if max(a, b) % 2 == 0:
+            chet[0] += 1
+        else:
+            chet[1] += 1
+
+print(summa, summa % 2)
+print(chet)
+print(mndelta)
