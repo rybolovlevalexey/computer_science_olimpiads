f20ab903daa40b6bcbe0c493c76644cd6de36a85
Rybolovlev Alexey <rybolovlevalexey@yandex.ru>
2022-05-18 11:19:56 +0500

    вариат 5

--- подготовка к егэ/вариант ЕГЭ/школьный вариант 5.py
+++ подготовка к егэ/вариант ЕГЭ/школьный вариант 5.py
@@ -1,9 +1,14 @@
-for num in range(1000):
-    s = num
-    n = 0
-    while s * s < 101:
-        s += 1
-        n += 2
-    if n == 10:
-        print(num)
-        break
+cnt = 1
+ans = ''
+sp = ['a', 'b', 'c', 'd', 'e', 'f', 'g']
+for i1 in sp:
+    for i2 in sp:
+        for i3 in sp:
+            for i4 in sp:
+                for i5 in sp:
+                    st = i1 + i2 + i3 + i4 + i5
+                    if cnt == 12769:
+                        ans = st
+                        print(ans)
+                    cnt += 1
+print(ans)
