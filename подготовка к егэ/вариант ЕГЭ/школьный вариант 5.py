7f26b58029d5e26fd3872c7a0e62d3b0607547e3
Rybolovlev Alexey <rybolovlevalexey@yandex.ru>
2022-05-18 11:11:59 +0500

    вариат 5

--- подготовка к егэ/вариант ЕГЭ/школьный вариант 5.py
+++ подготовка к егэ/вариант ЕГЭ/школьный вариант 5.py
@@ -1,14 +1,9 @@
-cnt = 0
-for num in range(2, 1000):
-    n = num
-    r = bin(n)[2:]
-    summa = sum(map(int, list(r)))
-    r += str(summa % 2)
-    if summa > len(r) - summa - 1:
-        r += '0'
-    else:
-        r += '1'
-    r = int(r, 2)
-    if 50 <= r <= 80:
-        cnt += 1
-print(cnt)
+for num in range(1000):
+    s = num
+    n = 0
+    while s * s < 101:
+        s += 1
+        n += 2
+    if n == 10:
+        print(num)
+        break
