bee2709bb892c5e4b982294ff633ff527cd94714
Rybolovlev Alexey <rybolovlevalexey@yandex.ru>
2022-05-19 12:20:26 +0500

    вариат 5

--- подготовка к егэ/вариант ЕГЭ/школьный вариант 5.py
+++ подготовка к егэ/вариант ЕГЭ/школьный вариант 5.py
@@ -1,26 +1,13 @@
-file = open('zadanie_24.txt', 'r')
-st = file.read().strip()
-dl = 1
-ans = ''
-s = st[0]
-mxdl = 0
+for num in range(78920, 92430 + 1):
+    #if int(num ** 0.5) != num ** 0.5:
+    #    continue
+    sp = set()
+    sp.add(1)
+    sp.add(num)
+    for d in range(2, int(num ** 0.5) + 1):
+        if num % d == 0:
+            sp.add(d)
+            sp.add(num // d)
 
-for i in range(1, len(st)):
-    if st[i] != st[i - 1]:
-        dl += 1
-        s += st[i]
-    else:
-        if dl > mxdl:
-            mxdl = dl
-            ans = s
-        dl = 1
-        s = st[i]
-
-if dl > mxdl:
-    mxdl = dl
-print(mxdl)
-print(ans)
-print(st.count(ans))
-print(st.index(ans))
-ind = st.index(ans)
-print(st[ind: ind + 85])
+    if len(sp) == 5:
+        print(sorted(sp))
