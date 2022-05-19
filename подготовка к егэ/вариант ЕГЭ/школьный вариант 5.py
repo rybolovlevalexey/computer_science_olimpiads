819c8c6814f4f14099ed3a74b35c90c46c54607c
Rybolovlev Alexey <rybolovlevalexey@yandex.ru>
2022-05-19 12:04:22 +0500

    вариат 5

--- подготовка к егэ/вариант ЕГЭ/школьный вариант 5.py
+++ подготовка к егэ/вариант ЕГЭ/школьный вариант 5.py
@@ -1,11 +1,17 @@
-for num in range(100000, 1, -1):
-    x = num
-    a = 0
-    b = 1
-    while x > 0:
-        a += 1
-        b = b + (x % 100)
-        x = x // 10
-    if a == 4 and b == 153:
-        print(num)
-        break
+dp = [0] * 101
+dp[10] = 1
+for i in range(11, 66):
+    if i % 2 == 0:
+        dp[i] = dp[i - 1] + dp[i // 2]
+    else:
+        dp[i] = dp[i - 1] + dp[(i - 1) // 2]
+print(dp)
+for i in range(65):
+    dp[i] = 0
+print(dp)
+for i in range(66, 101):
+    if i % 2 == 0:
+        dp[i] = dp[i - 1] + dp[i // 2]
+    else:
+        dp[i] = dp[i - 1] + dp[(i - 1) // 2]
+print(dp)
