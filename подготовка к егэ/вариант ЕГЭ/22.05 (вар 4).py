933fe49e011707f00a5ea9fd9a423feefee51de9
Rybolovlev Alexey <rybolovlevalexey@yandex.ru>
2022-05-22 22:59:41 +0500

    норм

--- подготовка к егэ/вариант ЕГЭ/22.05 (вар 4).py
+++ подготовка к егэ/вариант ЕГЭ/22.05 (вар 4).py
@@ -1,8 +1,11 @@
-print('x y z w result')
-for x in [True, False]:
-    for y in [True, False]:
-        for z in [True, False]:
-            for w in [True, False]:
-                result = (((x and w) or (w and z)) == ((not z or y) and (not y or x)))
-                if result and y and not z:
-                    print(int(x), int(y), int(z), int(w), int(result))
+for num in range(1000):
+    n = num
+    r = bin(n)[2:]
+    summa = sum(map(int, list(r)))
+    r += str(summa % 2)
+    summa = sum(map(int, list(r)))
+    r += str(summa % 2)
+    ans = int(r, 2)
+    if ans > 97:
+        print(ans)
+        break
