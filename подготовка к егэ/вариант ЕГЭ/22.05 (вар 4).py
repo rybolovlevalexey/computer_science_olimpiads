79d1ca63d5e6dedd00c0373a578e28d6699dc0df
Rybolovlev Alexey <rybolovlevalexey@yandex.ru>
2022-05-22 23:10:38 +0500

    норм

--- подготовка к егэ/вариант ЕГЭ/22.05 (вар 4).py
+++ подготовка к егэ/вариант ЕГЭ/22.05 (вар 4).py
@@ -1,7 +1,25 @@
-num = 49**7 + 7**20 - 28
-cnt = 0
-while num > 0:
-    if num % 7 == 6:
-        cnt += 1
-    num //= 7
-print(cnt)
+dp = [0] * 29
+dp[2] = 1
+for i in range(3, 15):
+    if i % 6 == 0:
+        dp[i] = dp[i - 1] + dp[i // 2] + dp[i // 3]
+    elif i % 2 == 0:
+        dp[i] = dp[i - 1] + dp[i // 2]
+    elif i % 3 == 0:
+        dp[i] = dp[i - 1] + dp[i // 3]
+    else:
+        dp[i] = dp[i - 1]
+print(dp)
+for i in range(14):
+    dp[i] = 0
+print(dp)
+for i in range(15, 29):
+    if i % 6 == 0:
+        dp[i] = dp[i - 1] + dp[i // 2] + dp[i // 3]
+    elif i % 2 == 0:
+        dp[i] = dp[i - 1] + dp[i // 2]
+    elif i % 3 == 0:
+        dp[i] = dp[i - 1] + dp[i // 3]
+    else:
+        dp[i] = dp[i - 1]
+print(dp)
