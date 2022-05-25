531b6f10db8c66fbb4ae29d9b3b078fc3629a68a
Rybolovlev Alexey <rybolovlevalexey@yandex.ru>
2022-05-25 21:40:14 +0500

    норм

--- подготовка к егэ/вариант ЕГЭ/25.05 (вар 5).py
+++ подготовка к егэ/вариант ЕГЭ/25.05 (вар 5).py
@@ -1,12 +1,30 @@
-for num in range(1000000, 2000000 + 1):
-    sp = set()
-    for d in range(1, int(num**0.5)+1):
-        if num % d == 0:
-            d2 = num // d
-            delta = abs(d2 - d)
-            if delta <= 100:
-                sp.add(delta)
-        if len(sp) >= 3:
-            break
-    if len(sp) == 3:
-        print(num)
+file = open('26 (1).txt', 'r')
+sp = file.read().strip().split('\n')
+n, m = map(int, sp[0].split())
+del sp[0]
+summa = 0
+izdA = dict()
+
+for elem in sp:
+    cost, colvo, tip = elem.split()
+    cost = int(cost)
+    colvo = int(colvo)
+    if tip == 'B':
+        summa += (colvo * cost)
+    else:
+        if cost in izdA:
+            izdA[cost] += colvo
+        else:
+            izdA[cost] = colvo
+
+cntA = 0
+for key in sorted(izdA.keys()):
+    if summa + key * izdA[key] <= m:
+        summa += key * izdA[key]
+        cntA += izdA[key]
+    else:
+        for colichestvo in range(izdA[key], 0, -1):
+            if summa + key * colichestvo <= m:
+                cntA += colichestvo
+                summa += key * colichestvo
+print(cntA, m - summa)
