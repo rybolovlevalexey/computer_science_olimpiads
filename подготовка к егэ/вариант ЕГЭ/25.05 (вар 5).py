aff56b472a59fdfac14f6832c26f9cefdaa9e02b
Rybolovlev Alexey <rybolovlevalexey@yandex.ru>
2022-05-25 20:06:38 +0500

    норм

--- подготовка к егэ/вариант ЕГЭ/25.05 (вар 5).py
+++ подготовка к егэ/вариант ЕГЭ/25.05 (вар 5).py
@@ -1,12 +1,13 @@
-file = open('17 (7).txt', 'r')
-sp = list(map(int, file.read().strip().split('\n')))
-cnt = 0
-mxsm = 0
-
-for i in range(len(sp)):
-    for j in range(i + 1, len(sp)):
-        if (sp[i] + sp[j]) % 80 == 0 and (sp[i] % 50 == 0 or sp[j] % 50 == 0):
-            cnt += 1
-            if sp[i] + sp[j] > mxsm:
-                mxsm = sp[i] + sp[j]
-print(cnt, mxsm)
+for num in range(0, 1000):
+    x = num
+    a = 1
+    b = 0
+    while x > 0:
+        c = x % 10
+        a = a * c
+        if c > b:
+            b = c
+        x //= 10
+    if a == 48 and b == 6:
+        print(num)
+        break
