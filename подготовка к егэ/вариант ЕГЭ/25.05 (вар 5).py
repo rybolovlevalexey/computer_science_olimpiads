90b338860ecaa2cf633b043636fac2a65727ab94
Rybolovlev Alexey <rybolovlevalexey@yandex.ru>
2022-05-25 21:15:06 +0500

    норм

--- подготовка к егэ/вариант ЕГЭ/25.05 (вар 5).py
+++ подготовка к егэ/вариант ЕГЭ/25.05 (вар 5).py
@@ -1,28 +1,12 @@
-letter = 'L'
-file = open('zadanie24_2 (1).txt', 'r')
-st = file.read().strip()
-dl = 0
-mxdl = 0
-
-for i in range(0, len(st)):
-    if st[i] == letter:
-        dl += 1
-    else:
-        if dl > mxdl:
-            mxdl = dl
-        if st[i] == 'L':
-            dl = 1
-        else:
-            dl = 0
-
-    if letter == 'L':
-        letter = 'D'
-    elif letter == 'D':
-        letter = 'R'
-    else:
-        letter = 'L'
-
-print(dl)
-print(mxdl)
-print(st.count('LDR' * 5 + 'L'))
-print(st[st.index('LDR' * 5):st.index('LDR' * 5) + 16])
+for num in range(1000000, 2000000 + 1):
+    sp = set()
+    for d in range(1, int(num**0.5)+1):
+        if num % d == 0:
+            d2 = num // d
+            delta = abs(d2 - d)
+            if delta <= 100:
+                sp.add(delta)
+        if len(sp) >= 3:
+            break
+    if len(sp) == 3:
+        print(num)
