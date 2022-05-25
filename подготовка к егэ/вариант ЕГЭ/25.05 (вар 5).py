5a1345a6fe2a24623d36b0d710c602b0a6f23b11
Rybolovlev Alexey <rybolovlevalexey@yandex.ru>
2022-05-25 19:29:24 +0500

    норм

--- подготовка к егэ/вариант ЕГЭ/25.05 (вар 5).py
+++ подготовка к егэ/вариант ЕГЭ/25.05 (вар 5).py
@@ -1,58 +1,15 @@
-ans = 0
-sp = ['г', 'е', 'р', 'а', 'с', 'и', 'м']
-sogl = ['г', 'р', 'с', 'м']
-glas = ['е', 'а', 'и']
-for i1 in sp:
-    for i2 in sp:
-        for i3 in sp:
-            for i4 in sp:
-                for i5 in sp:
-                    for i6 in sp:
-                        for i7 in sp:
-                            st = i1 + i2 + i3 + i4 + i5 + i6 + i7
-                            word = set()
-                            word.add(i1)
-                            word.add(i2)
-                            word.add(i3)
-                            word.add(i4)
-                            word.add(i5)
-                            word.add(i6)
-                            word.add(i7)
-                            pointword = ''
-                            if i1 in glas:
-                                pointword += '1'
-                            else:
-                                pointword += '2'
-
-                            if i2 in glas:
-                                pointword += '1'
-                            else:
-                                pointword += '2'
-
-                            if i3 in glas:
-                                pointword += '1'
-                            else:
-                                pointword += '2'
-
-                            if i4 in glas:
-                                pointword += '1'
-                            else:
-                                pointword += '2'
-
-                            if i5 in glas:
-                                pointword += '1'
-                            else:
-                                pointword += '2'
-
-                            if i6 in glas:
-                                pointword += '1'
-                            else:
-                                pointword += '2'
-
-                            if i7 in glas:
-                                pointword += '1'
-                            else:
-                                pointword += '2'
-                            if len(word) == 7 and pointword.count('11') == 0 and pointword.count('22') == 0:
-                                ans += 1
-print(ans)
+for a in range(0, 1000):
+    flag = True
+    for x in range(0, 10000):
+        for y in range(0, 10000):
+            res = (x*y < a) or (y > x) or (x >= 8)
+            if not res:
+                flag = False
+                break
+            if y > x:
+                break
+        if not flag:
+            break
+    if flag:
+        print(a)
+        break
