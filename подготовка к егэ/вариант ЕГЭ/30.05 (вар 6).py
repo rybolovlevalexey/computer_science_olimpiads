bf7c4be5a91bd7266e91f69a3bfa543f2ca30559
Rybolovlev Alexey <rybolovlevalexey@yandex.ru>
2022-05-30 22:02:17 +0500

    норм

--- подготовка к егэ/вариант ЕГЭ/30.05 (вар 6).py
+++ подготовка к егэ/вариант ЕГЭ/30.05 (вар 6).py
@@ -1,6 +1,11 @@
-s = 500
-n = 200
-while s // n >= 2:
-    s += 5
-    n += 5
-print(s)
+numbers = set()
+for i1 in range(1, 7):
+    for i2 in range(7):
+        for i3 in range(7):
+            for i4 in range(7):
+                for i5 in range(7):
+                    num = int(str(i1) + str(i2) + str(i3) + str(i4) + str(i5))
+                    if len(set(str(num))) == 5 and (i1 % 2 != i2 % 2) and (i2 % 2 != i3 % 2) and (i3 % 2 != i4 % 2) and (i4 % 2 != i5 % 2):
+                        numbers.add(num)
+print(numbers)
+print(len(numbers))
