3f54a93861e3a1c5adac6e564dcf96671ebd8347
Rybolovlev Alexey <rybolovlevalexey@yandex.ru>
2022-06-06 21:15:14 +0500

    июньский вариант 2

---
+++ подготовка к егэ/вариант ЕГЭ/06.06 (вар 2).py
@@ -0,0 +1,6 @@
+print('x y z')
+for x in [True, False]:
+    for y in [True, False]:
+        for z in [True, False]:
+            res = not(z) and x
+            print(int(x), int(y), int(z), int(res))
