9f840befd61d3df6bb58d3370ce20b2153027103
Rybolovlev Alexey <rybolovlevalexey@yandex.ru>
2022-06-06 11:15:03 +0500

    вариант 1

--- подготовка к егэ/вариант ЕГЭ/05.06 (вар 1).py
+++ подготовка к егэ/вариант ЕГЭ/05.06 (вар 1).py
@@ -1,11 +1,15 @@
-sp = list()
+file = open('17 (8).txt', 'r')
+sp = list(map(int, file.read().strip().split('\n')))
+cnt = 0
+chet = list(filter(lambda x: x % 2 == 0, sp))
+sr = sum(chet) / len(chet)
+mxsm = 0
 
-def f(n):
-    if n > 0:
-        f(n // 4)
-        sp.append(n)
-        f(n - 1)
-
-f(5)
-print(sp)
-print(''.join(list(map(str, sp))))
+for i in range(len(sp) - 1):
+    i1 = sp[i]
+    i2 = sp[i + 1]
+    if (i1 % 3 == 0 or i2 % 3 == 0) and (i1 < sr or i2 < sr):
+        cnt += 1
+        if i1 + i2 > mxsm:
+            mxsm = i1 + i2
+print(cnt, mxsm)
