f99df954002d99fd26603603a5e8940a885344b6
Rybolovlev Alexey <rybolovlevalexey@yandex.ru>
2022-06-06 13:13:45 +0500

    вариант 1

--- подготовка к егэ/вариант ЕГЭ/05.06 (вар 1).py
+++ подготовка к егэ/вариант ЕГЭ/05.06 (вар 1).py
@@ -1,12 +1,15 @@
-# XZZY
-file = open('24 (3).txt', 'r')
-st = file.read().strip()
-sp = st.split('XZZY')
-dliny = list()
-for i in range(len(sp)):
-    dliny.append(len(sp[i]))
-print(dliny)
-dlina = max(dliny)
-dlina += 3
-dlina += 3
-print(dlina)
+for num in range(45000000, 50000000 + 1):
+    x = num
+    while x % 2 == 0:
+        x //= 2
+    if int(x ** 0.5) ** 2 != x:
+        continue
+    sp = set()
+    for d in range(3, int(x ** 0.5) + 1, 2):
+        if num % d == 0:
+            sp.add(d)
+            sp.add(x // d)
+        if len(sp) >= 4:
+            break
+    if len(sp) == 3:
+        print(num)
