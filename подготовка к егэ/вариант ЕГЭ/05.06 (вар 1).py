816a7015042c29975a20758d0dc61680e945989e
Rybolovlev Alexey <rybolovlevalexey@yandex.ru>
2022-06-06 10:42:52 +0500

    вариант 1

--- подготовка к егэ/вариант ЕГЭ/05.06 (вар 1).py
+++ подготовка к егэ/вариант ЕГЭ/05.06 (вар 1).py
@@ -1,6 +1,7 @@
-s = 0
-k = 1
-while s < 66:
-    k += 3
-    s += k
-print(k)
+st = 1000 * '9'
+while '999' in st or '888' in st:
+    if '888' in st:
+        st = st.replace('888', '9', 1)
+    else:
+        st = st.replace('999', '8', 1)
+print(st)
