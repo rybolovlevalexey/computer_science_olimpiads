30389cb97c75a0f17f07f83c145e3e7e30059843
Rybolovlev Alexey <rybolovlevalexey@yandex.ru>
2022-06-06 21:27:07 +0500

    июньский вариант 2

--- подготовка к егэ/вариант ЕГЭ/06.06 (вар 2).py
+++ подготовка к егэ/вариант ЕГЭ/06.06 (вар 2).py
@@ -1,6 +1,15 @@
-print('x y z')
-for x in [True, False]:
-    for y in [True, False]:
-        for z in [True, False]:
-            res = not(z) and x
-            print(int(x), int(y), int(z), int(res))
+ans = set()
+for number in range(10, 1000 + 1):
+    n = number
+    r = bin(n)[2:]
+    r = r[1:]
+    while len(r) > 0 and r[0] == '0':
+        r = r[1:]
+    if len(r) > 0:
+        res = int(r, 2)
+    else:
+        res = 0
+    res = n - res
+    ans.add(res)
+print(len(ans))
+print(ans)
