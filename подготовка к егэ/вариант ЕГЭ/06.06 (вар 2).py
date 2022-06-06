29dcd157e5f512a16d1a69a4506fa6c0e8d1d37a
Rybolovlev Alexey <rybolovlevalexey@yandex.ru>
2022-06-06 21:28:24 +0500

    июньский вариант 2

--- подготовка к егэ/вариант ЕГЭ/06.06 (вар 2).py
+++ подготовка к егэ/вариант ЕГЭ/06.06 (вар 2).py
@@ -1,15 +1,6 @@
-ans = set()
-for number in range(10, 1000 + 1):
-    n = number
-    r = bin(n)[2:]
-    r = r[1:]
-    while len(r) > 0 and r[0] == '0':
-        r = r[1:]
-    if len(r) > 0:
-        res = int(r, 2)
-    else:
-        res = 0
-    res = n - res
-    ans.add(res)
-print(len(ans))
-print(ans)
+s = 0
+n = 170
+while s + n < 325:
+    s = s + 25
+    n = n - 5
+print(s)
