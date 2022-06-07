467e779697a513ae81a983d6d755fd52815d90d9
Rybolovlev Alexey <rybolovlevalexey@yandex.ru>
2022-06-07 09:59:14 +0500

    июньский вариант 2

--- подготовка к егэ/вариант ЕГЭ/06.06 (вар 2).py
+++ подготовка к егэ/вариант ЕГЭ/06.06 (вар 2).py
@@ -1,27 +1,8 @@
-d = {10: 'a', 11: 'b', 12: 'c', 13: 'd', 14: 'e', 15: 'f'}
-def five(x):
-    res = ''
-    while x > 0:
-        res += str(x % 5)
-        x //= 5
-    return res[::-1]
-def six(x):
-    res = ''
-    while x > 0:
-        res += str(x % 6)
-        x //= 6
-    return res[::-1]
-def eleven(x):
-    res = ''
-    while x > 0:
-        ost = x % 11
-        if ost >= 10:
-            res += d[ost]
-        else:
-            res += str(ost)
-        x //= 11
-    return res[::-1]
+def f(n):
+    if n == 1:
+        return 1
+    elif n > 1:
+        return f(n - 1) + n
 
-for num in range(1, 10000):
-    if len(six(num)) == 2 and len(five(num)) == 3 and eleven(num)[-1] == '1':
-        print(num)
+
+print(f(40))
