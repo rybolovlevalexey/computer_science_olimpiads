55d18b05961e03498f203ae6cbe1da9ada3343d4
Rybolovlev Alexey <rybolovlevalexey@yandex.ru>
2022-06-07 11:36:17 +0500

    июньский вариант 2

--- подготовка к егэ/вариант ЕГЭ/06.06 (вар 2).py
+++ подготовка к егэ/вариант ЕГЭ/06.06 (вар 2).py
@@ -1,13 +1,22 @@
-dp = [0] * 21
-dp[3] = 1
-for i in range(4, 10):
-    dp[i] = dp[i - 1] + dp[i - 2]
-print(dp)
-for i in range(9):
-    dp[i] = 0
-print(dp)
-for i in range(10, 21):
-    if i == 15:
-        continue
-    dp[i] = dp[i - 1] + dp[i - 2]
-print(dp)
+file = open('24 (4).txt', 'r')
+sp = file.read().strip().split('\n')
+cnt = None
+ansst = ''
+for elem in sp:
+    if cnt is None or elem.count('N') < cnt:
+        cnt = elem.count('N')
+        ansst = elem
+d = dict()
+for let in ansst:
+    if let in d:
+        d[let] += 1
+    else:
+        d[let] = 1
+print(d)
+print(max(d.values()))
+ansd = dict()
+for key, value in d.items():
+    if value == max(d.values()):
+        ansd[key] = value
+print(ansd)
+print(sorted(ansd.keys())[-1])
