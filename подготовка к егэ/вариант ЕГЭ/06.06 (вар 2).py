b72e807ff5b2efe345b68b10e56e77351665321c
Rybolovlev Alexey <rybolovlevalexey@yandex.ru>
2022-06-07 10:14:31 +0500

    июньский вариант 2

--- подготовка к егэ/вариант ЕГЭ/06.06 (вар 2).py
+++ подготовка к егэ/вариант ЕГЭ/06.06 (вар 2).py
@@ -1,8 +1,12 @@
-def f(n):
-    if n == 1:
-        return 1
-    elif n > 1:
-        return f(n - 1) + n
+file = open('107_17.txt', 'r')
+sp = list(map(int, file.read().strip().split()))
+k = min(filter(lambda x: x % 21 == 0, sp))
+cnt = 0
+mxsm = 0
 
-
-print(f(40))
+for i in range(len(sp) - 1):
+    if sp[i] % k == 0 or sp[i + 1] % k == 0:
+        cnt += 1
+        if mxsm < sp[i] + sp[i + 1]:
+            mxsm = sp[i] + sp[i + 1]
+print(cnt, mxsm)
