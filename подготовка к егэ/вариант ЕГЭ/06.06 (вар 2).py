e7ac38177049d39469770ab2d1877f459d450628
Rybolovlev Alexey <rybolovlevalexey@yandex.ru>
2022-06-07 11:56:03 +0500

    июньский вариант 2

--- подготовка к егэ/вариант ЕГЭ/06.06 (вар 2).py
+++ подготовка к егэ/вариант ЕГЭ/06.06 (вар 2).py
@@ -1,13 +1,25 @@
-num = 452021 + 1
-anscnt = 0
-while anscnt < 5:
-    m = 0
-    for d in range(2, int(num**0.5) + 1):
-        if num % d == 0:
-            m += d
-            m += (num // d)
-            break
-    if m % 7 == 3:
-        print(num, m)
-        anscnt += 1
-    num += 1
+file = open('26 (3).txt', 'r')
+sp = file.read().strip().split('\n')
+s, n = map(int, sp[0].split())  # грузоподъёмность, количество
+del sp[0]
+sp = list(sorted(map(int, sp)))
+cnt = 0
+summa = 0
+lastelem = 0
+for elem in sp:
+    if elem + summa <= s:
+        summa += elem
+        cnt += 1
+    if elem + summa > s:
+        lastelem = elem
+        break
+summa -= lastelem
+print(summa)
+ans = 0
+for i in range(len(sp) - 1):
+    if summa + sp[i] <= s:
+        ans = sp[i]
+print(cnt, summa)
+print(ans)
+print(sp[-1])
+print(summa + ans, s - summa - ans)
