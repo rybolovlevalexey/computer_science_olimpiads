ebafa5b23d31dc41d37f0e591e1c9f1f8819f181
Rybolovlev Alexey <rybolovlevalexey@yandex.ru>
2022-06-07 11:46:57 +0500

    июньский вариант 2

--- подготовка к егэ/вариант ЕГЭ/06.06 (вар 2).py
+++ подготовка к егэ/вариант ЕГЭ/06.06 (вар 2).py
@@ -1,22 +1,13 @@
-file = open('24 (4).txt', 'r')
-sp = file.read().strip().split('\n')
-cnt = None
-ansst = ''
-for elem in sp:
-    if cnt is None or elem.count('N') < cnt:
-        cnt = elem.count('N')
-        ansst = elem
-d = dict()
-for let in ansst:
-    if let in d:
-        d[let] += 1
-    else:
-        d[let] = 1
-print(d)
-print(max(d.values()))
-ansd = dict()
-for key, value in d.items():
-    if value == max(d.values()):
-        ansd[key] = value
-print(ansd)
-print(sorted(ansd.keys())[-1])
+num = 452021 + 1
+anscnt = 0
+while anscnt < 5:
+    m = 0
+    for d in range(2, int(num**0.5) + 1):
+        if num % d == 0:
+            m += d
+            m += (num // d)
+            break
+    if m % 7 == 3:
+        print(num, m)
+        anscnt += 1
+    num += 1
