7f53817934eda877490fc0a65320950bf8450e21
Rybolovlev Alexey <rybolovlevalexey@yandex.ru>
2022-06-07 11:26:46 +0500

    июньский вариант 2

--- подготовка к егэ/вариант ЕГЭ/06.06 (вар 2).py
+++ подготовка к егэ/вариант ЕГЭ/06.06 (вар 2).py
@@ -1,12 +1,13 @@
-file = open('107_17.txt', 'r')
-sp = list(map(int, file.read().strip().split()))
-k = min(filter(lambda x: x % 21 == 0, sp))
-cnt = 0
-mxsm = 0
-
-for i in range(len(sp) - 1):
-    if sp[i] % k == 0 or sp[i + 1] % k == 0:
-        cnt += 1
-        if mxsm < sp[i] + sp[i + 1]:
-            mxsm = sp[i] + sp[i + 1]
-print(cnt, mxsm)
+for num in range(0, 1000):
+    x = num
+    a = 0
+    b = 0
+    while x > 0:
+        if x % 2 == 0:
+            a += 1
+        else:
+            b += x % 6
+        x = x // 6
+    if a == 2 and b == 4:
+        print(num)
+        break
