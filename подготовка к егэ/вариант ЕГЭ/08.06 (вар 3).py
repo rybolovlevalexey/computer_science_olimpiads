b070f3534125ae98a9803c9f3af6b09e47923650
Rybolovlev Alexey <rybolovlevalexey@yandex.ru>
2022-06-08 10:36:08 +0500

    июньский вариант 3

--- подготовка к егэ/вариант ЕГЭ/08.06 (вар 3).py
+++ подготовка к егэ/вариант ЕГЭ/08.06 (вар 3).py
@@ -1,15 +1,6 @@
-ans = set()
-for num in range(100, 3000 + 1):
-    n = num
-    r = bin(n)[2:]
-    r = r[1:]
-    while len(r) > 0 and r[0] == '0':
-        r = r[1:]
-    if len(r) == 0:
-        r = 0
-    else:
-        r = int(r, 2)
-    res = n - r
-    ans.add(res)
-print(ans)
-print(len(ans))
+s = 47
+n = 1
+while s > 0:
+    s = s - 9
+    n = n + 4
+print(n)
