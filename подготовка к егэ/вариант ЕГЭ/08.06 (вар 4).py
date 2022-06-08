abc8245aa0dc009da506875ea81ed3e3829de741
Rybolovlev Alexey <rybolovlevalexey@yandex.ru>
2022-06-08 19:58:15 +0500

    июньский вариант 4

--- подготовка к егэ/вариант ЕГЭ/08.06 (вар 4).py
+++ подготовка к егэ/вариант ЕГЭ/08.06 (вар 4).py
@@ -1,15 +1,6 @@
-for num in range(0, 256):
-    n = num
-    r = bin(n)[2:]
-    while len(r) < 8:
-        r = '0' + r
-    r1 = ''
-    for elem in r:
-        if elem == '0':
-            r1 += '1'
-        else:
-            r1 += '0'
-    res = int(r1, 2)
-    res = res - n
-    if res == 133:
-        print(num)
+s = 0
+k = 0
+while k < 30:
+    k += 3
+    s += k
+print(s)
