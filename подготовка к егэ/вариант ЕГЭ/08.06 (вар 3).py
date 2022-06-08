b3eacc9450c673d74c1395af4d400d87a9f055ef
Rybolovlev Alexey <rybolovlevalexey@yandex.ru>
2022-06-08 12:31:47 +0500

    июньский вариант 3

--- подготовка к егэ/вариант ЕГЭ/08.06 (вар 3).py
+++ подготовка к егэ/вариант ЕГЭ/08.06 (вар 3).py
@@ -1,7 +1,17 @@
-file = open('24 (5).txt', 'r')
-st = file.read().strip().split('E')
-mxdl = 0
-for elem in st:
-    if elem.count('A') >= 3 and len(elem) > mxdl:
-        mxdl = len(elem)
-print(mxdl)
+output = 0
+num = 10000000 + 1
+while output < 5:
+    sp = set()
+    s = 0
+    for d in range(2, int(num**0.5) + 1):
+        if num % d == 0:
+            sp.add(d)
+            sp.add(num // d)
+        if len(sp) >= 3:
+            break
+    if len(sp) >= 2:
+        s = sorted(sp)[-1] + sorted(sp)[-2]
+    if 0 < s < 10000:
+        print(s)
+        output += 1
+    num += 1
