2a7bfd9eb374d52cba52548e853ea21e46c4fdb9
Rybolovlev Alexey <rybolovlevalexey@yandex.ru>
2022-06-08 22:18:50 +0500

    июньский вариант 4

--- подготовка к егэ/вариант ЕГЭ/08.06 (вар 4).py
+++ подготовка к егэ/вариант ЕГЭ/08.06 (вар 4).py
@@ -1,8 +1,11 @@
-sp = list()
-def f(n):
-    if n > 0:
-        f(n - 3)
-        f(n // 3)
-        sp.append(n)
-f(9)
-print(''.join(map(str, sp)))
+file = open('17 (9).txt', 'r')
+sp = list(map(int, file.read().strip().split('\n')))
+cnt = 0
+mxsm = 0
+for i in range(len(sp) - 1):
+    for j in range(i + 1, len(sp)):
+        if sp[i] % 160 != sp[j] % 160 and (sp[i] % 7 == 0 or sp[j] % 7 == 0):
+            cnt += 1
+            if sp[i] + sp[j] > mxsm:
+                mxsm = sp[i] + sp[j]
+print(cnt, mxsm)
