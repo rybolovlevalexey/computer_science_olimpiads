4fd36e1484ffe280c73cb9e872b02fb232e1e868
Rybolovlev Alexey <rybolovlevalexey@yandex.ru>
2022-06-08 20:02:40 +0500

    июньский вариант 4

--- подготовка к егэ/вариант ЕГЭ/08.06 (вар 4).py
+++ подготовка к егэ/вариант ЕГЭ/08.06 (вар 4).py
@@ -1,6 +1,11 @@
-s = 0
-k = 0
-while k < 30:
-    k += 3
-    s += k
-print(s)
+ans = list()
+sp = ['а', 'б', 'в', 'г', 'д']
+for i1 in sp[1:]:
+    for i2 in sp:
+        for i3 in sp:
+            for i4 in sp:
+                for i5 in sp:
+                    st = i1 + i2 + i3 + i4 + i5
+                    if st.count('аа') == 0 and st.count('бб') == 0 and st.count('вв') == 0 and st.count('гг') == 0 and st.count('дд') == 0:
+                        ans.append(st)
+print(len(ans))
