fc3de4a6e5e49ed7e3e78b1409faa31c1e975f13
Rybolovlev Alexey <rybolovlevalexey@yandex.ru>
2022-06-09 18:31:02 +0500

    июньский вариант 5

--- подготовка к егэ/вариант ЕГЭ/09.06 (вар 5).py
+++ подготовка к егэ/вариант ЕГЭ/09.06 (вар 5).py
@@ -1,3 +1,12 @@
-for n in range(2, 100):
-    if 31 % n == 4:
-        print(n)
+for a in range(0, 300):
+    flag = True
+    for x in range(1000):
+        for y in range(1000):
+            res = (y + 2*x != 48) or (a < x) or (x < y)
+            if not res:
+                flag = False
+                break
+        if not flag:
+            break
+    if flag:
+        print(a)
