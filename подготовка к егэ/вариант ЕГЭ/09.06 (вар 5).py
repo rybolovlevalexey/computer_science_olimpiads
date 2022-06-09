75cfc516842fed6d3930db5eaf3a85b607af007a
Rybolovlev Alexey <rybolovlevalexey@yandex.ru>
2022-06-09 19:41:14 +0500

    июньский вариант 5

--- подготовка к егэ/вариант ЕГЭ/09.06 (вар 5).py
+++ подготовка к егэ/вариант ЕГЭ/09.06 (вар 5).py
@@ -1,1 +1,16 @@
-print(len(max(filter(lambda elem: elem.count('E') >= 3, open('24 (6).txt', 'r').read().strip().split('A')), key=lambda x: len(x))))
+ansd = dict()
+for num in range(174457, 174505 + 1):
+    sp = set()
+    for d in range(2, int(num**0.5) + 1):
+        if num % d == 0:
+            sp.add(num // d)
+            sp.add(d)
+        if len(sp) > 3:
+            break
+    if len(sp) == 2:
+        ansd[num] = sorted(sp)
+proizkeyd = dict()
+for key, value in ansd.items():
+    proizkeyd[value[0] * value[1]] = value
+for key in sorted(proizkeyd.keys()):
+    print(proizkeyd[key])
