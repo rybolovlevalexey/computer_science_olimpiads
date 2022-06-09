d3f02c58bb075aaa1f0e0c62db35c0bc5dff1f3b
Rybolovlev Alexey <rybolovlevalexey@yandex.ru>
2022-06-09 18:42:28 +0500

    июньский вариант 5

--- подготовка к егэ/вариант ЕГЭ/09.06 (вар 5).py
+++ подготовка к егэ/вариант ЕГЭ/09.06 (вар 5).py
@@ -1,10 +1,10 @@
-def f(n):
-    if n == 1:
-        return 1
-    elif n == 2:
-        return 3
-    elif n > 2:
-        return f(n - 1) * n + f(n - 2) * (n - 1)
-
-
-print(f(5))
+file = open('17 (10).txt', 'r')
+sp = list(map(int, file.read().strip().split('\n')))
+cnt = 0
+mxsm = 0
+for i in range(len(sp) - 1):
+    if sp[i] % 3 == 0 or sp[i + 1] % 3 == 0:
+        cnt += 1
+        if sp[i] + sp[i + 1] > mxsm:
+            mxsm = sp[i] + sp[i + 1]
+print(cnt, mxsm)
