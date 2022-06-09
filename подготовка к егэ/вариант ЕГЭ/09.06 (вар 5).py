fdd467b4e6bd2e0c564c1c2bbed2d7cd3aaf9694
Rybolovlev Alexey <rybolovlevalexey@yandex.ru>
2022-06-09 10:47:37 +0500

    июньский вариант 4

--- подготовка к егэ/вариант ЕГЭ/09.06 (вар 5).py
+++ подготовка к егэ/вариант ЕГЭ/09.06 (вар 5).py
@@ -1,6 +1,7 @@
-n = 4
-s = 0
-while n <= 8:
-    s += n
-    n += 1
-print(s)
+st = 77 * '1'
+while '11' in st:
+    if '222' in st:
+        st = st.replace('222', '1', 1)
+    else:
+        st = st.replace('11', '2', 1)
+print(st)
