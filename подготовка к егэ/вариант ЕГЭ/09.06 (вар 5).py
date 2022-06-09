c020dcaaa638d62875f0dae280e68df3accae3d0
Rybolovlev Alexey <rybolovlevalexey@yandex.ru>
2022-06-09 22:55:04 +0500

    июньский вариант 5

--- подготовка к егэ/вариант ЕГЭ/09.06 (вар 5).py
+++ подготовка к егэ/вариант ЕГЭ/09.06 (вар 5).py
@@ -1,16 +1,14 @@
-ansd = dict()
-for num in range(174457, 174505 + 1):
-    sp = set()
-    for d in range(2, int(num**0.5) + 1):
-        if num % d == 0:
-            sp.add(num // d)
-            sp.add(d)
-        if len(sp) > 3:
-            break
-    if len(sp) == 2:
-        ansd[num] = sorted(sp)
-proizkeyd = dict()
-for key, value in ansd.items():
-    proizkeyd[value[0] * value[1]] = value
-for key in sorted(proizkeyd.keys()):
-    print(proizkeyd[key])
+file = open('26 (4).txt', 'r')
+sp = list(map(int, file.read().strip().split('\n')))
+n = sp[0]
+del sp[0]
+cnt = 0
+mxsr = 0
+chet = list(filter(lambda x: x % 2 == 0, sp))
+for i in range(len(chet) - 1):
+    for j in range(i + 1, len(chet)):
+        if (chet[i] + chet[j]) % 2 == 0 and (chet[i] + chet[j]) // 2 in sp:
+            cnt += 1
+            if mxsr < (chet[i] + chet[j]) // 2:
+                mxsr = (chet[i] + chet[j]) // 2
+print(cnt, mxsr)
