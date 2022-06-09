7c8661a464cfc6aa5a7c571c1e4e93ea0669f253
Rybolovlev Alexey <rybolovlevalexey@yandex.ru>
2022-06-09 10:42:51 +0500

    июньский вариант 4

--- подготовка к егэ/вариант ЕГЭ/09.06 (вар 5).py
+++ подготовка к егэ/вариант ЕГЭ/09.06 (вар 5).py
@@ -1,7 +1,6 @@
-print('x y z res')
-for x in [True, False]:
-    for y in [True, False]:
-        for z in [True, False]:
-            res = (not x or y) and (not y or z)
-            if res:
-                print(int(x), int(y), int(z), int(res))
+n = 4
+s = 0
+while n <= 8:
+    s += n
+    n += 1
+print(s)
