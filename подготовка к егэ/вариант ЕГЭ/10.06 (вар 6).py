b2fd22f910eb7cba190a3fe87e0d716d70ef6986
Rybolovlev Alexey <rybolovlevalexey@yandex.ru>
2022-06-10 10:52:30 +0500

    июньский вариант 6

--- подготовка к егэ/вариант ЕГЭ/10.06 (вар 6).py
+++ подготовка к егэ/вариант ЕГЭ/10.06 (вар 6).py
@@ -1,7 +1,11 @@
-print('x y z res')
-for x in [True, False]:
-    for y in [True, False]:
-        for z in [True, False]:
-            res = (x == y) or (not y and not z or x)
-            if not res:
-                print(int(x), int(y), int(z), int(res))
+for num in range(1000):
+    n = num
+    r = bin(n)[2:]
+    s = sum(map(int, list(r)))
+    r += str(s % 2)
+    s = sum(map(int, list(r)))
+    r += str(s % 2)
+    res = int(r, 2)
+    if res > 43:
+        print(res)
+        break
