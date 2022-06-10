5815078813654e12104318de66f05e2fb700ef62
Rybolovlev Alexey <rybolovlevalexey@yandex.ru>
2022-06-10 12:34:35 +0500

    июньский вариант 6

--- подготовка к егэ/вариант ЕГЭ/10.06 (вар 6).py
+++ подготовка к егэ/вариант ЕГЭ/10.06 (вар 6).py
@@ -1,8 +1,26 @@
-for num in range(1000):
-    q = 0
-    n = num
-    for i in range(1, n):
-        if n % i == 0:
-            q = i
-    if q == 17:
-        print(num)
+dp = [0] * 13
+dp[2] = 1
+for i in range(3, 9):
+    if i % 3 == 0:
+        dp[i] = dp[i - 1] + dp[i - 2] + dp[i // 3]
+    else:
+        dp[i] = dp[i - 1] + dp[i - 2]
+print(dp)
+for i in range(8):
+    dp[i] = 0
+print(dp)
+for i in range(9, 11):
+    if i % 3 == 0:
+        dp[i] = dp[i - 1] + dp[i - 2] + dp[i // 3]
+    else:
+        dp[i] = dp[i - 1] + dp[i - 2]
+print(dp)
+for i in range(10):
+    dp[i] = 0
+print(dp)
+for i in range(11, 13):
+    if i % 3 == 0:
+        dp[i] = dp[i - 1] + dp[i - 2] + dp[i // 3]
+    else:
+        dp[i] = dp[i - 1] + dp[i - 2]
+print(dp)
