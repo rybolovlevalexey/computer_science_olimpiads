2cb3e0d004aeec58df18943a8b0d62d642d3b06c
Rybolovlev Alexey <rybolovlevalexey@yandex.ru>
2022-06-10 12:28:58 +0500

    июньский вариант 6

--- подготовка к егэ/вариант ЕГЭ/10.06 (вар 6).py
+++ подготовка к егэ/вариант ЕГЭ/10.06 (вар 6).py
@@ -1,12 +1,11 @@
-sp = list()
-def f(n):
-    if n > 0:
-        g(n - 1)
-def g(n):
-    sp.append('*')
-    if n > 1:
-        f(n - 3)
-
-f(10)
-print(len(sp))
-print(sp)
+cnt = 0
+mxsm = 0
+file = open('17 (11).txt', 'r')
+sp = list(map(int, file.read().strip().split('\n')))
+for i in range(len(sp) - 1):
+    for j in range(i + 1, len(sp)):
+        if abs(sp[i] - sp[j]) % 2 == 0 and (sp[i] % 31 == 0 or sp[j] % 31 == 0):
+            cnt += 1
+            if sp[i] + sp[j] > mxsm:
+                mxsm = sp[i] + sp[j]
+print(cnt, mxsm)
