9defe9ba751ca4266c691be377bd7f9993c2fd78
Rybolovlev Alexey <rybolovlevalexey@yandex.ru>
2022-06-10 15:38:05 +0500

    пробник

--- подготовка к егэ/вариант ЕГЭ/пробник.py
+++ подготовка к егэ/вариант ЕГЭ/пробник.py
@@ -1,9 +1,13 @@
-for num in range(1000, 2, -1):
-    s = num
-    n = s
-    s = s // 10
-    while s + n < 125:
-        s = s + n
-        n = n - 5
-    if len(str(n)) == 2:
-        print(num)
+n = 1
+glas = ['е', 'а']
+sp = sorted(['ф', 'е', 'в', 'р', 'а', 'л', 'ь'])
+for i1 in sp:
+    for i2 in sp:
+        for i3 in sp:
+            for i4 in sp:
+                for i5 in sp:
+                    for i6 in sp:
+                        if i1 not in glas and i2 not in glas and i3 not in glas and i4 not in glas and i5 not in glas and i6 not in glas:
+                            print(n)
+                            x = 1/0
+                        n += 1
