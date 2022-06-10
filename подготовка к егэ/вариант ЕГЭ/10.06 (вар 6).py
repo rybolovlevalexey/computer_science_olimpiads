24bed54526b52b520c2cf34e329badf27280b0cd
Rybolovlev Alexey <rybolovlevalexey@yandex.ru>
2022-06-10 11:49:18 +0500

    июньский вариант 6

--- подготовка к егэ/вариант ЕГЭ/10.06 (вар 6).py
+++ подготовка к егэ/вариант ЕГЭ/10.06 (вар 6).py
@@ -1,6 +1,11 @@
-n = 1
-s = 0
-while n <= 100:
-    s = s + 30
-    n = n * 3
-print(s)
+cnt = 0
+sp = ['з', 'и', 'м', 'а']
+for i1 in sp:
+    for i2 in sp:
+        for i3 in sp:
+            for i4 in sp:
+                for i5 in sp:
+                    st = i1 + i2 + i3 + i4 + i5
+                    if (st.count('и') == 1 and st.count('а') == 0) or (st.count('и') == 0 and st.count('а') == 1):
+                        cnt += 1
+print(cnt)
