c2b80a7667017c02442ced1920f4ebd285bdd349
Rybolovlev Alexey <rybolovlevalexey@yandex.ru>
2022-06-10 21:48:12 +0500

    вариант 6

--- подготовка к егэ/вариант ЕГЭ/10.06 (вар 6).py
+++ подготовка к егэ/вариант ЕГЭ/10.06 (вар 6).py
@@ -1,14 +1,17 @@
-file = open('26 (5).txt', 'r')
-sp = list(map(int, file.read().strip().split('\n')))
-n = sp[0]
-del sp[0]
-nechet = list(filter(lambda x: x % 2 != 0, sp))
-cnt = 0
-mxsr = 0
-for i in range(len(nechet) - 1):
-    for j in range(i + 1, len(nechet)):
-        if (nechet[i] + nechet[j]) // 2 in sp:
-            cnt += 1
-            if (nechet[i] + nechet[j]) // 2 > mxsr:
-                mxsr = (nechet[i] + nechet[j]) // 2
-print(cnt, mxsr)
+file = open('27-A_demo.txt', 'r')
+s = file.read().strip().split('\n')
+n = int(s[0])
+del s[0]
+sp = list()
+summa = 0
+mndl = None
+for elem in s:
+    a, b = map(int, elem.split())
+    sp.append(sorted([a, b]))
+    summa += max(a, b)
+    if (mndl is None or abs(a - b) < mndl) and min(a, b) % 3 != 0:
+        mndl = abs(a - b)
+sp = sorted(sp, key= lambda x: x[0])
+print(summa, summa % 3, mndl)
+ans = summa - mndl
+print(ans, ans % 3)
