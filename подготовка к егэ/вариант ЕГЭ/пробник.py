aea3aad5319e27c79891639a230baeaec090b5b3
Rybolovlev Alexey <rybolovlevalexey@yandex.ru>
2022-06-10 18:27:04 +0500

    пробник

--- подготовка к егэ/вариант ЕГЭ/пробник.py
+++ подготовка к егэ/вариант ЕГЭ/пробник.py
@@ -1,14 +1,15 @@
-def convert(x, system):
-    res = ''
-    while x > 0:
-        res += str(x % system)
-        x //= system
-    return res[::-1]
-
-
 sp = list()
-for num in range(1, 96):
-    if len(convert(num, 3)) >= 2 and convert(num, 3)[-1] == '1' and convert(num, 3)[-2] == '2' and convert(num, 5)[0] == '3':
-        sp.append(num)
+for r in range(1000):
+    flag = True
+    for x in range(10000):
+        for a in range(10000):
+            res = (x & 108 != 0 and x & 60 != 0 or x & a == 0) or (x & r == 0)
+            if not res:
+                flag = False
+                break
+        if not flag:
+            break
+    if flag:
+        sp.append(r)
 print(sp)
-print(sum(sp))
+print(len(sp))
