8f03430b7e423982234506d9bcedbb891cbb5609
Rybolovlev Alexey <rybolovlevalexey@yandex.ru>
2022-06-10 18:49:16 +0500

    вариант 6

--- подготовка к егэ/вариант ЕГЭ/10.06 (вар 6).py
+++ подготовка к егэ/вариант ЕГЭ/10.06 (вар 6).py
@@ -1,26 +1,17 @@
-dp = [0] * 13
-dp[2] = 1
-for i in range(3, 9):
-    if i % 3 == 0:
-        dp[i] = dp[i - 1] + dp[i - 2] + dp[i // 3]
+file = open('107_24.txt', 'r')
+st = file.read().strip()
+cnt = 0
+mxcnt = 0
+i = 0
+
+while i < len(st) - 1:
+    if (st[i] == 'A' and st[i + 1] == 'B') or (st[i] == 'C' and st[i + 1] == 'B'):
+        i += 2
+        cnt += 1
     else:
-        dp[i] = dp[i - 1] + dp[i - 2]
-print(dp)
-for i in range(8):
-    dp[i] = 0
-print(dp)
-for i in range(9, 11):
-    if i % 3 == 0:
-        dp[i] = dp[i - 1] + dp[i - 2] + dp[i // 3]
-    else:
-        dp[i] = dp[i - 1] + dp[i - 2]
-print(dp)
-for i in range(10):
-    dp[i] = 0
-print(dp)
-for i in range(11, 13):
-    if i % 3 == 0:
-        dp[i] = dp[i - 1] + dp[i - 2] + dp[i // 3]
-    else:
-        dp[i] = dp[i - 1] + dp[i - 2]
-print(dp)
+        if cnt > mxcnt:
+            mxcnt = cnt
+        cnt = 0
+        i += 1
+print(mxcnt)
+print(cnt)
--- подготовка к егэ/вариант ЕГЭ/пробник.py
+++ подготовка к егэ/вариант ЕГЭ/пробник.py
@@ -1,15 +1,35 @@
-sp = list()
-for r in range(1000):
-    flag = True
-    for x in range(10000):
-        for a in range(10000):
-            res = (x & 108 != 0 and x & 60 != 0 or x & a == 0) or (x & r == 0)
-            if not res:
-                flag = False
-                break
-        if not flag:
-            break
-    if flag:
-        sp.append(r)
-print(sp)
-print(len(sp))
+def f(n):
+    if n == 30:
+        return 890
+    if n == 35:
+        return 1751
+    if n == 40:
+        return 8186
+    if n == 45:
+        return 16333
+    if n <= 2:
+        return 1
+    elif n > 2 and n % 2 != 0:
+        return f(n - 1) - n
+    else:
+        return f(n - 2) + g(n - 1) + 2
+
+
+def g(n):
+    if n == 30:
+        return 2296
+    if n == 35:
+        return 2302
+    if n == 40:
+        return -2056
+    if n == 45:
+        return -2050
+    if n <= 0:
+        return 2
+    elif n > 0 and n % 2 != 0:
+        return f(n - 1) - 2 * g(n - 2)
+    else:
+        return 2*f(n - 2) - 2*g(n - 1)
+
+
+print(f(96))
