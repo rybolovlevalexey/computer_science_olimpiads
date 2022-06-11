8a5d193d6a5fc1dae26aecdae3b1035f068ce689
Rybolovlev Alexey <rybolovlevalexey@yandex.ru>
2022-06-11 19:30:14 +0500

    вариант 7

--- подготовка к егэ/вариант ЕГЭ/11.06 (вар 8).py
+++ подготовка к егэ/вариант ЕГЭ/11.06 (вар 8).py
@@ -1,17 +1,6 @@
 file = open('24_demo (1).txt', 'r')
 st = file.read().strip()
-ansst = ''
-mxdl = 0
-dl = 1
-s = st[0]
-for i in range(1, len(st)):
-    if s[-1] == st[i]:
-        if dl > mxdl:
-            mxdl = dl
-            ansst = s
-        dl = 1
-        s = st[i]
-    else:
-        s += st[i]
-        dl += 1
-print(ansst, mxdl)
+st = st.replace('ZZ', 'XX')
+st = st.replace('YY', 'XX')
+st = st.split('XX')
+print(len(max(st, key= lambda x: len(x))))
