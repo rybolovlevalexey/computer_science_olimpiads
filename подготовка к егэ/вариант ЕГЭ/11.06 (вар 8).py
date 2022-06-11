ea91560e91dd77bf2e1bedffd9021ee069fcd412
Rybolovlev Alexey <rybolovlevalexey@yandex.ru>
2022-06-11 18:02:03 +0500

    вариант 7

--- подготовка к егэ/вариант ЕГЭ/11.06 (вар 8).py
+++ подготовка к егэ/вариант ЕГЭ/11.06 (вар 8).py
@@ -1,7 +1,12 @@
-def f(n):
-    if n == 1:
-        return 1
-    if n > 1:
-        return f(n - 1) + n
-
-print(f(30))
+file = open('17 (13).txt', 'r')
+sp = list(map(int, file.read().strip().split('\n')))
+cnt = 0
+chet = list(filter(lambda x: x % 2 == 0, sp))
+sr = sum(chet) / len(chet)
+mxsm = 0
+for i in range(len(sp) - 1):
+    if (sp[i] % 3 == 0 or sp[i + 1] % 3 == 0) and (sp[i] < sr or sp[i + 1] < sr):
+        cnt += 1
+        if mxsm < sp[i] + sp[i + 1]:
+            mxsm = sp[i] + sp[i + 1]
+print(cnt, mxsm)
