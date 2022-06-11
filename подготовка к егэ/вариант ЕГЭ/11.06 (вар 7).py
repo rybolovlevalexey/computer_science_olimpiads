fd9cf33fb523f967bd83889da9c67b5b468d4f6b
Rybolovlev Alexey <rybolovlevalexey@yandex.ru>
2022-06-11 12:18:53 +0500

    вариант 7

--- подготовка к егэ/вариант ЕГЭ/11.06 (вар 7).py
+++ подготовка к егэ/вариант ЕГЭ/11.06 (вар 7).py
@@ -1,20 +1,12 @@
-def convert(x, s):
-    r = ''
-    while x > 0:
-        r += str(x % s)
-        if s == 11:
-            print(x % s)
-        if s == 13:
-            print(x % s)
-        if s == 19:
-            print(x % s)
-        x //= s
-    if s in [11, 13, 19]:
-        print('----------')
-    return r[::-1]
-
-
-for n in range(2, 20):
-    res = convert(144, n)
-    if res[-1] == '1' and len(res) >= 3:
-        print(n, 'answer')
+for a in range(1000):
+    flag = True
+    for x in range(1000):
+        for y in range(1000):
+            res = (y + 2*x != 48) or (a < x) or (a < y)
+            if not res:
+                flag = False
+                break
+        if not flag:
+            break
+    if flag:
+        print(a)
