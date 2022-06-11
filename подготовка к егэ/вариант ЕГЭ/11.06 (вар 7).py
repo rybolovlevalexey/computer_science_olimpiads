36daff80239cad6b424e29250f0971d70fa33ec6
Rybolovlev Alexey <rybolovlevalexey@yandex.ru>
2022-06-11 11:26:09 +0500

    вариант 7

--- подготовка к егэ/вариант ЕГЭ/11.06 (вар 7).py
+++ подготовка к егэ/вариант ЕГЭ/11.06 (вар 7).py
@@ -1,6 +1,12 @@
-num = '1110100000110010'
-num = int(num, 2)
-print(num)
-num = hex(num)
-print(num)
-print(num[2:].upper())
+cnt = 0
+for num in range(1000, 10000):
+    n = num
+    a, b, c, d = map(int, list(str(num)))
+    if a % 2 == 0 or b % 2 == 0 or c % 2 == 0 or d % 2 == 0:
+        continue
+    s1 = a + b
+    s2 = c + d
+    st = ''.join(map(str, sorted([s1, s2])))
+    if st == '414':
+        cnt += 1
+print(cnt)
