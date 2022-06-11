fa923fc5e52174a88320fb4cf2d2b9c840210d73
Rybolovlev Alexey <rybolovlevalexey@yandex.ru>
2022-06-11 12:27:13 +0500

    вариант 7

--- подготовка к егэ/вариант ЕГЭ/11.06 (вар 7).py
+++ подготовка к егэ/вариант ЕГЭ/11.06 (вар 7).py
@@ -1,9 +1,13 @@
-sp = list()
-def f(n):
-    if n > 0:
-        f(n - 3)
-        sp.append(n)
-        f(n // 3)
-
-f(9)
-print(''.join(map(str, sp)))
+file = open('17 (12).txt', 'r')
+sp = list(map(int, file.read().strip().split('\n')))
+sr = sum(filter(lambda x: x % 2 != 0, sp)) / len(list(filter(lambda x: x % 2 != 0, sp)))
+cnt = 0
+mxsm = 0
+for i in range(len(sp) - 1):
+    i1 = sp[i]
+    i2 = sp[i + 1]
+    if (i1 % 5 == 0 or i2 % 5 == 0) and (i1 < sr or i2 < sr):
+        cnt += 1
+        if mxsm < i1 + i2:
+            mxsm = i1 + i2
+print(cnt, mxsm)
