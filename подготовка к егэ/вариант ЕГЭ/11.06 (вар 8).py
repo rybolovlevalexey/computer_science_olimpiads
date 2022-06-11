1e8d9455217a2653cfc9bec0b862eede2f82022e
Rybolovlev Alexey <rybolovlevalexey@yandex.ru>
2022-06-11 17:49:34 +0500

    вариант 7

--- подготовка к егэ/вариант ЕГЭ/11.06 (вар 8).py
+++ подготовка к егэ/вариант ЕГЭ/11.06 (вар 8).py
@@ -1,11 +1,9 @@
-sp = ['г', 'о', 'д']
-sogl = ['г', 'д']
+num = 8**2020 + 4**2017 + 26 - 1
 cnt = 0
-for i1 in sogl:
-    for i2 in sp:
-        for i3 in sp:
-            for i4 in sp:
-                for i5 in sp:
-                    for i6 in sp:
-                        cnt += 1
+while num > 0:
+    if num % 2 == 1:
+        cnt += 1
+    num //= 2
 print(cnt)
+num = 8**2020 + 4**2017 + 26 - 1
+print(bin(num)[2:].count('1'))
