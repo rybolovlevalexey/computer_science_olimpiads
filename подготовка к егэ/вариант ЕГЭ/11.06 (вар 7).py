cc69e8f3221d8a47bd584abcfd499244727b7186
Rybolovlev Alexey <rybolovlevalexey@yandex.ru>
2022-06-11 14:09:35 +0500

    вариант 7

--- подготовка к егэ/вариант ЕГЭ/11.06 (вар 7).py
+++ подготовка к егэ/вариант ЕГЭ/11.06 (вар 7).py
@@ -1,22 +1,21 @@
-file = open("27880.txt", 'r')
-sp = file.read().strip().split('\n')
-s, n = map(int, sp[0].split())
+file = open('27-B_2.txt', 'r')
+sp = list(map(int, file.read().strip().split('\n')))
+n = sp[0]
 del sp[0]
-sp = list(sorted(map(int, sp)))
-summa = 0
-cnt = 0
-lstelem = 0
+m = 0
+m14 = 0
+m2 = 0
+m7 = 0
 for elem in sp:
-    if elem + summa <= s:
-        summa += elem
-        cnt += 1
-        lstelem = elem
-    else:
-        break
-print(cnt)
-summa -= lstelem
-print(lstelem, summa, s)
-for elem in sp[::-1]:
-    if summa + elem <= s:
-        print(elem)
-        break
+    if m % 14 != 0 and elem > m:
+        m = elem
+    if elem % 2 == 0 and elem > m2:
+        m2 = elem
+    if elem % 7 == 0 and elem > m7:
+        m7 = elem
+    if elem % 14 == 0 and elem > m14:
+        m14 = elem
+print(m, m2, m7, m14)
+ans = [m * m2, m * m7, m * m14, m2 * m7, m2 * m14, m7 * m14]
+print(ans)
+print(max(ans) % 14)
