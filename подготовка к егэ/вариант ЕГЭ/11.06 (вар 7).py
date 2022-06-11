bfe825442a4d7a75be3bd5f74c094dcb85dc0a5e
Rybolovlev Alexey <rybolovlevalexey@yandex.ru>
2022-06-11 11:32:47 +0500

    вариант 7

--- подготовка к егэ/вариант ЕГЭ/11.06 (вар 7).py
+++ подготовка к егэ/вариант ЕГЭ/11.06 (вар 7).py
@@ -1,6 +1,10 @@
-s = 22
-n = 0
-while s < s*s:
-    s = s - 1
-    n = n + 3
-print(n)
+sp = ['е', 'г', 'э']
+glas = ['е', 'э']
+cnt = 0
+for i1 in glas:
+    for i2 in sp:
+        for i3 in sp:
+            for i4 in sp:
+                for i5 in sp:
+                    cnt += 1
+print(cnt)
