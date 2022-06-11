dbcee83d3116d719361814d6c20ea0821ec1d251
Rybolovlev Alexey <rybolovlevalexey@yandex.ru>
2022-06-11 11:27:40 +0500

    вариант 7

--- подготовка к егэ/вариант ЕГЭ/11.06 (вар 7).py
+++ подготовка к егэ/вариант ЕГЭ/11.06 (вар 7).py
@@ -1,12 +1,6 @@
-cnt = 0
-for num in range(1000, 10000):
-    n = num
-    a, b, c, d = map(int, list(str(num)))
-    if a % 2 == 0 or b % 2 == 0 or c % 2 == 0 or d % 2 == 0:
-        continue
-    s1 = a + b
-    s2 = c + d
-    st = ''.join(map(str, sorted([s1, s2])))
-    if st == '414':
-        cnt += 1
-print(cnt)
+s = 22
+n = 0
+while s < s*s:
+    s = s - 1
+    n = n + 3
+print(n)
