07a4757eed1ca10916a20a2065861f323f6d3b43
Rybolovlev Alexey <rybolovlevalexey@yandex.ru>
2022-06-11 11:49:47 +0500

    вариант 7

--- подготовка к егэ/вариант ЕГЭ/11.06 (вар 7).py
+++ подготовка к егэ/вариант ЕГЭ/11.06 (вар 7).py
@@ -1,10 +1,14 @@
-sp = ['е', 'г', 'э']
-glas = ['е', 'э']
+file = open('Безымянный.txt', 'r')
+sp = file.read().strip().split('\n')
+numbers = list()
+for line in sp:
+    line = map(float, line.split())
+    for num in line:
+        numbers.append(num)
+sr = round(sum(numbers) / len(numbers), 1)
+mn = min(numbers) * 2
 cnt = 0
-for i1 in glas:
-    for i2 in sp:
-        for i3 in sp:
-            for i4 in sp:
-                for i5 in sp:
-                    cnt += 1
+for num in numbers:
+    if mn < num < sr:
+        cnt += 1
 print(cnt)
