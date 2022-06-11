a24027ad3ed2023174b1dba70caeeb0b9ca5e793
Rybolovlev Alexey <rybolovlevalexey@yandex.ru>
2022-06-11 18:56:22 +0500

    вариант 7

--- подготовка к егэ/вариант ЕГЭ/11.06 (вар 8).py
+++ подготовка к егэ/вариант ЕГЭ/11.06 (вар 8).py
@@ -1,14 +1,8 @@
-for num in range(10000, 100000):
-    a = 0
-    b = 0
-    x = num
-    while x > 0:
-        y = x % 10
-        if y > 3:
-            a = a + 1
-        if y < 8:
-            b = b + 1
-        x = x // 10
-    if a == 4 and b == 2:
-        print(num)
-        break
+dp = [0] * 21
+dp[1] = 1
+for i in range(2, 21):
+    if i - 5 >= 0:
+        dp[i] = dp[i - 2] + dp[i - 5]
+    else:
+        dp[i] = dp[i - 2]
+print(dp)
