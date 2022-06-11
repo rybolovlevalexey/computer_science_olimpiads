39e7c6a87fa78fc3e217dc5a5682fe64dd17376c
Rybolovlev Alexey <rybolovlevalexey@yandex.ru>
2022-06-11 11:18:59 +0500

    вариант 7

---
+++ подготовка к егэ/вариант ЕГЭ/11.06 (вар 7).py
@@ -0,0 +1,7 @@
+print('x y z res')
+for x in [True, False]:
+    for y in [True, False]:
+        for z in [True, False]:
+            res = not y or (x and not z)
+            if res:
+                print(int(x), int(y), int(z), int(res))
