6a5294aa4ee34764c96b1722b1536a73758d2a31
Rybolovlev Alexey <rybolovlevalexey@yandex.ru>
2022-06-11 18:50:34 +0500

    вариант 7

--- подготовка к егэ/вариант ЕГЭ/11.06 (вар 8).py
+++ подготовка к егэ/вариант ЕГЭ/11.06 (вар 8).py
@@ -1,12 +1,14 @@
-file = open('17 (13).txt', 'r')
-sp = list(map(int, file.read().strip().split('\n')))
-cnt = 0
-chet = list(filter(lambda x: x % 2 == 0, sp))
-sr = sum(chet) / len(chet)
-mxsm = 0
-for i in range(len(sp) - 1):
-    if (sp[i] % 3 == 0 or sp[i + 1] % 3 == 0) and (sp[i] < sr or sp[i + 1] < sr):
-        cnt += 1
-        if mxsm < sp[i] + sp[i + 1]:
-            mxsm = sp[i] + sp[i + 1]
-print(cnt, mxsm)
+for num in range(10000, 100000):
+    a = 0
+    b = 0
+    x = num
+    while x > 0:
+        y = x % 10
+        if y > 3:
+            a = a + 1
+        if y < 8:
+            b = b + 1
+        x = x // 10
+    if a == 4 and b == 2:
+        print(num)
+        break
