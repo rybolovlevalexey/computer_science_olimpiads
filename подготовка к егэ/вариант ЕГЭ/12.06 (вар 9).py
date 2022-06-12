5ac85e6024f719b0676e8a1a1e1e98fd4a5110da
Rybolovlev Alexey <rybolovlevalexey@yandex.ru>
2022-06-12 19:51:45 +0500

    вариант 9

--- подготовка к егэ/вариант ЕГЭ/12.06 (вар 9).py
+++ подготовка к егэ/вариант ЕГЭ/12.06 (вар 9).py
@@ -1,12 +1,2 @@
-sp = ['п', 'и', 'р']
-cnt = 0
-for i1 in sp:
-    for i2 in sp:
-        for i3 in sp:
-            for i4 in sp:
-                for i5 in sp:
-                    st = i1 + i2 + i3 + i4 + i5
-                    if st.count('п') == 1:
-                        cnt += 1
-
-print(cnt)
+num = 4**255 + 2**255 - 255
+print(bin(num)[2:].count('1'))
