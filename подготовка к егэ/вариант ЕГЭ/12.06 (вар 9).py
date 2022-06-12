5295f360f960e5310875a1de04d994c7aaaed8b5
Rybolovlev Alexey <rybolovlevalexey@yandex.ru>
2022-06-12 10:08:45 +0500

    вариант 9

--- подготовка к егэ/вариант ЕГЭ/12.06 (вар 9).py
+++ подготовка к егэ/вариант ЕГЭ/12.06 (вар 9).py
@@ -1,4 +1,9 @@
-num = '100010000110001'
-num = int(num, 2)
-print(num)
-print(oct(num)[2:])
+cnt = 0
+for num in range(100, 1000):
+    n = num
+    s1 = int(str(n)[0]) + int(str(n)[1])
+    s2 = int(str(n)[1]) + int(str(n)[2])
+    ans = ''.join(map(str, sorted([s1, s2])))
+    if ans == '1216':
+        cnt += 1
+print(cnt)
