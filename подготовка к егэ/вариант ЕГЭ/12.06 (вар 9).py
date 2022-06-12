075d8973bf9769cb55ad84632aa77c0859e2c26c
Rybolovlev Alexey <rybolovlevalexey@yandex.ru>
2022-06-12 20:04:33 +0500

    вариант 9

--- подготовка к егэ/вариант ЕГЭ/12.06 (вар 9).py
+++ подготовка к егэ/вариант ЕГЭ/12.06 (вар 9).py
@@ -1,9 +1,11 @@
-sp = list()
-def f(n):
-    if n > 0:
-        f(n - 1)
-        sp.append(str(n))
-        f(n - 2)
-
-f(4)
-print(''.join(sp))
+file = open('17 (14).txt', 'r')
+sp = list(map(int, file.read().strip().split('\n')))
+cnt = 0
+mxsm = 0
+for i in range(len(sp) - 1):
+    for j in range(i + 1, len(sp)):
+        if sp[i] * sp[j] % 26 == 0:
+            cnt += 1
+            if mxsm < sp[i] + sp[j]:
+                mxsm = sp[i] + sp[j]
+print(cnt, mxsm)
