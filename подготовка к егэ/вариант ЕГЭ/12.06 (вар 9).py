d01f98398215147e7b8a3f7681a893598b02ed76
Rybolovlev Alexey <rybolovlevalexey@yandex.ru>
2022-06-12 20:21:12 +0500

    вариант 9

--- подготовка к егэ/вариант ЕГЭ/12.06 (вар 9).py
+++ подготовка к егэ/вариант ЕГЭ/12.06 (вар 9).py
@@ -1,19 +1,3 @@
-dp = [0] * 22
-dp[1] = 1
-for i in range(2, 11):
-    if i % 2 == 0:
-        dp[i] = dp[i // 2] + dp[i - 1]
-    else:
-        dp[i] = dp[i - 1]
-print(dp)
-for i in range(10):
-    dp[i] = 0
-print(dp)
-for i in range(11, 22):
-    if i == 17:
-        continue
-    if i % 2 == 0:
-        dp[i] = dp[i // 2] + dp[i - 1]
-    else:
-        dp[i] = dp[i - 1]
-print(dp)
+file = open('24 (7).txt', 'r')
+sp = file.read().strip().split('XZZY')
+print(len(max(sp, key=lambda x: len(x))) + 6)
