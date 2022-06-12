319661b87a37d65f37c7e2a3ba8dd1137947d5b9
Rybolovlev Alexey <rybolovlevalexey@yandex.ru>
2022-06-12 09:59:14 +0500

    вариант 9

--- подготовка к егэ/вариант ЕГЭ/12.06 (вар 9).py
+++ подготовка к егэ/вариант ЕГЭ/12.06 (вар 9).py
@@ -1,8 +1,4 @@
-print('x y z w res')
-for x in [True, False]:
-    for y in [True, False]:
-        for z in [True, False]:
-            for w in [True, False]:
-                res = (x and not y) or (y == z) or w
-                if not res:
-                    print(int(x), int(y), int(z), int(w), int(res))
+num = '100010000110001'
+num = int(num, 2)
+print(num)
+print(oct(num)[2:])
