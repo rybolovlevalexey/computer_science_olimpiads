02e2df6820fad55565747de55270f1f80a685324
Rybolovlev Alexey <rybolovlevalexey@yandex.ru>
2022-06-13 15:06:47 +0500

    вариант 10

--- подготовка к егэ/вариант ЕГЭ/13.06 (вар 10).py
+++ подготовка к егэ/вариант ЕГЭ/13.06 (вар 10).py
@@ -1,11 +1,6 @@
-for num in range(1000):
-    n = num
-    r = bin(n)[2:]
-    sp = sum(map(int, list(r)))
-    if sp % 2 == 0:
-        r += '00'
-    else:
-        r += '11'
-    res = int(r, 2)
-    if res > 114:
-        print(res, num)
+s = 0
+n = 6
+while n > 1:
+    s = s + n
+    n = n - 1
+print(s)
