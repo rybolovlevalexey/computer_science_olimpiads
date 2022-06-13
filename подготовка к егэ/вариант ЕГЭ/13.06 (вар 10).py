486c21acf9df47eeb27a781c8e73ac7c592e42a2
Rybolovlev Alexey <rybolovlevalexey@yandex.ru>
2022-06-13 17:34:22 +0500

    вариант 10

--- подготовка к егэ/вариант ЕГЭ/13.06 (вар 10).py
+++ подготовка к егэ/вариант ЕГЭ/13.06 (вар 10).py
@@ -1,11 +1,12 @@
-file = open('17 (15).txt', 'r')
-sp = list(map(int, file.read().strip().split('\n')))
-cnt = 0
-mxsm = 0
-for i in range(len(sp) - 1):
-    for j in range(i + 1, len(sp)):
-        if abs(sp[i] - sp[j]) % 2 == 0 and (sp[i] % 19 == 0 or sp[j] % 19 == 0):
-            cnt += 1
-            if sp[i] + sp[j] > mxsm:
-                mxsm = sp[i] + sp[j]
-print(cnt, mxsm)
+for num in range(101, 1000):
+    x = num
+    L = x - 30
+    M = x + 30
+    while L != M:
+        if L > M:
+            L = L - M
+        else:
+            M = M - L
+    if M == 60:
+        print(num)
+        break
