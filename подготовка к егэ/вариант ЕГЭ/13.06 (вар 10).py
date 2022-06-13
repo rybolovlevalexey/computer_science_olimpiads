fb6a5b0691916193da5f933180764d11add7d309
Rybolovlev Alexey <rybolovlevalexey@yandex.ru>
2022-06-13 17:40:57 +0500

    вариант 10

--- подготовка к егэ/вариант ЕГЭ/13.06 (вар 10).py
+++ подготовка к егэ/вариант ЕГЭ/13.06 (вар 10).py
@@ -1,17 +1,8 @@
-dp = [0] * 16
-dp[1] = 1
-for i in range(2, 9):
-    if i - 3 > 0:
-        dp[i] = dp[i - 1] + dp[i - 3]
-    else:
-        dp[i] = dp[i - 1]
-print(dp)
-for i in range(8):
-    dp[i] = 0
-print(dp)
-for i in range(9, 16):
-    if i - 3 > 0:
-        dp[i] = dp[i - 1] + dp[i - 3]
-    else:
-        dp[i] = dp[i - 1]
-print(dp)
+file = open('zadanie24_2 (2).txt', 'r')
+st = file.read().strip()
+n = 1
+while True:
+    if st.count('LDR'*n) == 0:
+        break
+    n += 1
+print(st.count('LDR' * 5))
