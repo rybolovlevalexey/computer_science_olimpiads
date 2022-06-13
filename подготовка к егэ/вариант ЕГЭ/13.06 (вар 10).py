5a8cdb1f2525791a75cb5368bde71ad628445871
Rybolovlev Alexey <rybolovlevalexey@yandex.ru>
2022-06-13 21:58:00 +0500

    вариант 10

--- подготовка к егэ/вариант ЕГЭ/13.06 (вар 10).py
+++ подготовка к егэ/вариант ЕГЭ/13.06 (вар 10).py
@@ -1,9 +1,20 @@
-file = open('27991_A.txt', 'r')
+file = open('27991_B.txt', 'r')
 sp = list(map(int, file.read().strip().split('\n')))
 mxsm = 0
-
-for i in range(len(sp) - 1):
-    for j in range(i + 1, len(sp)):
-        if abs(sp[i] - sp[j]) % 2 == 0 and (sp[i] % 17 == 0 or sp[j] % 17 == 0) and sp[i] + sp[j] > mxsm:
-            mxsm = sp[i] + sp[j]
-print(mxsm)
+del sp[0]
+ans = list()
+chet = sorted(filter(lambda x: x % 2 == 0, sp))[::-1]
+nechet = sorted(filter(lambda x: x % 2 != 0, sp))[::-1]
+if chet[0] % 17 == 0:
+    ans.append(chet[0] + chet[1])
+else:
+    chet17 = list(filter(lambda x: x % 17 == 0, chet))
+    if len(chet17) > 0:
+        ans.append(max(chet17) + chet[0])
+if nechet[0] % 17 == 0:
+    ans.append(nechet[0] + nechet[1])
+else:
+    nechet17 = list(filter(lambda x: x % 17 == 0, nechet))
+    if len(nechet17) > 0:
+        ans.append(max(nechet17) + nechet[0])
+print(max(ans))
