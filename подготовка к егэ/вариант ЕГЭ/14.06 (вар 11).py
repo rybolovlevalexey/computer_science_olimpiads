898e100d00736fc4a108957480578a5a435bd87d
Rybolovlev Alexey <rybolovlevalexey@yandex.ru>
2022-06-14 10:23:55 +0500

    вариант 11

--- подготовка к егэ/вариант ЕГЭ/14.06 (вар 11).py
+++ подготовка к егэ/вариант ЕГЭ/14.06 (вар 11).py
@@ -1,4 +1,7 @@
-num = '1000101010011011'
-num = int(num, 2)
-print(num)
-print(oct(num)[2:])
+for num in range(1000, 10000):
+    a, b, c, d = map(int, list(str(num)))
+    s1 = a + b
+    s2 = c + d
+    ans = ''.join(map(str, sorted([s1, s2])))
+    if ans == '117':
+        print(num)
