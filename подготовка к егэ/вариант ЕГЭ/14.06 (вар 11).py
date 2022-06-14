fab4cd1e429bb5a63e803a47d97df6d35cee3c7b
Rybolovlev Alexey <rybolovlevalexey@yandex.ru>
2022-06-14 11:13:21 +0500

    вариант 11

--- подготовка к егэ/вариант ЕГЭ/14.06 (вар 11).py
+++ подготовка к егэ/вариант ЕГЭ/14.06 (вар 11).py
@@ -1,9 +1,11 @@
-sp = list()
-def f(n):
-    if n > 2:
-        sp.append(n)
-        f(n - 3)
-        f(n - 4)
-f(10)
-print(sp)
-print(sum(sp))
+file = open('17 (16).txt', 'r')
+sp = list(map(int, file.read().strip().split('\n')))
+cnt = 0
+mxsm = None
+for i in range(len(sp) - 1):
+    for j in range(i + 1, len(sp)):
+        if (sp[i] + sp[j]) % 7 == 0:
+            cnt += 1
+            if mxsm is None or mxsm < sp[i] + sp[j]:
+                mxsm = sp[i] + sp[j]
+print(cnt, mxsm)
