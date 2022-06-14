57416b3294f9406e9acf488d1ada88cb5480c2c0
Rybolovlev Alexey <rybolovlevalexey@yandex.ru>
2022-06-14 11:00:07 +0500

    вариант 11

--- подготовка к егэ/вариант ЕГЭ/14.06 (вар 11).py
+++ подготовка к егэ/вариант ЕГЭ/14.06 (вар 11).py
@@ -1,12 +1,9 @@
-ams = None
-for num in range(1, 1000):
-    if num % 16 !=0:
-        continue
-    st = bin(num)[2:][::-1]
-    cnt = 0
-    while st[0] == '0':
-        cnt += 1
-        st = st[1:]
-    if ams is None or cnt < ams:
-        ams = cnt
-print(ams)
+sp = list()
+def f(n):
+    if n > 2:
+        sp.append(n)
+        f(n - 3)
+        f(n - 4)
+f(10)
+print(sp)
+print(sum(sp))
