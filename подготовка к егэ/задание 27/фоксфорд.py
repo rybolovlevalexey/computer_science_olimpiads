1950a8a016bd36ace2b8b14b590730544c2c7b45
Rybolovlev Alexey <rybolovlevalexey@yandex.ru>
2022-06-15 19:51:09 +0500

    фоксфорд

--- подготовка к егэ/задание 27/фоксфорд.py
+++ подготовка к егэ/задание 27/фоксфорд.py
@@ -1,18 +1,11 @@
-file = open('27-32b.txt', 'r')
-sp = file.read().strip().split('\n')
-n = int(sp[0])
-del sp[0]
-summa = 0
-ost = dict()
-for elem in sp:
-    a, b, c = sorted(map(int, elem.split()))
-    summa += a
-    d1 = c - a
-    d2 = b - a
-    if (d1 % 11 in ost and ost[d1 % 11] > d1) or d1 % 11 not in ost:
-        ost[d1 % 11] = d1
-    if (d2 % 11 in ost and ost[d2 % 11] > d1) or d2 % 11 not in ost:
-        ost[d2 % 11] = d2
-print(summa, summa % 11)
-for key in sorted(ost.keys()):
-    print(key, ost[key])
+def f(n):
+    if n == 1:
+        return 2
+    return f(n - 1) + g(n)
+
+def g(n):
+    if n == 1:
+        return 1
+    return f(n - 1) * g(n - 1)
+
+print(f(5))
