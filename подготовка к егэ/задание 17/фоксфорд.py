a331f4eff536d045143f74b335854599408fd1bd
Rybolovlev Alexey <rybolovlevalexey@yandex.ru>
2022-06-16 09:37:50 +0500

    фоксфорд

--- подготовка к егэ/задание 17/фоксфорд.py
+++ подготовка к егэ/задание 17/фоксфорд.py
@@ -3,7 +3,7 @@ sp = list(map(int, file.read().strip().split('\n')))
 cnt = mxsm = 0
 sr = sum(sp) / len(sp)
 for i in range(len(sp) - 1):
-    if abs(sp[i] - sp[i - 1]) <= 20 and sp[i] + sp[i + 1] > sr:
+    if abs(sp[i] - sp[i + 1]) <= 20 and sp[i] + sp[i + 1] > sr:
         cnt += 1
         if sp[i] + sp[i + 1] > mxsm:
             mxsm = sp[i] + sp[i + 1]
