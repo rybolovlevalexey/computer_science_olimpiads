c77a80a8883d5643afab3fe9538909029f943030
Rybolovlev Alexey <rybolovlevalexey@yandex.ru>
2022-06-16 11:40:13 +0500

    вариант 12

--- подготовка к егэ/вариант ЕГЭ/16.06 (вар 12).py
+++ подготовка к егэ/вариант ЕГЭ/16.06 (вар 12).py
@@ -1,6 +1,6 @@
-for num in range(11, 101, 2):
-    x = num
-    res = int(str(x % 4) + str(x % 3) + str(x % 2))
-    if res == 301:
-        print(num)
-        break
+n = 4
+s = 0
+while n <= 8:
+    s += n
+    n += 1
+print(s)
