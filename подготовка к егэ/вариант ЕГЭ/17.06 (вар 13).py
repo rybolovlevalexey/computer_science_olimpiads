81a1d96608882491124b1bc57eb9f91e881c9b43
Rybolovlev Alexey <rybolovlevalexey@yandex.ru>
2022-06-17 10:06:53 +0500

    вариант 13

--- подготовка к егэ/вариант ЕГЭ/17.06 (вар 13).py
+++ подготовка к егэ/вариант ЕГЭ/17.06 (вар 13).py
@@ -1,8 +1,6 @@
-for num in range(100, 1000):
-    a, b, c = map(int, list(str(num)))
-    s1 = a + b
-    s2 = b + c
-    res = ''.join(list(map(str, sorted([s1, s2])[::-1])))
-    if res == '1412':
-        print(num)
-        break
+s = 0
+n = 32
+while n > s:
+    s = s + 1
+    n = n - 1
+print(n)
