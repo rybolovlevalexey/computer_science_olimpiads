c041fc7dfaa949fd309ac8f7e64917873516766b
Rybolovlev Alexey <rybolovlevalexey@yandex.ru>
2022-06-17 16:02:47 +0500

    вариант 14

--- подготовка к егэ/вариант ЕГЭ/17.06 (вар 14).py
+++ подготовка к егэ/вариант ЕГЭ/17.06 (вар 14).py
@@ -1,8 +1,3 @@
-print('x y z w res')
-for x in [True, False]:
-    for y in [True, False]:
-        for z in [True, False]:
-            for w in [True, False]:
-                res = (not x or y or not z or not w) and (not w or z or y)
-                if not res:
-                    print(int(x), int(y), int(z), int(w), int(res))
+num = '1011100111001010'
+num = int(num, 2)
+print(hex(num)[2:].upper())
