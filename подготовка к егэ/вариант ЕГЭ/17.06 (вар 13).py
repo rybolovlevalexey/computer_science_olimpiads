0033a182168e736894033f7b639c6318b77d42bf
Rybolovlev Alexey <rybolovlevalexey@yandex.ru>
2022-06-17 11:25:24 +0500

    вариант 13

--- подготовка к егэ/вариант ЕГЭ/17.06 (вар 13).py
+++ подготовка к егэ/вариант ЕГЭ/17.06 (вар 13).py
@@ -1,11 +1,9 @@
-ans = list()
-for a in range(-100, 1000):
-    flag = True
-    for x in range(10000):
-        res = (x & 51 == 0) or (x & 41 != 0 or x & a == 0)
-        if not res:
-            flag = False
-    if flag:
-        ans.append(a)
-        print(a)
-print(ans)
+sp = list()
+def f(n):
+    if n > 0:
+        sp.append(n)
+        f(n // 2)
+        f(n - 4)
+
+f(9)
+print(''.join(map(str, sp)))
