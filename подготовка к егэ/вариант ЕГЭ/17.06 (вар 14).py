0fa0a5507f4bbabea877f914afd99cf899b66381
Rybolovlev Alexey <rybolovlevalexey@yandex.ru>
2022-06-17 19:23:24 +0500

    вариант 14

--- подготовка к егэ/вариант ЕГЭ/17.06 (вар 14).py
+++ подготовка к егэ/вариант ЕГЭ/17.06 (вар 14).py
@@ -1,7 +1,8 @@
 n = 1
 file = open('24_demo (2).txt', 'r')
 st = file.read().strip()
-while st.count('Y' * (n + 1)) >= 1:
-    n += 1
-print(n)
-print(st.count('Y' * 11))
+ans = list()
+for i in range(1, 10000):
+    if st.count('Y' * i) >= 1:
+        ans.append(i)
+print(max(ans))
