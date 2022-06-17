d99c2ce5a9245d541b42f93091ba950f3f67ca44
Rybolovlev Alexey <rybolovlevalexey@yandex.ru>
2022-06-17 17:56:43 +0500

    вариант 14

--- подготовка к егэ/вариант ЕГЭ/17.06 (вар 14).py
+++ подготовка к егэ/вариант ЕГЭ/17.06 (вар 14).py
@@ -1,6 +1,9 @@
-n = 1
-s = 0
-while n <= 300:
-    s = s + 30
-    n = n * 5
-print(s)
+sp = ['a', 'b', 'c', 'd', 'e']
+cnt = 0
+for i1 in sp[:-1]:
+    for i2 in sp:
+        for i3 in sp:
+            for i4 in sp:
+                for i5 in sp[1:]:
+                    cnt += 1
+print(cnt)
