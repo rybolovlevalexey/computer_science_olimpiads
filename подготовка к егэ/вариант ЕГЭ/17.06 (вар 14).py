592f08c659582d42f2a0f15cf218f787201fbd51
Rybolovlev Alexey <rybolovlevalexey@yandex.ru>
2022-06-17 16:08:37 +0500

    вариант 14

--- подготовка к егэ/вариант ЕГЭ/17.06 (вар 14).py
+++ подготовка к егэ/вариант ЕГЭ/17.06 (вар 14).py
@@ -1,8 +1,6 @@
-for num in range(1000, 10000):
-    a, b, c, d = map(int, list(str(num)))
-    s1 = a * b
-    s2 = c * d
-    res = ''.join(list(map(str, sorted([s1, s2])[::-1])))
-    if res == "124":
-        print(num)
-        break
+n = 1
+s = 0
+while n <= 300:
+    s = s + 30
+    n = n * 5
+print(s)
