7cd8dc7ece67830c262787dcf28a15652b3843d0
Rybolovlev Alexey <rybolovlevalexey@yandex.ru>
2022-06-17 19:29:01 +0500

    вариант 14

--- подготовка к егэ/вариант ЕГЭ/17.06 (вар 14).py
+++ подготовка к егэ/вариант ЕГЭ/17.06 (вар 14).py
@@ -1,8 +1,16 @@
-n = 1
-file = open('24_demo (2).txt', 'r')
-st = file.read().strip()
-ans = list()
-for i in range(1, 10000):
-    if st.count('Y' * i) >= 1:
-        ans.append(i)
-print(max(ans))
+ansd = 0
+ans = None
+for num in range(568023, 569230+1):
+    sp = set()
+    sp.add(1)
+    sp.add(num)
+    for d in range(2, int(num**0.5) + 1):
+        if num % d == 0:
+            sp.add(d)
+            sp.add(num // d)
+    if ans is None or len(sp) > ansd:
+        ans = num
+        ansd = len(sp)
+    if len(sp) == 144:
+        print(sp)
+print(ansd, ans)
