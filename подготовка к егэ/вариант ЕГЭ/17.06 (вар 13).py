a1e7454dee3ceb9d86d311e64db422226a08ccfa
Rybolovlev Alexey <rybolovlevalexey@yandex.ru>
2022-06-17 13:16:51 +0500

    вариант 13

--- подготовка к егэ/вариант ЕГЭ/17.06 (вар 13).py
+++ подготовка к егэ/вариант ЕГЭ/17.06 (вар 13).py
@@ -1,7 +1,15 @@
-file = open('zadanie24_1.txt', 'r')
-st = file.read().strip()
-n = 1
-while st.count('AB' * (n + 1)) >= 1:
-    n += 1
-print(n)
-print(st.count('AB' * 12))
+for num in range(95632, 95650 + 1):
+    sp = set()
+    sp.add(1)
+    x = num
+    while x % 2 == 0:
+        x //= 2
+    sp.add(x)
+    for d in range(3, int(x**0.5) + 1, 2):
+        if x % d == 0:
+            sp.add(d)
+            sp.add(x // d)
+        if len(sp) > 6:
+            break
+    if len(sp) == 6:
+        print(sorted(sp))
