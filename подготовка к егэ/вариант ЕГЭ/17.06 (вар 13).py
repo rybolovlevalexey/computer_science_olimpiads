a268d3cc53e2502946e70b1982acaec0d580c49b
Rybolovlev Alexey <rybolovlevalexey@yandex.ru>
2022-06-17 11:43:36 +0500

    вариант 13

--- подготовка к егэ/вариант ЕГЭ/17.06 (вар 13).py
+++ подготовка к егэ/вариант ЕГЭ/17.06 (вар 13).py
@@ -1,9 +1,11 @@
-sp = list()
-def f(n):
-    if n > 0:
-        sp.append(n)
-        f(n // 2)
-        f(n - 4)
-
-f(9)
-print(''.join(map(str, sp)))
+file = open('17 (17).txt', 'r')
+sp = list(map(int, file.read().strip().split('\n')))
+cnt = 0
+mxsm = 0
+for i in range(len(sp) - 1):
+    for j in range(i + 1, len(sp)):
+        if sp[i] * sp[j] % 14 != 0:
+            cnt += 1
+            if sp[i] + sp[j] > mxsm:
+                mxsm = sp[i] + sp[j]
+print(cnt, mxsm)
