42867adc883a9ae4703b5ca7ca44100ba7e39e01
Rybolovlev Alexey <rybolovlevalexey@yandex.ru>
2022-06-17 12:55:31 +0500

    вариант 13

--- подготовка к егэ/вариант ЕГЭ/17.06 (вар 13).py
+++ подготовка к егэ/вариант ЕГЭ/17.06 (вар 13).py
@@ -1,13 +1,26 @@
-for num in range(10000):
-    x = num
-    a = 0
-    b = 0
-    while x > 0:
-        if x % 2 == 0:
-            a += 1
-        else:
-            b += x % 6
-        x = x // 6
-    if a == 1 and b == 6:
-        print(num)
-        break
+dp = [0] * 31
+dp[1] = 1
+for i in range(2, 11):
+    if i % 2 == 0:
+        dp[i] = dp[i // 2] + dp[i - 1]
+    else:
+        dp[i] = dp[i - 1]
+print(dp)
+for i in range(10):
+    dp[i] = 0
+print(dp)
+for i in range(11, 22):
+    if i % 2 == 0:
+        dp[i] = dp[i // 2] + dp[i - 1]
+    else:
+        dp[i] = dp[i - 1]
+print(dp)
+for i in range(21):
+    dp[i] = 0
+print(dp)
+for i in range(22, 31):
+    if i % 2 == 0:
+        dp[i] = dp[i // 2] + dp[i - 1]
+    else:
+        dp[i] = dp[i - 1]
+print(dp)
