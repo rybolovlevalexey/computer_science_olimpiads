cc3363b9a981c6454eb1297665479df40cd93e85
Rybolovlev Alexey <rybolovlevalexey@yandex.ru>
2022-06-17 18:50:17 +0500

    вариант 14

--- подготовка к егэ/вариант ЕГЭ/17.06 (вар 14).py
+++ подготовка к егэ/вариант ЕГЭ/17.06 (вар 14).py
@@ -1,15 +1,12 @@
-from functools import lru_cache
+file = open('17 (18).txt', 'r')
+sp = list(map(int, file.read().strip().split('\n')))
+cnt = 0
+mxsm = 0
+for i in range(len(sp) - 1):
+    for j in range(i + 1, len(sp)):
+        if (sp[i] + sp[j]) % 60 == 0 and (sp[i] % 40 == 0 or sp[j] % 40 == 0):
+            cnt += 1
+            if mxsm < sp[i] + sp[j]:
+                mxsm = sp[i] + sp[j]
 
-@lru_cache()
-def f(n):
-    if n > 2:
-        return f(n - 1) + g(n - 2)
-    return n + 1
-@lru_cache()
-def g(n):
-    if n > 2:
-        return g(n - 1) + f(n - 2)
-    return n
-
-
-print(g(77))
+print(cnt, mxsm)
