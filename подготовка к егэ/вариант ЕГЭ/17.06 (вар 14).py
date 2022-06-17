c4738dc8eca322992f3ca3dd7d2463d8ed52e7c6
Rybolovlev Alexey <rybolovlevalexey@yandex.ru>
2022-06-17 19:48:23 +0500

    вариант 14

--- подготовка к егэ/вариант ЕГЭ/17.06 (вар 14).py
+++ подготовка к егэ/вариант ЕГЭ/17.06 (вар 14).py
@@ -1,20 +1,10 @@
-file = open('28139.txt', 'r')
-sp = file.read().strip().split('\n')
-s, n = map(int, sp[0].split())
-del sp[0]
-sp = list(sorted(map(int, sp)))
-summa = 0
-cnt = 0
-lastelem = 0
+file = open('27986_B.txt', 'r')
+sp = list(map(int, file.read().strip().split('\n')))
+m = 0
+m7 = 0
 for elem in sp:
-    if elem + summa <= s:
-        summa += elem
-        cnt += 1
-        lastelem = elem
-print(cnt)
-print(summa, s, lastelem)
-summa -= lastelem
-for elem in sp[::-1]:
-    if elem + summa <= s:
-        print(elem)
-        break
+    if elem % 7 == 0 and elem > m7:
+        m7 = elem
+    if elem % 7 != 0 and elem > m:
+        m = elem
+print(m * m7)
