32bd976aa29c65fbf5d75d431ec91ca09dfc1b3d
Rybolovlev Alexey <rybolovlevalexey@yandex.ru>
2022-06-17 11:23:12 +0500

    вариант 13

--- подготовка к егэ/вариант ЕГЭ/17.06 (вар 13).py
+++ подготовка к егэ/вариант ЕГЭ/17.06 (вар 13).py
@@ -1,9 +1,11 @@
-cnt = 0
-first = ['x', 'y', 'z']
-second = ['a', 'b', 'c', 'd']
-for i1 in first:
-    for i2 in first:
-        for i3 in second:
-            for i4 in second:
-                cnt += 1
-print(cnt)
+ans = list()
+for a in range(-100, 1000):
+    flag = True
+    for x in range(10000):
+        res = (x & 51 == 0) or (x & 41 != 0 or x & a == 0)
+        if not res:
+            flag = False
+    if flag:
+        ans.append(a)
+        print(a)
+print(ans)
