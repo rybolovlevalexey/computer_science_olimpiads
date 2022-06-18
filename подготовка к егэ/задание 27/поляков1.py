137eeb8385d8291db636afc352d0b6082cb1d98f
Rybolovlev Alexey <rybolovlevalexey@yandex.ru>
2022-06-18 21:14:36 +0500

    №27.52, ответ:2919 299988174

--- подготовка к егэ/задание 27/поляков1.py
+++ подготовка к егэ/задание 27/поляков1.py
@@ -1,21 +1,16 @@
-file = open('27data/51/27-51b.txt', 'r')
+file = open('27data/52/27-52b.txt', 'r')
 sp = file.read().strip().split('\n')
 n = int(sp[0])
 del sp[0]
-nechet = chet = 0
-summa = 0
-mndl = None
-for elem in sp:
-    a, b = sorted(map(int, elem.split()))
-    summa += a
-    if a % 2 == 0:
-        chet += 1
-    else:
-        nechet += 1
-    if a % 2 != b % 2 and (mndl is None or mndl > b - a) and b != a:
-        mndl = b - a
-
-print(summa)
-print(chet, nechet)
-summa += mndl
-print(summa)
+sp = sorted(map(int, sp))[::-1]
+mx1 = sorted(filter(lambda x: x % 3 == 1, sp))[::-1][:3]
+mx2 = sorted(filter(lambda x: x % 3 == 2, sp))[::-1][:3]
+mx = sorted(filter(lambda x: x % 3 == 0, sp))[::-1][:3]
+ans = list()
+ans.append(max(mx1) + max(mx2) + max(mx))
+ans.append(sum(mx1))
+ans.append(sum(mx))
+ans.append(sum(mx2))
+print(ans)
+print(max(ans))
+print(max(ans) % 3)
