e3268b3a5db1ade7a9232c89a80fc3912f78edda
Rybolovlev Alexey <rybolovlevalexey@yandex.ru>
2022-06-18 22:01:10 +0500

    фоксфорд

--- подготовка к егэ/задание 25/фоксфорд.py
+++ подготовка к егэ/задание 25/фоксфорд.py
@@ -1,8 +1,17 @@
-for num in range(247264322, 369757523 + 1):
+k = 0
+num = 500000000 + 1
+while k < 5:
     sp = list()
-    for d1 in range(2, int(num **0.5) + 1):
-        if num % d1 == 0:
-            sp.append(d1)
-            #for d2 in range(2, int(num))
-    if len(sp) == 3:
-        print(num, max(sp))
+    for d in range(2, int(num**0.5) + 1):
+        if num % d == 0:
+            sp.append(d)
+            sp.append(num // d)
+        if len(sp) >= 6:
+            break
+    if len(sp) >= 3:
+        sp = sorted(sp)
+        s = sp[-1] + sp[-2] + sp[-3]
+        if sum(map(int, list(str(s)))) == 33:
+            print(s, max(sp))
+            k += 1
+    num += 1
