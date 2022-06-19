dee6f51f113bb085e83c0ec47da4dadaacf61039
Rybolovlev Alexey <rybolovlevalexey@yandex.ru>
2022-06-19 09:16:23 +0500

    фоксфорд

--- подготовка к егэ/задание 27/фоксфорд.py
+++ подготовка к егэ/задание 27/фоксфорд.py
@@ -1,16 +1,33 @@
-from functools import lru_cache
+def is_prost(x):
+    for d in range(2, int(x**0.5) + 1):
+        if x % d == 0:
+            return False
+    return True
 
-@lru_cache()
-def f(n):
-    if n == 1:
-        return 1
-    return f(n - 1) - 2*g(n - 1)
+file = open('27A (1).txt', 'r')
+sp = list(map(int, file.read().strip().split('\n')))
+n = sp[0]
+del sp[0]
+ind = list()
+for i in range(n):
+    elem = sp[i]
+    if is_prost(elem):
+        ind.append(i)
+mxsm = 0
+poz1 = 0
+poz2 = 6
 
-def g(n):
-    if n == 1:
-        return 1
-    return f(n - 1) + g(n - 1) + n
-
-print(g(36))
-res = sum(map(int, list(str(g(36)))))
-print(res)
+while True:
+    i1 = ind[poz1]
+    i2 = ind[poz2]
+    summa = sum(sp[i1: i2])
+    print(sp[i1: i2])
+    if summa > mxsm:
+        mxsm = summa
+    poz2 += 6
+    if poz2 >= len(ind):
+        poz1 += 1
+        poz2 = poz1 + 6
+        if poz1 >= len(ind) or poz2 >= len(ind):
+            break
+print(mxsm)
