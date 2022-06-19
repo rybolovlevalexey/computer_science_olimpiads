ce5a1721ef15e7b6f7fafe4084b2256e613aaf6d
Rybolovlev Alexey <rybolovlevalexey@yandex.ru>
2022-06-19 14:22:33 +0500

    вариант 15

--- подготовка к егэ/вариант ЕГЭ/19.06 (вар 15).py
+++ подготовка к егэ/вариант ЕГЭ/19.06 (вар 15).py
@@ -1,13 +1,7 @@
-for a in range(100, 200):
-    flag = True
-    for x in range(10000):
-        for y in range(10000):
-            res = (2*x + 3*y < a) or (x >= y) or (y > 24)
-            if not res:
-                flag = False
-                break
-        if not flag:
-            break
-    if flag:
-        print(a)
-        break
+def f(n):
+    if n > 2:
+        return f(n - 1) + f(n - 2)
+    else:
+        return 1
+
+print(f(6))
