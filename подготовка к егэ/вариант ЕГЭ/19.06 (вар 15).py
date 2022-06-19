279a21bfe46e1264c3a2ccec7770e544801bbd38
Rybolovlev Alexey <rybolovlevalexey@yandex.ru>
2022-06-19 15:51:00 +0500

    вариант 15

--- подготовка к егэ/вариант ЕГЭ/19.06 (вар 15).py
+++ подготовка к егэ/вариант ЕГЭ/19.06 (вар 15).py
@@ -1,12 +1,19 @@
-for num in range(12, 14 + 1):
-    sp = set()
-    sp.add(1)
-    sp.add(num)
-    for d in range(2, int(num**0.5) + 1):
-        if num % d == 0:
-            sp.add(d)
-            sp.add(num // d)
-        if len(sp) >= 5:
-            break
-    if len(sp) == 4:
-        print(sorted(sp))
+file = open('28138.txt', 'r')
+sp = file.read().strip().split('\n')
+s, n = map(int, sp[0].split())
+del sp[0]
+sp = list(sorted(map(int, sp)))
+summa = 0
+cnt = 0
+lastelem = 0
+for elem in sp:
+    if elem + summa <= s:
+        summa += elem
+        lastelem = elem
+        cnt += 1
+summa -= lastelem
+for elem in sp[::-1]:
+    if summa + elem <= s:
+        print(elem)
+        break
+print(summa + elem, s)
