cf3ac4245948e67781593037eb0f30dfdfbf06d3
Rybolovlev Alexey <rybolovlevalexey@yandex.ru>
2022-06-19 14:42:12 +0500

    вариант 15

--- подготовка к егэ/вариант ЕГЭ/19.06 (вар 15).py
+++ подготовка к егэ/вариант ЕГЭ/19.06 (вар 15).py
@@ -1,11 +1,12 @@
-for num in range(100000):
-    x = num
-    a = 0
-    b = 1
-    while x > 0:
-        a += 1
-        b = b * (x % 1000)
-        x = x // 1000
-    if a == 2 and b == 11:
-        print(num)
-        break
+dp = [0] * 14
+dp[1] = 1
+for i in range(2, 14):
+    if i % 6 == 0:
+        dp[i] = dp[i - 1] + dp[i // 2] + dp[i // 3]
+    elif i % 2 == 0:
+        dp[i] = dp[i - 1] + dp[i // 2]
+    elif i % 3 == 0:
+        dp[i] = dp[i - 1] + dp[i // 3]
+    else:
+        dp[i] = dp[i - 1]
+print(dp)
