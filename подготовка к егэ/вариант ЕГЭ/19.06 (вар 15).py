6fb3d05d8135a9afda4d3e6f83adda4f9c10068f
Rybolovlev Alexey <rybolovlevalexey@yandex.ru>
2022-06-19 14:20:55 +0500

    вариант 15

--- подготовка к егэ/вариант ЕГЭ/19.06 (вар 15).py
+++ подготовка к егэ/вариант ЕГЭ/19.06 (вар 15).py
@@ -1,13 +1,13 @@
-def convert(x, q):
-    st = ''
-    while x > 0:
-        st += str(x % q)
-        x //= q
-    return st[::-1]
-
-num = int('65', 8)
-for i in range(2, 15):
-    if convert(num, i) == '311':
-        print(i)
-print(int('311', 4))
-print(int('65', 8))
+for a in range(100, 200):
+    flag = True
+    for x in range(10000):
+        for y in range(10000):
+            res = (2*x + 3*y < a) or (x >= y) or (y > 24)
+            if not res:
+                flag = False
+                break
+        if not flag:
+            break
+    if flag:
+        print(a)
+        break
