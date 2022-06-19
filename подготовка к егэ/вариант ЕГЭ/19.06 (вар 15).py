60d6250f2e3305c40a59e5f7e9edb910e3811b3f
Rybolovlev Alexey <rybolovlevalexey@yandex.ru>
2022-06-19 13:50:09 +0500

    вариант 15

--- подготовка к егэ/вариант ЕГЭ/19.06 (вар 15).py
+++ подготовка к егэ/вариант ЕГЭ/19.06 (вар 15).py
@@ -1,11 +1,6 @@
-ans = list()
-for num in range(1000, 10000):
-    a, b, c, d = map(int, list(str(num)))
-    s1 = a + b
-    s2 = b + c
-    s3 = c + d
-    res = ''.join(list(map(str, sorted([s1, s2, s3])[1:])))
-    if res == '1517':
-        ans.append(num)
-print(ans)
-print(max(ans))
+s = 0
+n = 0
+while s < 165:
+    s += 15
+    n += 2
+print(n)
