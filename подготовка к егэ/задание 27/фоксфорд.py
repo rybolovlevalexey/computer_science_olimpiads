d5028a5b4b49aadf824f8be912e7645bcd7784f3
Rybolovlev Alexey <rybolovlevalexey@yandex.ru>
2022-06-19 10:19:18 +0500

    фоксфорд

--- подготовка к егэ/задание 27/фоксфорд.py
+++ подготовка к егэ/задание 27/фоксфорд.py
@@ -4,7 +4,7 @@ def is_prost(x):
             return False
     return True
 
-file = open('27A (1).txt', 'r')
+file = open('27B (1).txt', 'r')
 sp = list(map(int, file.read().strip().split('\n')))
 n = sp[0]
 del sp[0]
@@ -14,31 +14,24 @@ for i in range(n):
     if is_prost(elem) and len(str(elem)) == 3:
         ind.append(i)
 mxsm = 0
-poz1 = 0
-poz2 = 5
-print(ind)
+poz1 = 1
+poz2 = poz1 + 5
 while True:
-    if poz1 > 0 and poz2 + 1 < len(ind):
+    if poz2 + 1 >= len(ind):
+        i1 = ind[poz1 - 1] + 1
+        srez = sp[i1:]
+    else:
         i1 = ind[poz1 - 1] + 1
         i2 = ind[poz2 + 1]
         srez = sp[i1: i2]
-    else:
-        i1 = ind[poz1]
-        i2 = ind[poz2]
-        srez = sp[i1: i2]
+
     summa = sum(srez)
-    coutn = 0
-    for elem in srez:
-        if is_prost(elem) and len(str(elem)) == 3:
-            coutn += 1
-    print(coutn)
-    # ......
     if summa > mxsm:
         mxsm = summa
-    poz2 += 5
+    poz2 += 6
     if poz2 >= len(ind):
         poz1 += 1
         poz2 = poz1 + 5
-        if poz1 >= len(ind) or poz2 >= len(ind):
-            break
+    if poz2 >= len(ind) or poz1 >= len(ind):
+        break
 print(mxsm)
