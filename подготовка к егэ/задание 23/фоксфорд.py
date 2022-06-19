6079eb9fab2a67b808716329bcc5d5a770cbc4e4
Rybolovlev Alexey <rybolovlevalexey@yandex.ru>
2022-06-19 20:49:02 +0500

    фоксфорд

--- подготовка к егэ/задание 23/фоксфорд.py
+++ подготовка к егэ/задание 23/фоксфорд.py
@@ -1,27 +1,27 @@
-dp = [0] * 35
-dp[4] = 1
-for i in range(5, 11):
-    if i % 6 == 0:
-        dp[i] = dp[i - 1] + dp[i // 2] + dp[i // 3]
-    elif i % 2 == 0:
-        dp[i] = dp[i - 1] + dp[i // 2]
+dp = [0] * 66
+dp[2] = 1
+for i in range(3, 17):
+    if i % 12 == 0:
+        dp[i] = dp[i - 1] + dp[i // 3] + dp[i // 4]
     elif i % 3 == 0:
         dp[i] = dp[i - 1] + dp[i // 3]
+    elif i % 4 == 0:
+        dp[i] = dp[i - 1] + dp[i // 4]
     else:
         dp[i] = dp[i - 1]
 print(dp)
-for i in range(10):
+for i in range(16):
     dp[i] = 0
 print(dp)
-for i in range(11, 35):
-    if i == 18:
+for i in range(17, 66):
+    if i == 21:
         continue
-    if i % 6 == 0:
-        dp[i] = dp[i - 1] + dp[i // 2] + dp[i // 3]
-    elif i % 2 == 0:
-        dp[i] = dp[i - 1] + dp[i // 2]
+    if i % 12 == 0:
+        dp[i] = dp[i - 1] + dp[i // 3] + dp[i // 4]
     elif i % 3 == 0:
         dp[i] = dp[i - 1] + dp[i // 3]
+    elif i % 4 == 0:
+        dp[i] = dp[i - 1] + dp[i // 4]
     else:
         dp[i] = dp[i - 1]
 print(dp)
