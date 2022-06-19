762ddd48826417274cf0b3ea7af311e2de9df7d7
Rybolovlev Alexey <rybolovlevalexey@yandex.ru>
2022-06-19 21:06:13 +0500

    фоксфорд

--- подготовка к егэ/задание 23/фоксфорд.py
+++ подготовка к егэ/задание 23/фоксфорд.py
@@ -1,10 +1,9 @@
-for i in range(1, 50):
-    for j in range(1, 50):
-        for k in range(1, 50):
-            st = '0' + '1' * i + '2' * j + '3' * k
-            while '01' in st or '02' in st or '03' in st:
-                st = st.replace('01', '210', 1)
-                st = st.replace('02', '3101', 1)
-                st = st.replace('03', '2302', 1)
-            if st.count('1') == 52 and st.count('2') == 33 and st.count('3') == 23:
-                print(j)
+st = '>' + 30 * '1' + 10 * '2' + 20 * '3'
+while '>1' in st or '>2' in st or '>3' in st:
+    if '>1' in st:
+        st = st.replace('>1', '22>', 1)
+    if '>2' in st:
+        st = st.replace('>2', '2>', 1)
+    if '>3' in st:
+        st = st.replace('>3', '1>', 1)
+print(st.count('1') + st.count('2') * 2)
