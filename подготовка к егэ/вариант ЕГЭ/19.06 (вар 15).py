1f746c046b512235c0a3f2d0a88e52cdc5de75f4
Rybolovlev Alexey <rybolovlevalexey@yandex.ru>
2022-06-19 15:57:27 +0500

    вариант 15

--- подготовка к егэ/вариант ЕГЭ/19.06 (вар 15).py
+++ подготовка к егэ/вариант ЕГЭ/19.06 (вар 15).py
@@ -1,19 +1,14 @@
-file = open('28138.txt', 'r')
+file = open('27-B (4).txt', 'r')
 sp = file.read().strip().split('\n')
-s, n = map(int, sp[0].split())
 del sp[0]
 sp = list(sorted(map(int, sp)))
-summa = 0
-cnt = 0
-lastelem = 0
-for elem in sp:
-    if elem + summa <= s:
-        summa += elem
-        lastelem = elem
-        cnt += 1
-summa -= lastelem
-for elem in sp[::-1]:
-    if summa + elem <= s:
-        print(elem)
-        break
-print(summa + elem, s)
+m1 = sorted(filter(lambda x: x % 3 == 1, sp))[:3]
+m2 = sorted(filter(lambda x: x % 3 == 2, sp))[:3]
+m = sorted(filter(lambda x: x % 3 == 0, sp))[:3]
+ans = list()
+ans.append(min(m1) + min(m2) + min(m))
+ans.append(sum(m))
+ans.append(sum(m1))
+ans.append(sum(m2))
+print(min(ans), min(ans) % 3)
+print(ans)
