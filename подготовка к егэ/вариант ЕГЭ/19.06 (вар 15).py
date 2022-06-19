9210a688f24fed24899b8b4cc925ea3c904a3206
Rybolovlev Alexey <rybolovlevalexey@yandex.ru>
2022-06-19 15:42:41 +0500

    вариант 15

--- подготовка к егэ/вариант ЕГЭ/19.06 (вар 15).py
+++ подготовка к егэ/вариант ЕГЭ/19.06 (вар 15).py
@@ -1,12 +1,7 @@
-dp = [0] * 14
-dp[1] = 1
-for i in range(2, 14):
-    if i % 6 == 0:
-        dp[i] = dp[i - 1] + dp[i // 2] + dp[i // 3]
-    elif i % 2 == 0:
-        dp[i] = dp[i - 1] + dp[i // 2]
-    elif i % 3 == 0:
-        dp[i] = dp[i - 1] + dp[i // 3]
-    else:
-        dp[i] = dp[i - 1]
-print(dp)
+file = open('zadanie24_2 (3).txt', 'r')
+st = file.read().strip()
+ans = list()
+for i in range(1, 1000):
+    if st.count('L' * i) > 0:
+        ans.append(i)
+print(max(ans))
