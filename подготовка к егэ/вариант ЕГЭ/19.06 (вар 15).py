c0debefccaba676709241e4f01f80cf0574de903
Rybolovlev Alexey <rybolovlevalexey@yandex.ru>
2022-06-19 14:11:01 +0500

    вариант 15

--- подготовка к егэ/вариант ЕГЭ/19.06 (вар 15).py
+++ подготовка к егэ/вариант ЕГЭ/19.06 (вар 15).py
@@ -1,9 +1,13 @@
-st = '1' + 99 * '8' + '1'
-while '81' in st or '882' in st or '8883' in st:
-    if '81' in st:
-        st = st.replace('81', '2', 1)
-    elif '882' in st:
-        st = st.replace('882', '3', 1)
-    else:
-        st = st.replace('8883', '1', 1)
-print(st)
+def convert(x, q):
+    st = ''
+    while x > 0:
+        st += str(x % q)
+        x //= q
+    return st[::-1]
+
+num = int('65', 8)
+for i in range(2, 15):
+    if convert(num, i) == '311':
+        print(i)
+print(int('311', 4))
+print(int('65', 8))
