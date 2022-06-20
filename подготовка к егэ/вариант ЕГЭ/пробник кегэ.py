9e8b64d95dd92658626f7d96ecd9dbc3478206c8
Rybolovlev Alexey <rybolovlevalexey@yandex.ru>
2022-06-20 10:31:09 +0500

    пробник

--- подготовка к егэ/вариант ЕГЭ/пробник кегэ.py
+++ подготовка к егэ/вариант ЕГЭ/пробник кегэ.py
@@ -1,8 +1,11 @@
-print('x y z w res')
-for x in [True, False]:
-    for y in [True, False]:
-        for z in [True, False]:
-            for w in [True, False]:
-                res = (x or not y) and (x != z) and w
-                if res:
-                    print(int(x), int(y), int(z), int(w), int(res))
+for num in range(1000):
+    n = num
+    res = bin(n)[2:]
+    s = sum(map(int, list(res)))
+    res += str(s % 2)
+    s = sum(map(int, list(res)))
+    res += str(s % 2)
+    res = int(res, 2)
+    if res > 60:
+        print(res)
+        break
