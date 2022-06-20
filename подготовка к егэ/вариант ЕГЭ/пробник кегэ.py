a63e5903a94185550d68f556535126cae156900e
Rybolovlev Alexey <rybolovlevalexey@yandex.ru>
2022-06-20 11:53:30 +0500

    пробник

--- подготовка к егэ/вариант ЕГЭ/пробник кегэ.py
+++ подготовка к егэ/вариант ЕГЭ/пробник кегэ.py
@@ -1,10 +1,11 @@
-file = open('17 (20).txt', 'r')
-sp = list(map(int, file.read().strip().split('\n')))
-cnt = 0
-mxsm = None
-for i in range(len(sp) - 1):
-    if abs(sp[i]) % 5 == 0 and abs(sp[i + 1]) % 5 == 0 and abs(sp[i]) % 3 != 0 and abs(sp[i + 1]) % 3 != 0:
-        cnt += 1
-        if sp[i] + sp[i + 1] > 0 and (mxsm is None or sp[i] + sp[i + 1] < mxsm):
-            mxsm = sp[i] + sp[i + 1]
-print(cnt, mxsm)
+for num in range(-1000, 1000):
+    x = num
+    a = 0
+    b = 1
+    while x > 0:
+        a = a + 1
+        b = b * (x % 10)
+        x = x // 10
+    if a == 2 and b == 15:
+        print(num)
+        break
