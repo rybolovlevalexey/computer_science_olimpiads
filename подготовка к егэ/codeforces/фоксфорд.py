62e4d1ab0ba8edb5daec39a477058e9bcbc86e25
Rybolovlev Alexey <rybolovlevalexey@yandex.ru>
2022-06-20 18:42:22 +0500

    фоксфорд

--- подготовка к егэ/codeforces/фоксфорд.py
+++ подготовка к егэ/codeforces/фоксфорд.py
@@ -1,29 +1,26 @@
-file = open('26_2(1).txt', 'r')
+file = open('26 (10)(2).txt', 'r')
 sp = file.read().strip().split('\n')
-n, s = map(int, sp[0].split())
+n, m = map(int, sp[0].split())
 del sp[0]
-typea = list()
-typeb = list()
+sp = sorted(list(map(int, sp)))
 summa = 0
+cnt = 0
+mxelem = 0
+for elem in sp[::-1]:
+    if 200 <= elem <= 220:
+        summa += elem
+        cnt += 1
+        if elem > mxelem:
+            mxelem = elem
 for elem in sp:
-    cost, count, type = elem.split()
-    if type == 'A':
-        typea.append([int(cost), int(count)])
-        summa += (int(cost) * int(count))
-    else:
-        typeb.append([int(cost), int(count)])
-typeb = sorted(typeb)
-cntb = 0
-money = s - summa
-for elem in typeb:
-    cost, count = elem
-    if cost * count <= money:
-        money -= cost * count
-        cntb += count
-    else:
-        for j in range(count, 0, -1):
-            if cost * j <= money:
-                money -= cost * j
-                cntb += j
-                break
-print(cntb, money)
+    if elem + summa <= m and not 200 <= elem <= 220:
+        cnt += 1
+        summa += elem
+        if elem > mxelem:
+            mxelem = elem
+summa -= mxelem
+for elem in sp[::-1]:
+    if summa + elem <= m:
+        print(elem)
+        print(summa, m)
+        break
