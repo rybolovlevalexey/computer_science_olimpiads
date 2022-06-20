0b79cc3ae4da6d5b5a02757e52ac2af5f747d66a
Rybolovlev Alexey <rybolovlevalexey@yandex.ru>
2022-06-20 12:53:01 +0500

    пробник

--- подготовка к егэ/вариант ЕГЭ/пробник кегэ.py
+++ подготовка к егэ/вариант ЕГЭ/пробник кегэ.py
@@ -1,19 +1,20 @@
-file = open('24 (10).txt', 'r')
-st = file.read().strip()
-dl = 1
-mxdl = 0
-ansst = ''
-s = st[0]
-for i in range(1, len(st)):
-    if int(s[-1]) <= int(st[i]):
-        dl += 1
-        s += st[i]
-    else:
-        if dl > mxdl:
-            mxdl = dl
-            ansst = s
-        dl = 1
-        s = st[i]
-print(mxdl, ansst)
-print(dl, s)
-print(st[st.index(ansst) - 1: st.index(ansst) + 49 + 1])
+def isprost(x):
+    for d in range(2, int(x**0.5)+1):
+        if x % d == 0:
+            return False
+    return True
+
+k = 0
+num = 670_001
+while k < 5:
+    s = 0
+    for d in range(2, int(num**0.5)+1):
+        if num % d == 0:
+            if isprost(d):
+                s += d
+            if isprost(num // d):
+                s += num // d
+    if s % 10 == 5:
+        print(num, s)
+        k += 1
+    num += 1
