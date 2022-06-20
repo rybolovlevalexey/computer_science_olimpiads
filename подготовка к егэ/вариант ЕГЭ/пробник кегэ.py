872b023820c55571c169f51e73838b8c9029c74e
Rybolovlev Alexey <rybolovlevalexey@yandex.ru>
2022-06-20 13:51:18 +0500

    пробник

--- подготовка к егэ/вариант ЕГЭ/пробник кегэ.py
+++ подготовка к егэ/вариант ЕГЭ/пробник кегэ.py
@@ -1,37 +1,16 @@
-file = open('26 (9).txt', 'r')
-sp = file.read().strip().split('\n')
-s, n = map(int, sp[0].split())
+file = open('27_A (2).txt', 'r')
+sp = list(map(int, file.read().strip().split('\n')))
+n = sp[0]
 del sp[0]
-percnt = 0
-cntB = 0
-afiles = list()
-bfiles = list()
-for i in range(n):
-    num, type = sp[i].split()
-    if type == 'A':
-        afiles.append(int(num))
-    else:
-        bfiles.append(int(num))
-afiles = sorted(afiles)[::-1]
-bfiles = sorted(bfiles)
-while len(afiles) > 0:
-    summa = 0
-    i = 0
-    while i < len(afiles):
-        elem = afiles[i]
-        if elem + summa <= s:
-            summa += elem
-            del afiles[i]
-        else:
-            i += 1
-    percnt += 1
-    i = 0
-    while i < len(bfiles):
-        elem = bfiles[i]
-        if elem + summa <= s:
-            summa += elem
-            cntB += 1
-            del bfiles[i]
-        else:
-            i += 1
-print(percnt, cntB)
+mxsm = 0
+mndl = 0
+for i in range(n - 1):
+    for j in range(i + 1, n + 1):
+        srez = sp[i: j]
+        summa = sum(filter(lambda x: x % 2 == 0, srez))
+        if summa % 16 == 0 and summa > mxsm:
+            mxsm = summa
+            mndl = len(srez)
+        elif summa % 16 == 0 and summa == mxsm and len(srez) < mndl:
+            mndl = len(srez)
+print(mndl)
