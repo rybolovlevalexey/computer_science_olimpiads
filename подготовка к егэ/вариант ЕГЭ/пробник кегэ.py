13797570c28e951365b4785600ff52decafb5b94
Rybolovlev Alexey <rybolovlevalexey@yandex.ru>
2022-06-20 10:43:05 +0500

    пробник

--- подготовка к егэ/вариант ЕГЭ/пробник кегэ.py
+++ подготовка к егэ/вариант ЕГЭ/пробник кегэ.py
@@ -1,12 +1,22 @@
-ans = list()
-for num in range(1000000):
-    s = num
-    s = s // 100
-    n = 1
-    while s < 51:
-        s = s + 5
-        n = n * 2
-    if n == 128:
-        ans.append(num)
-print(ans)
-print(max(ans))
+from itertools import product, permutations
+cnt = 0
+sp = ['в', 'з', 'г', 'л', 'я', 'д']
+
+for i1 in sp:
+    for i2 in sp:
+        for i3 in sp:
+            for i4 in sp:
+                st = i1 + i2 + i3 + i4
+                if st.count('з') == 1 or st.count('з') == 2:
+                    cnt += 1
+print(cnt)
+
+words = list(product(sp, repeat=4))
+cnt = 0
+for wor in words:
+    if ''.join(wor).count('з') == 1 or ''.join(wor).count('з') == 2:
+        cnt += 1
+print(cnt)
+
+words = permutations(sp)
+print(*words)
