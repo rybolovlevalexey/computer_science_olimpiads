a240dc71e8d8c143ea4b5c0139ffbc35519da3e4
Rybolovlev Alexey <rybolovlevalexey@yandex.ru>
2022-06-20 11:04:26 +0500

    пробник

--- подготовка к егэ/вариант ЕГЭ/пробник кегэ.py
+++ подготовка к егэ/вариант ЕГЭ/пробник кегэ.py
@@ -1,12 +1,6 @@
-ans = list()
-for a in range(10000):
-    flag = True
-    for x in range(10000):
-        res = (x & a == 0) or (x & 17 != 0) or (x & 5 != 0) or (x & 3 != 0)
-        if not res:
-            flag = False
-            break
-    if flag:
-        ans.append(a)
-print(ans)
-print(max(ans))
+def f(n):
+    if n <= 2:
+        return 1
+    return f(n - 1) + 2*f(n - 2)
+
+print(f(17))
