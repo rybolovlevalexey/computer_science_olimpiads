cf4100159bcdbc449ccb653230c7d0fc974127c8
Rybolovlev Alexey <rybolovlevalexey@yandex.ru>
2022-06-20 11:09:05 +0500

    пробник

--- подготовка к егэ/вариант ЕГЭ/пробник кегэ.py
+++ подготовка к егэ/вариант ЕГЭ/пробник кегэ.py
@@ -1,6 +1,10 @@
-def f(n):
-    if n <= 2:
-        return 1
-    return f(n - 1) + 2*f(n - 2)
-
-print(f(17))
+file = open('17 (20).txt', 'r')
+sp = list(map(int, file.read().strip().split('\n')))
+cnt = 0
+mxsm = None
+for i in range(len(sp) - 1):
+    if abs(sp[i]) % 5 == 0 and abs(sp[i + 1]) % 5 == 0 and abs(sp[i]) % 3 != 0 and abs(sp[i + 1]) % 3 != 0:
+        cnt += 1
+        if sp[i] + sp[i + 1] > 0 and (mxsm is None or sp[i] + sp[i + 1] < mxsm):
+            mxsm = sp[i] + sp[i + 1]
+print(cnt, mxsm)
