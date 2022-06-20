50e8654d91aa4c05537a3e7b350d11112563774d
Rybolovlev Alexey <rybolovlevalexey@yandex.ru>
2022-06-20 10:58:45 +0500

    пробник

--- подготовка к егэ/вариант ЕГЭ/пробник кегэ.py
+++ подготовка к егэ/вариант ЕГЭ/пробник кегэ.py
@@ -1,9 +1,7 @@
-st = '>' + 11 * '1' + 12 * '2' + 30 * '3'
-while '>1' in st or '>2' in st or '>3' in st:
-    if '>1' in st:
-        st = st.replace('>1', '22>', 1)
-    if '>2' in st:
-        st = st.replace('>2', '222>', 1)
-    if '>3' in st:
-        st = st.replace('>3', '1>', 1)
-print(st.count('2') * 2 + st.count('1'))
+num = 16**820 - 2**761 + 14
+cnt = 0
+while num > 0:
+    if num % 4 == 0:
+        cnt += 1
+    num //= 4
+print(cnt)
