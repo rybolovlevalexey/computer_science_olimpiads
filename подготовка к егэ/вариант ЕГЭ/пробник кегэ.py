0784520f4dcc74bdc0174af4b67ce3b39f399435
Rybolovlev Alexey <rybolovlevalexey@yandex.ru>
2022-06-20 13:53:42 +0500

    пробник

--- подготовка к егэ/вариант ЕГЭ/пробник кегэ.py
+++ подготовка к егэ/вариант ЕГЭ/пробник кегэ.py
@@ -4,13 +4,9 @@ n = sp[0]
 del sp[0]
 mxsm = 0
 mndl = 0
-for i in range(n - 1):
-    for j in range(i + 1, n + 1):
-        srez = sp[i: j]
-        summa = sum(filter(lambda x: x % 2 == 0, srez))
-        if summa % 16 == 0 and summa > mxsm:
-            mxsm = summa
-            mndl = len(srez)
-        elif summa % 16 == 0 and summa == mxsm and len(srez) < mndl:
-            mndl = len(srez)
-print(mndl)
+ind = list()
+for i in range(n):
+    elem = sp[i]
+    if elem % 2 == 0:
+        ind.append(i)
+print('ok')
