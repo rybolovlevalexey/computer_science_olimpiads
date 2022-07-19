cc69a5b2e261a7c6f02901fb17e8cddc19a7a8af
Rybolovlev Alexey <rybolovlevalexey@yandex.ru>
2022-07-19 16:29:08 +0500

    третья задача

--- подготовка к егэ/stepik/Введение в python.py
+++ подготовка к егэ/stepik/Введение в python.py
@@ -16,3 +16,10 @@ class Circle:
         pi = 3.14
         s = pi * self.radius**2
         return s
+
+    def do_intersect(self, other):
+        dis = self.center.dist(other.center)
+        if dis <= self.radius + other.radius:
+            return True
+        else:
+            return False
