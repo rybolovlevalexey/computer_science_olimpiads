5a29aba13309f88b7a4e973a06f236378accc0e1
Rybolovlev Alexey <rybolovlevalexey@yandex.ru>
2022-07-21 15:18:39 +0500

    норм

--- подготовка к егэ/stepik/Введение в python.py
+++ подготовка к егэ/stepik/Введение в python.py
@@ -1,40 +1,25 @@
-class Point:
-    def __init__(self, x, y):
-        self.x = int(x)
-        self.y = int(y)
+class Animal:
+    def __init__(self, name):
+        self.name = name
 
-    def dist(self, other):
-        res = ((self.x - other.x)**2 + (self.y - other.y)**2)**0.5
-        return res
+    def make_sound(self):
+        print(self.name + " says " + self.sound)
 
-class Circle:
-    def __init__(self, center, radius):
-        self.center = center
-        self.radius = radius
+    def move(self):
+        raise NotImplementedError()
 
-    def square(self):
-        pi = 3.14
-        s = pi * self.radius**2
-        return s
 
-    def do_intersect(self, other):
-        dis = self.center.dist(other.center)
-        if dis <= self.radius + other.radius:
-            return True
-        else:
-            return False
+class Dog(Animal):
+    def __init__(self, name):
+        super.__init__(name)
+        self.sound = 'woof'
+    def move(self):
+        return f'{self.name} walks'
 
-    def __eq__(self, other):
-        if self.square() == other.square():
-            return True
-        return False
 
-    def __lt__(self, other):
-        if self.square() < other.square():
-            return True
-        return False
-
-    def __le__(self, other):
-        if self.square() <= other.square():
-            return True
-        return False
+class Snake(Animal):
+    def __init__(self, name):
+        super.__init__(name)
+        self.sound = 'hsss'
+    def move(self):
+        return f'{self.name} crawles'
