8c3a5dbae58600eb0430bdc2840a4096da00043f
Rybolovlev Alexey <rybolovlevalexey@yandex.ru>
2022-07-21 15:56:03 +0500

    норм

--- подготовка к егэ/stepik/поколение python.py
+++ подготовка к егэ/stepik/поколение python.py
@@ -1,22 +1,12 @@
-first = input()
-second = input()
-fird = dict()
-secd = dict()
-for elem in first:
-    if elem in fird:
-        fird[elem] += 1
+sostnow = 0
+ans = list()
+st = input()
+i = 0
+while i < len(st):
+    if st[i] == '|':
+        ans.append(1)
+        i += 2
     else:
-        fird[elem] = 1
-for elem in second:
-    if elem in secd:
-        secd[elem] += 1
-    else:
-        secd[elem] = 1
-if len(fird) == len(secd):
-    for key in fird:
-        if fird[key] != secd[key]:
-            print(key)
-else:
-    for key, value in secd.items():
-        if value == 1 and key not in fird:
-            print(key)
+        ans.append(0)
+        i += 1
+print(''.join(list(map(str, ans))))
