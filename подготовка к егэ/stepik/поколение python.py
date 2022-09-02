b425b0f9f5e9691a5dafd63beede36bfa9642efb
Rybolovlev Alexey <rybolovlevalexey@yandex.ru>
2022-09-02 17:22:21 +0300

    норм

--- подготовка к егэ/stepik/поколение python.py
+++ подготовка к егэ/stepik/поколение python.py
@@ -1,19 +1,7 @@
-n = int(input())
-i = 1
-num = 1
-sp = list()
-while len(sp) < n:
-    r = list()
-    for j in range(num, num + i):
-        r.append(j)
-    print(r)
-    num += i
-    i += 1
-    r = r[::-1]
-    sp.extend(r)
-ans = list()
-i = 0
-while len(ans) < n:
-    ans.append(sp[i])
-    i += 1
-print(*ans)
+a = int(input())
+b = int(input())
+ans = 0
+while a >= b:
+    ans += 1
+    a -= b
+print(ans)
