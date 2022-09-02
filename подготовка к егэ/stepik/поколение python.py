2bb75434b0d9876bc59cc5a68c9a6d7eb965b1a2
Rybolovlev Alexey <rybolovlevalexey@yandex.ru>
2022-09-02 17:28:42 +0300

    норм

--- подготовка к егэ/stepik/поколение python.py
+++ подготовка к егэ/stepik/поколение python.py
@@ -1,7 +1,9 @@
-a = int(input())
-b = int(input())
-ans = 0
-while a >= b:
-    ans += 1
-    a -= b
-print(ans)
+ans = 0  # количество счастливых билетов
+cnt = int(input())  # количество билетов
+for i in range(cnt):
+    num = int(input())  # текущий билет
+    first = sum(map(int, list(str(num)[:3])))  # сумма первых трёх цифр
+    second = sum(map(int, list(str(num)[3:])))  # сумма вторых трёх цифр
+    if first == second:
+        ans += 1
+print(ans)  # вывод количества счастливых билетов
