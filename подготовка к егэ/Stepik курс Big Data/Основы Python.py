25e10e28b34441ef80ef0f8a7ee6053b304479a7
Rybolovlev Alexey <rybolovlevalexey@yandex.ru>
2022-09-03 12:04:44 +0300

    норм

--- подготовка к егэ/Stepik курс Big Data/Основы Python.py
+++ подготовка к егэ/Stepik курс Big Data/Основы Python.py
@@ -1,4 +1,9 @@
-n = int(input())
-sp = list(map(int, input().split()))
-x = int(input())
-print('YES' if x in sp else 'NO')
+st = input().split()
+n = int(st[0])
+del st[0]
+sp = list(map(int, st))
+num = min(sp)
+for i in range(len(sp)):
+    if sp[i] == max(sp):
+        sp[i] = num
+print(*sp)
