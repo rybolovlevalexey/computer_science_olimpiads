47f7e04dc1e70c9f311a8a643be60a2b6f13d61f
Rybolovlev Alexey <rybolovlevalexey@yandex.ru>
2022-09-03 12:20:56 +0300

    норм

--- подготовка к егэ/Stepik курс Big Data/Основы Python.py
+++ подготовка к егэ/Stepik курс Big Data/Основы Python.py
@@ -1,18 +1,17 @@
-n, m = map(int, input().split())
-cnt = 0
+x = int(input())
+n = int(input())
 sp = list()
 for i in range(n):
     st = list(map(int, input().split()))
     sp.append(st)
-for elem in sp:
-    num = min(elem)
-    for i in range(len(elem)):
-        if num == elem[i]:
-            flag = True
-            for j in range(n):
-                if sp[j][i] > num:
-                    flag = False
-                    break
-            if flag:
-                cnt += 1
-print(cnt)
+matrix = list()
+for i in range(n):
+    st = list()
+    for j in range(n):
+        st.append(sp[j][i])
+    matrix.append(st)
+for elem in matrix:
+    if x in elem:
+        print('YES')
+    else:
+        print('NO')
