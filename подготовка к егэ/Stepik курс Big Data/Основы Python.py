18d39dbbffcfc63128459ac8f931aedfd6ff4460
Rybolovlev Alexey <rybolovlevalexey@yandex.ru>
2022-09-03 12:13:54 +0300

    норм

--- подготовка к егэ/Stepik курс Big Data/Основы Python.py
+++ подготовка к егэ/Stepik курс Big Data/Основы Python.py
@@ -1,4 +1,18 @@
-n = int(input())
-sp = list(map(int, input().split()))
-mx = max(sp)
-print(max(filter(lambda x: x != mx, sp)))
+n, m = map(int, input().split())
+cnt = 0
+sp = list()
+for i in range(n):
+    st = list(map(int, input().split()))
+    sp.append(st)
+for elem in sp:
+    num = min(elem)
+    for i in range(len(elem)):
+        if num == elem[i]:
+            flag = True
+            for j in range(n):
+                if sp[j][i] > num:
+                    flag = False
+                    break
+            if flag:
+                cnt += 1
+print(cnt)
