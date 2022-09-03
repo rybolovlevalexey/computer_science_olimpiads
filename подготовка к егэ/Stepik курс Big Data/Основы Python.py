f49521fdbbce4ca0aa9ebb7856ae0891d09b7b7a
Rybolovlev Alexey <rybolovlevalexey@yandex.ru>
2022-09-03 12:01:59 +0300

    норм

--- подготовка к егэ/Stepik курс Big Data/Основы Python.py
+++ подготовка к егэ/Stepik курс Big Data/Основы Python.py
@@ -1,8 +1,4 @@
 n = int(input())
 sp = list(map(int, input().split()))
 x = int(input())
-cnt = 0
-for elem in sp:
-    if elem == x:
-        cnt += 1
-print(cnt)
+print('YES' if x in sp else 'NO')
