55db81647714d6a3728400915f4a4f8593636395
Rybolovlev Alexey <rybolovlevalexey@yandex.ru>
2022-09-18 18:18:11 +0300

    норм

--- подготовка к егэ/stepik/практикум по программированию.py
+++ подготовка к егэ/stepik/практикум по программированию.py
@@ -1,12 +1,5 @@
-sp = ['1', '2', '2', '3', '3']
-ans = set()
-for i1 in range(5):
-    for i2 in range(5):
-        for i3 in range(5):
-            for i4 in range(5):
-                for i5 in range(5):
-                    if i1 + i2 + i3 + i4 + i5 == 10:
-                        st = sp[i1] + sp[i2] + sp[i3] + sp[i4] + sp[i5]
-                        ans.add(st)
-                        print(st)
-print(len(ans))
+num = int(input())
+sp = list()
+for i in range(1, num // 2 + 1):
+    if num % i == 0 and i not in sp and (num // i) not in sp:
+        print(f"{num} = {i} * {num // i}")
