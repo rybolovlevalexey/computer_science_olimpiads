dff23a52fe53bc40073eac42d829c847966c525e
Rybolovlev Alexey <rybolovlevalexey@yandex.ru>
2022-09-25 17:41:50 +0300

    норм

--- подготовка к егэ/stepik/практикум по программированию.py
+++ подготовка к егэ/stepik/практикум по программированию.py
@@ -1,4 +1,10 @@
-import math
-n = 81
-k = 15
-print(math.factorial(n) / (math.factorial(k) * math.factorial(n - k)))
+import itertools
+k, n = map(int, input().split())
+sp = list(i for i in range(0, n))
+res = itertools.permutations(sp, k)
+ans = list()
+for elem in res:
+    if sorted(elem) not in ans:
+        ans.append(sorted(elem))
+for elem in ans:
+    print(*elem)
