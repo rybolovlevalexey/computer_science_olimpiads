6990b5a7baf5dc36de20052266bf9b3e371706af
Rybolovlev Alexey <rybolovlevalexey@yandex.ru>
2022-09-25 18:11:06 +0300

    норм

--- подготовка к егэ/stepik/практикум по программированию.py
+++ подготовка к егэ/stepik/практикум по программированию.py
@@ -1,2 +1,5 @@
-import math
-print(math.factorial(17) / (math.factorial(9) * math.factorial(8)))
+cnt = 0
+for num in range(10**6, 10**7):
+    if sum(map(int, list(str(num)))) <= 47:
+        cnt += 1
+print(cnt)
