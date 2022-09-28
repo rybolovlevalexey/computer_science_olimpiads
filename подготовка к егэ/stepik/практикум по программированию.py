718a1278fb977b4b96030a019d65ffe8fbb43da6
Rybolovlev Alexey <rybolovlevalexey@yandex.ru>
2022-09-28 08:11:17 +0300

    норм

--- подготовка к егэ/stepik/практикум по программированию.py
+++ подготовка к егэ/stepik/практикум по программированию.py
@@ -1,14 +1,13 @@
-import itertools
-def primes():
-    def is_prost(x):
-        for d in range(2, x // 2 + 1):
-            if x % d == 0:
-                return False
-        return True
-    num = 2
-    while True:
-        if is_prost(num) or num == 2 or num == 3:
-            yield num
-        num += 1
-
-print(list(itertools.takewhile(lambda x : x <= 40, primes())))
+n = int(input())
+k = int(input())
+sp = list(i for i in range(1, n + 1))
+n = 1
+while len(sp) > 1:
+    ind = 0
+    while ind < len(sp) and len(sp) > 1:
+        if n % k == 0:
+            del sp[ind]
+        print(sp, n)
+        ind += 1
+        n += 1
+print(*sp)
