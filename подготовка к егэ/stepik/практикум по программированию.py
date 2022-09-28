0c1266047210366d38f277d2a172405fb21a216b
Rybolovlev Alexey <rybolovlevalexey@yandex.ru>
2022-09-28 08:13:59 +0300

    норм

--- подготовка к егэ/stepik/практикум по программированию.py
+++ подготовка к егэ/stepik/практикум по программированию.py
@@ -1,13 +1,14 @@
-n = int(input())
-k = int(input())
-sp = list(i for i in range(1, n + 1))
-n = 1
-while len(sp) > 1:
-    ind = 0
-    while ind < len(sp) and len(sp) > 1:
-        if n % k == 0:
-            del sp[ind]
-        print(sp, n)
-        ind += 1
-        n += 1
-print(*sp)
+st = input().split()
+ans = list()
+sp = list()
+for elem in st:
+    if len(sp) == 0:
+        sp.append(elem)
+    else:
+        if sp[-1] == elem:
+            sp.append(elem)
+        else:
+            ans.append(sp)
+            sp = [elem]
+ans.append(sp)
+print(ans)
