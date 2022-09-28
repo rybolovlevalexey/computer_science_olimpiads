28b80b4af409c7db7596ab4f43f043471fe435e2
Rybolovlev Alexey <rybolovlevalexey@yandex.ru>
2022-09-28 08:34:17 +0300

    норм

--- подготовка к егэ/stepik/практикум по программированию.py
+++ подготовка к егэ/stepik/практикум по программированию.py
@@ -1,10 +1,17 @@
-st = input().split()
-ans = list()
-ans.append([])
-for d in range(len(st) + 1):
-    for s in range(len(st)):
-        if s + d >= len(st):
-            break
-        subset = st[s:s+d+1]
-        ans.append(subset)
-print(ans)
+n = int(input())
+sp = list()
+mx = 0
+flag = True
+for i in range(n):
+    line = list(map(int, input().split()))
+    sp.append(line)
+for i in range(n):
+    if i <= n // 2:
+        line = list(sp[i][:i+1] + sp[i][n-1-i:])
+    else:
+        line = list(sp[i][:n-i] + sp[i][i:])
+    for elem in line:
+        if flag or elem > mx:
+            mx = elem
+            flag = False
+print(mx)
