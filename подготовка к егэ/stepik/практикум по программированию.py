d51357003ce7d4a811c84e41c67167a0bf43e248
Rybolovlev Alexey <rybolovlevalexey@yandex.ru>
2022-10-03 15:55:12 +0300

    норм

--- подготовка к егэ/stepik/практикум по программированию.py
+++ подготовка к егэ/stepik/практикум по программированию.py
@@ -6,14 +6,15 @@ for i in range(n):
     sp.append(line)
     line = list('*' for j in range(n))
     ans.append(line)
-#for d in range(n // 2):
-#    for i in range(d, n - d):
-#        ans[i][n - 1 - d] = sp[0 + d][i]
-#        ans[i][d] = sp[n - 1 - d][i]
-#if n % 2 == 1:
-#    ans[n // 2][n // 2] = sp[n // 2][n // 2]
+for d in range(n // 2):
+    for i in range(d, n - d):
+        ans[i][n - 1 - d] = sp[0 + d][i]
+        ans[i][d] = sp[n - 1 - d][i]
+if n % 2 == 1:
+    ans[n // 2][n // 2] = sp[n // 2][n // 2]
 for d in range(n // 2):
     for i in range(1 + d, n - 1 - d):
         ans[n - 1 - d][n - i - 1] = sp[i][n - 1 - d]
-
-print(*ans, sep='\n')
+        ans[d][n - i - 1] = sp[i][d]
+for elem in ans:
+    print(*elem)
