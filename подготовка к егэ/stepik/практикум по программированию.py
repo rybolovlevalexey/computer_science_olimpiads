0a3e98fc7e01bfd9f51566fc7202f82dba1f5aa8
Rybolovlev Alexey <rybolovlevalexey@yandex.ru>
2022-10-03 16:13:13 +0300

    норм

--- подготовка к егэ/stepik/практикум по программированию.py
+++ подготовка к егэ/stepik/практикум по программированию.py
@@ -1,7 +1,14 @@
 n, m = map(int, input().split())
 num = 1
+sp = list()
 for i in range(n):
-    for j in range(m):
-        print(str(num) + " " * (3 - len(str(num))), end="")
+    line = list("*" for j in range(m))
+    sp.append(line)
+for j in range(m):
+    for i in range(n):
+        sp[i][j] = num
         num += 1
+for elem in sp:
+    for el in elem:
+        print(str(el) + " " * (3 - len(str(el))), end="")
     print()
