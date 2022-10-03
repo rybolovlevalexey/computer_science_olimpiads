5772b6806299b1d474bfa63638d2836187b0119a
Rybolovlev Alexey <rybolovlevalexey@yandex.ru>
2022-10-03 13:02:51 +0300

    норм

--- подготовка к егэ/stepik/практикум по программированию.py
+++ подготовка к егэ/stepik/практикум по программированию.py
@@ -1,37 +1,19 @@
 n = int(input())
-flag = True
-check = None
 sp = list()
+ans = list()
 for i in range(n):
     line = list(map(int, input().split()))
-    if check is None:
-        check = sum(line)
-    if check != sum(line):
-        flag = False
     sp.append(line)
-s1 = 0
-s2 = 0
-for j in range(n):
-    s1 += sp[j][j]
-    s2 += sp[j][n - j - 1]
-if s1 != check or s2 != check:
-    flag = False
-numbers = set()
-for j in range(n):
-    s = 0
-    for i in range(n):
-        s += sp[i][j]
-        numbers.add(sp[i][j])
-    if s != check:
-        flag = False
-if len(numbers) != n**2:
-    flag = False
-if not flag:
-    print("NO")
-else:
-    numbers = sorted(numbers)
-    for i in range(n**2):
-        if numbers[i] != i + 1:
-            flag = False
-            break
-    print("YES" if flag else "NO")
+    line = list('*' for j in range(n))
+    ans.append(line)
+#for d in range(n // 2):
+#    for i in range(d, n - d):
+#        ans[i][n - 1 - d] = sp[0 + d][i]
+#        ans[i][d] = sp[n - 1 - d][i]
+#if n % 2 == 1:
+#    ans[n // 2][n // 2] = sp[n // 2][n // 2]
+for d in range(n // 2):
+    for i in range(1 + d, n - 1 - d):
+        ans[n - 1 - d][n - i - 1] = sp[i][n - 1 - d]
+
+print(*ans, sep='\n')
