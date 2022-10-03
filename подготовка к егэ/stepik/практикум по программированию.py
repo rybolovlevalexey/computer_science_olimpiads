8ee42ea9e2b1d86e32fbd952d84357d5c639c2fc
Rybolovlev Alexey <rybolovlevalexey@yandex.ru>
2022-10-03 12:20:13 +0300

    норм

--- подготовка к егэ/stepik/практикум по программированию.py
+++ подготовка к егэ/stepik/практикум по программированию.py
@@ -1,27 +1,16 @@
-n = int(input())
-if n == 1:
-    print(input())
-else:
-    sp = list()
-    for i in range(n):
-        line = input().split()
-        sp.append(line)
-    for i in range(n // 2):
-        up = sp[i][:]
-        down = sp[n - i - 1][:]
-        left = list()
-        rigth = list()
-        for j in range(i, n - i):
-            left.append(sp[j][i])
-            rigth.append(sp[j][n - i - 1])
-        for j in range(len(left)):
-            sp[i][j + i] = left[len(left) - j - 1]
-            sp[n - i - 1][j + i] = rigth[len(rigth) - j - 1]
-        if i + 1 == n // 2 and n % 2 == 0:
-            break
-        for j in range(1, len(up) - 1):
-            sp[i + j][n - i - 1] = up[j]
-        for j in range(1, len(down) - 1):
-            sp[n - j - 1][i] = down[len(down) - j - 1]
-    for elem in sp:
-        print(*elem, sep=' ')
+sp = list()
+for i in range(8):
+    line = ["." for j in range(8)]
+    sp.append(line)
+st = input()
+letters = {'a': 1, 'b': 2, 'c': 3, 'd': 4, 'e': 5, 'f': 6, 'g': 7, 'h': 8, }
+x = letters[st[0]] - 1
+y = 8 - int(st[1])
+sp[y][x] = "N"
+for i in [-1, -2, 1, 2]:
+    for j in [-1, -2, 1, 2]:
+        if abs(i) != abs(j):
+            if 0 <= x + i <= 7 and 0 <= y + j <= 7:
+                sp[y + j][x + i] = "*"
+for elem in sp:
+    print(*elem)
