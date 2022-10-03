4553d84c7b9eb49cc47b2a8a890c08e8b61ff327
Rybolovlev Alexey <rybolovlevalexey@yandex.ru>
2022-10-03 16:20:50 +0300

    норм

--- подготовка к егэ/stepik/практикум по программированию.py
+++ подготовка к егэ/stepik/практикум по программированию.py
@@ -2,11 +2,32 @@ n = int(input())
 sp = list()
 for i in range(n):
     line = list()
+    flag = False
     for j in range(n):
-        if i == j or i == n - j - 1:
-            line.append(1)
+        if i < n // 2:
+            if i == j:
+                flag = True
+                line.append(1)
+            elif i == n - j - 1:
+                flag = False
+                line.append(1)
+            else:
+                if flag:
+                    line.append(1)
+                else:
+                    line.append(0)
         else:
-            line.append(0)
+            if i == j:
+                flag = False
+                line.append(1)
+            elif i == n - j - 1:
+                flag = True
+                line.append(1)
+            else:
+                if flag:
+                    line.append(1)
+                else:
+                    line.append(0)
     sp.append(line)
 for elem in sp:
     print(*elem, sep="  ")
