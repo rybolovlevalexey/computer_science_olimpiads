eed7d3396b68555e8fdcbbc00bef54deed1c0dc7
Rybolovlev Alexey <rybolovlevalexey@yandex.ru>
2022-10-04 07:47:20 +0300

    норм

--- подготовка к егэ/stepik/практикум по программированию.py
+++ подготовка к егэ/stepik/практикум по программированию.py
@@ -1,33 +1,20 @@
-n = int(input())
+n, m = map(int, input().split())
 sp = list()
 for i in range(n):
-    line = list()
-    flag = False
-    for j in range(n):
-        if i < n // 2:
-            if i == j:
-                flag = True
-                line.append(1)
-            elif i == n - j - 1:
-                flag = False
-                line.append(1)
-            else:
-                if flag:
-                    line.append(1)
-                else:
-                    line.append(0)
-        else:
-            if i == j:
-                flag = False
-                line.append(1)
-            elif i == n - j - 1:
-                flag = True
-                line.append(1)
-            else:
-                if flag:
-                    line.append(1)
-                else:
-                    line.append(0)
+    line = list("*" for j in range(m))
     sp.append(line)
+for j in range(m):
+    if j == 0:
+        k = 1
+    else:
+        k = sp[0][j - 1] + 1
+    for i in range(n):
+        k = k % max(m, n)
+        sp[i][j] = k
+        k += 1
+for i in range(n):
+    for j in range(m):
+        if sp[i][j] == 0:
+            sp[i][j] = max(m, n)
 for elem in sp:
-    print(*elem, sep="  ")
+    print(*elem)
