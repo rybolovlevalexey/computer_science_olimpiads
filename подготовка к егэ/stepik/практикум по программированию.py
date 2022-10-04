57a8dd3a20452952cf88e858142aac4952fe4296
Rybolovlev Alexey <rybolovlevalexey@yandex.ru>
2022-10-04 16:37:22 +0300

    норм

--- подготовка к егэ/stepik/практикум по программированию.py
+++ подготовка к егэ/stepik/практикум по программированию.py
@@ -1,15 +1,23 @@
-n = int(input())
-sp = list()
-flag = True
-for i in range(n):
-    line = list(map(int, input().split()))
-    sp.append(line)
-    if list(sorted(set(line))) != list(i+1 for i in range(n)):
-        flag = False
-for j in range(n):
-    line = set()
-    for i in range(n):
-        line.add(sp[i][j])
-    if list(sorted(line)) != list(i+1 for i in range(n)):
-        flag = False
-print("YES" if flag else "NO")
+st = input()
+d = {"a": 0, "b": 1, "c": 2, "d": 3, "e": 4, "f": 5, "g": 6, "h": 7}
+x = d[st[0]]
+y = 8 - int(st[1])
+sp = list(['.'] * 8 for i in range(8))
+sp[y][x] = 'Q'
+for i in range(8):
+    if i != y:
+        sp[i][x] = '*'
+    if i != x:
+        sp[y][i] = '*'
+
+for i in range(1, 8):
+    if 0 <= x - i <= 7 and 0 <= y - i <= 7:
+        sp[y - i][x - i] = '*'
+    if 0 <= x + i <= 7 and 0 <= y + i <= 7:
+        sp[y + i][x + i] = '*'
+    if 0 <= x + i <= 7 and 0 <= y - i <= 7:
+        sp[y - i][x + i] = '*'
+    if 0 <= x - i <= 7 and 0 <= y + i <= 7:
+        sp[y + i][x - i] = '*'
+for elem in sp:
+    print(*elem)
