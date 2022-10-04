72f155b9360abaa65adfd3fc216c5633cdfbbc21
Rybolovlev Alexey <rybolovlevalexey@yandex.ru>
2022-10-04 15:51:16 +0300

    норм

--- подготовка к егэ/stepik/практикум по программированию.py
+++ подготовка к егэ/stepik/практикум по программированию.py
@@ -1,14 +1,8 @@
 n = int(input())
-if n == 1:
-    print(input())
-else:
-    mx = None
-    for i in range(n):
-        flag = False
-        st = list(map(int, input().split()))
-        for j in range(n):
-            if i == n - j - 1:
-                flag = True
-            if flag and (mx is None or mx < st[j]):
-                mx = st[j]
-    print(mx)
+sp = list(['*'] * n for i in range(n))
+for i in range(n):
+    st = input().split()
+    for j in range(n):
+        sp[j][i] = st[j]
+for elem in sp:
+    print(*elem, sep=' ')
