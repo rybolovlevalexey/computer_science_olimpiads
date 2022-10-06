0c57dc0f3ba770d81486a283a19b9858ea21ed3d
Rybolovlev Alexey <rybolovlevalexey@yandex.ru>
2022-10-06 20:22:41 +0300

    норм

--- подготовка к егэ/stepik/практикум по программированию.py
+++ подготовка к егэ/stepik/практикум по программированию.py
@@ -1,16 +1,5 @@
-n = int(input())
-start = 0
-plus = True
-for i in range(n):
-    num = start
-    for j in range(n):
-        print(num, end=' ')
-        if num == 0 and not plus:
-            plus = True
-        if plus:
-            num += 1
-        else:
-            num -= 1
-    plus = False
-    start += 1
-    print()
+dic = {0: 'zero', 1: 'one', 2: 'two', 3: 'three', 4: 'four', 5: 'five', 6: 'six', 7: 'seven',
+       8: 'eight', 9: 'nine'}
+st = input()
+for elem in st:
+    print(dic[int(elem)], end=' ')
