d7f19c0bf374db306e7eea067f15eda426d73854
Rybolovlev Alexey <rybolovlevalexey@yandex.ru>
2022-10-10 21:52:36 +0300

    норм

--- подготовка к егэ/stepik/практикум по программированию.py
+++ подготовка к егэ/stepik/практикум по программированию.py
@@ -1,19 +1,2 @@
-sp1 = list()
-sp2 = list()
-res = list()
-n, m = map(int, input().split())
-for i in range(n):
-    sp1.append(list(map(int, input().split())))
-input()
-m, k = map(int, input().split())
-for i in range(m):
-    sp2.append(list(map(int, input().split())))
-for i in range(max(m, n, k)):
-    res.append(['*'] * max(m, n, k))
-for i in range(max(n, m)):
-    for j in range(max(m, k)):
-        su = 0
-        for k in range(max(m, k, n)):
-            su += (sp1[i][k] * sp2[k][j])
-        res[i][j] = su
-print(*res, sep='\n')
+for i in range(int(input())):
+    print(len(set(input().lower())))
