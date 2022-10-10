f0521e8e818c5bed75a669b37557a0aab22c90f1
Rybolovlev Alexey <rybolovlevalexey@yandex.ru>
2022-10-10 21:05:32 +0300

    норм

--- подготовка к егэ/stepik/практикум по программированию.py
+++ подготовка к егэ/stepik/практикум по программированию.py
@@ -1,18 +1,21 @@
-n = int(input())
+x1 = int(input())
+y1 = int(input())
+x2 = int(input())
+y2 = int(input())
 sp1 = list()
 sp2 = list()
-for i in range(n):
+for i in range(y1):
     sp1.append(list(map(int, input().split())))
-for i in range(n):
+for i in range(y2):
     sp2.append(list(map(int, input().split())))
 res = list()
-for i in range(n):
-    res.append(['*'] * n)
+for i in range(min(y1, y2)):
+    res.append(['*'] * min(x1, x2))
 flag = True
-for i in range(n):
-    for j in range(n):
+for i in range(min(y1, y2)):
+    for j in range(min(x1, x2)):
         su = 0
-        for k in range(n):
+        for k in range(max(x1, x2)):
             su += (sp1[i][k] * sp2[k][j])
             if flag:
                 print(sp1[i][k], sp2[k][j], su)
