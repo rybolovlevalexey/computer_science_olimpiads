feb16134dff9f2e8ec2feb61fc941c5d2af3be19
Rybolovlev Alexey <rybolovlevalexey@yandex.ru>
2022-10-10 21:30:21 +0300

    норм

--- подготовка к егэ/stepik/практикум по программированию.py
+++ подготовка к егэ/stepik/практикум по программированию.py
@@ -1,26 +1,19 @@
-x1 = int(input())
-y1 = int(input())
-x2 = int(input())
-y2 = int(input())
 sp1 = list()
 sp2 = list()
-for i in range(y1):
-    sp1.append(list(map(int, input().split())))
-for i in range(y2):
-    sp2.append(list(map(int, input().split())))
 res = list()
-for i in range(min(y1, y2)):
-    res.append(['*'] * min(x1, x2))
-flag = True
-for i in range(min(y1, y2)):
-    for j in range(min(x1, x2)):
+n, m = map(int, input().split())
+for i in range(n):
+    sp1.append(list(map(int, input().split())))
+input()
+m, k = map(int, input().split())
+for i in range(m):
+    sp2.append(list(map(int, input().split())))
+for i in range(max(m, n, k)):
+    res.append(['*'] * max(m, n, k))
+for i in range(max(n, m)):
+    for j in range(max(m, k)):
         su = 0
-        for k in range(max(x1, x2)):
+        for k in range(max(m, k, n)):
             su += (sp1[i][k] * sp2[k][j])
-            if flag:
-                print(sp1[i][k], sp2[k][j], su)
-        flag = False
         res[i][j] = su
-        print(res[i][j])
-        su = 0
 print(*res, sep='\n')
