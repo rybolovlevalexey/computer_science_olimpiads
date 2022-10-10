c357ca295c50363b28fd971597de1a3e46a89da0
Rybolovlev Alexey <rybolovlevalexey@yandex.ru>
2022-10-10 08:10:11 +0300

    норм

--- подготовка к егэ/stepik/практикум по программированию.py
+++ подготовка к егэ/stepik/практикум по программированию.py
@@ -1,8 +1,24 @@
-s = input()
-dic = dict()
-for elem in s.split():
-    if elem in dic:
-        dic[elem] += 1
+pets = [('Hatiko', 'Parker', 'Wilson', 50),
+        ('Rusty', 'Josh', 'King', 25),
+        ('Fido', 'John', 'Smith', 28),
+        ('Butch', 'Jake', 'Smirnoff', 18),
+        ('Odi', 'Emma', 'Wright', 18),
+        ('Balto', 'Josh', 'King', 25),
+        ('Barry', 'Josh', 'King', 25),
+        ('Snape', 'Hannah', 'Taylor', 40),
+        ('Horry', 'Martha', 'Robinson', 73),
+        ('Giro', 'Alex', 'Martinez', 65),
+        ('Zooma', 'Simon', 'Nevel', 32),
+        ('Lassie', 'Josh', 'King', 25),
+        ('Chase', 'Martha', 'Robinson', 73),
+        ('Ace', 'Martha', 'Williams', 38),
+        ('Rocky', 'Simon', 'Nevel', 32)]
+
+result = {}
+for elem in pets:
+    dog, *man = elem
+    man = tuple(man)
+    if man in result:
+        result[man].append(dog)
     else:
-        dic[elem] = 1
-print(sorted([key for key in dic.keys() if max(dic.values()) == dic[key]])[0])
+        result[man] = [dog]
