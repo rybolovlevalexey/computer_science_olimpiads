a882827894e0c0a7ede9dd7cbd5100277279361b
Rybolovlev Alexey <rybolovlevalexey@yandex.ru>
2022-10-10 19:57:42 +0300

    норм

--- подготовка к егэ/stepik/практикум по программированию.py
+++ подготовка к егэ/stepik/практикум по программированию.py
@@ -1,15 +1,23 @@
-n, m = map(int, input().split())
-sp = [['*'] * m for i in range(n)]
-start = 1
+n = int(input())
+sp1 = list()
+sp2 = list()
 for i in range(n):
-    num = start
-    for j in range(m):
-        sp[i][j] = num
-        num += 1
-        if num > m:
-            num = 1
-    start += 1
-    if start > m:
-        start = 1
-for elem in sp:
-    print(*elem, sep=' ')
+    sp1.append(list(map(int, input().split())))
+for i in range(n):
+    sp2.append(list(map(int, input().split())))
+res = list()
+for i in range(n):
+    res.append(['*'] * n)
+flag = True
+for i in range(n):
+    for j in range(n):
+        su = 0
+        for k in range(n):
+            su += (sp1[i][k] * sp2[k][j])
+            if flag:
+                print(sp1[i][k], sp2[k][j], su)
+        flag = False
+        res[i][j] = su
+        print(res[i][j])
+        su = 0
+print(*res, sep='\n')
