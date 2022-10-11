875ce4d7a26ef5af654133ce8d5d9102b2867cdd
Rybolovlev Alexey <rybolovlevalexey@yandex.ru>
2022-10-11 11:48:28 +0300

    норм

--- подготовка к егэ/stepik/практикум по программированию.py
+++ подготовка к егэ/stepik/практикум по программированию.py
@@ -1,4 +1,16 @@
 first = set(input().split())
 second = set(input().split())
 third = set(input().split())
-print(*sorted(map(int, first & second - third))[::-1])
+al = first | second | third
+ans = list()
+for elem in al:
+    cnt = 0
+    if elem in first:
+        cnt += 1
+    if elem in second:
+        cnt += 1
+    if elem in third:
+        cnt += 1
+    if cnt <= 2:
+        ans.append(elem)
+print(*sorted(map(int, ans)))
