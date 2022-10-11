a9779ae96048341373f0e2e76a0d24220ce5f352
Rybolovlev Alexey <rybolovlevalexey@yandex.ru>
2022-10-11 17:13:25 +0300

    норм

--- подготовка к егэ/stepik/практикум по программированию.py
+++ подготовка к егэ/stepik/практикум по программированию.py
@@ -6,4 +6,10 @@ for i in range(m):
     math.add(input())
 for i in range(n):
     inf.add(input())
-print(len(math - inf))
+res = len(math) + len(inf) - 2 * len(math & inf)
+print(math, inf)
+print(math & inf)
+if res == 0:
+    print('NO')
+else:
+    print(res)
