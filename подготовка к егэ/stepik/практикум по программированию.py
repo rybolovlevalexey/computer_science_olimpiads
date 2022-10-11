29bd6ad384d3b3295f9eca9d5deced7e8bd5b73e
Rybolovlev Alexey <rybolovlevalexey@yandex.ru>
2022-10-11 16:51:48 +0300

    норм

--- подготовка к егэ/stepik/практикум по программированию.py
+++ подготовка к егэ/stepik/практикум по программированию.py
@@ -1,2 +1,11 @@
-sp = list(map(int, input().split()))
-print(len(sp) - len(set(sp)))
+m = int(input())
+n = int(input())
+sp = set()
+for i in range(m):
+    sp.add(input())
+for i in range(n):
+    town = input()
+    if town in sp:
+        print('YES')
+    else:
+        print('NO')
