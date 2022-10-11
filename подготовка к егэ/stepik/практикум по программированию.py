830033269b081f1bdeb2f054f69428bdded3fb6e
Rybolovlev Alexey <rybolovlevalexey@yandex.ru>
2022-10-11 11:40:12 +0300

    норм

--- подготовка к егэ/stepik/практикум по программированию.py
+++ подготовка к егэ/stepik/практикум по программированию.py
@@ -1,8 +1,1 @@
-n = int(input())
-ans = set()
-for i in range(n):
-    if i == 0:
-        ans = set(input())
-    else:
-        ans = ans & set(input())
-print(*sorted(map(int, ans)) if len(ans) > 0 else '')
+print('YES' if len(set(input()) & set(input())) > 0 else 'NO')
