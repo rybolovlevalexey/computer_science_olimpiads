6868688d24fa51a86260135e9ad8daeaa8be4769
Rybolovlev Alexey <rybolovlevalexey@yandex.ru>
2022-10-11 16:42:25 +0300

    норм

--- подготовка к егэ/stepik/практикум по программированию.py
+++ подготовка к егэ/stepik/практикум по программированию.py
@@ -1,11 +1,2 @@
-from itertools import permutations
-
-num = 53617824
-sp = sorted(map(lambda x: ''.join(x), sorted(permutations(str(num)))))
-cnt = 0
-for elem in sp:
-    if elem == str(num):
-        cnt += 1
-    if 1 <= cnt <= 10:
-        print(elem)
-        cnt += 1
+sp = list(map(int, input().split()))
+print(len(sp) - len(set(sp)))
