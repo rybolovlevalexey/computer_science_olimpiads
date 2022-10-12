9f9a5074e3eddf323ac7549dc18c6a2b325b7f5d
Rybolovlev Alexey <rybolovlevalexey@yandex.ru>
2022-10-12 19:23:47 +0300

    норм

--- подготовка к егэ/stepik/практикум по программированию.py
+++ подготовка к егэ/stepik/практикум по программированию.py
@@ -1,11 +1,17 @@
-n = int(input())
-numbers = dict()
-for i in range(n):
-    number, name = input().split()
-    name = name.lower()
-    if name in numbers:
-        numbers[name.lower()] += [number]
+st = input()
+shifr = dict()
+for elem in st:
+    if elem in shifr:
+        shifr[elem] += 1
     else:
-        numbers[name.lower()] = [number]
-for i in range(int(input())):
-    print(*numbers.get(input().lower(), 'абонент не найден'.split()))
+        shifr[elem] = 1
+n = int(input())
+letters = dict()
+for i in range(n):
+    let, cnt = input().split(': ')
+    cnt = int(cnt)
+    for key, value in shifr.items():
+        if value == cnt:
+            letters[key] = let
+for elem in st:
+    print(letters[elem], end='')
