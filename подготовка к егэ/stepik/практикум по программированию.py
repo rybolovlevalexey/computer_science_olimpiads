84d93963295ea7231bc01c0955d5d6cc6a8f9184
Rybolovlev Alexey <rybolovlevalexey@yandex.ru>
2022-10-12 22:29:11 +0300

    норм

--- подготовка к егэ/stepik/практикум по программированию.py
+++ подготовка к егэ/stepik/практикум по программированию.py
@@ -1,11 +1,7 @@
-emails = {'nosu.edu': ['timyr', 'joseph', 'svetlana.gaeva', 'larisa.mamuk'],
-          'gmail.com': ['ruslan.chaika', 'rustam.mini', 'stepik-best'],
-          'msu.edu': ['apple.fruit', 'beegeek', 'beegeek.school'],
-          'yandex.ru': ['surface', 'google'],
-          'hse.edu': ['tomas-henders', 'cream.soda', 'zivert'],
-          'mail.ru': ['angel.down', 'joanne', 'the.fame.moster']}
-ans = list()
-for key, value in emails.items():
-    for name in value:
-        ans.append(name + '@' + key)
-print(*sorted(ans))
+dnk = {'g': 'c', 'c': 'g', 't': 'a', 'a': 'u'}
+st = input()
+ans = ''
+for elem in st:
+    ans += dnk[elem.lower()]
+ans = ans.upper()
+print(ans)
