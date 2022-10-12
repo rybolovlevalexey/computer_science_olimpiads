8c4693a9b1daf53f23cf3fad7de219334e83a612
Rybolovlev Alexey <rybolovlevalexey@yandex.ru>
2022-10-12 18:57:58 +0300

    норм

--- подготовка к егэ/stepik/практикум по программированию.py
+++ подготовка к егэ/stepik/практикум по программированию.py
@@ -1,9 +1,23 @@
-st = input().split()
-letters = dict()
-for let in st:
-    if let in letters:
-        print(f'{let}_{letters[let]}', end=' ')
-        letters[let] += 1
+st1 = input()
+st2 = input()
+if len(st1) == len(st2):
+    letters = dict()
+    for i in range(len(st1)):
+        if st1[i] in letters:
+            letters[i] += 1
+        else:
+            letters[i] = 1
+        if st2[i] in letters:
+            letters[i] -= 1
+        else:
+            letters[i] = -1
+    flag = True
+    for value in letters.values():
+        if value != 0:
+            flag = False
+    if flag:
+        print('YES')
     else:
-        print(let, end=' ')
-        letters[let] = 1
+        print('NO')
+else:
+    print('NO')
