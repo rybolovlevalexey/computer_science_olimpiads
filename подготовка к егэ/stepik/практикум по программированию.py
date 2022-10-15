31816b1a98cca6e426a11bf1bdae781410eda6e9
Rybolovlev Alexey <rybolovlevalexey@yandex.ru>
2022-10-15 21:50:54 +0300

    норм

--- подготовка к егэ/stepik/практикум по программированию.py
+++ подготовка к егэ/stepik/практикум по программированию.py
@@ -1,7 +1,16 @@
-file = open('numbers.txt')
+file = open('nums.txt')
 sp = file.readlines()
+su = 0
 for line in sp:
     line = line.strip()
-    while '  ' in line:
-        line = line.replace('  ', ' ')
-    print(sum(map(int, line.split())))
+    num = ''
+    for x in line:
+        if x.isdigit():
+            num += x
+        else:
+            if num != '':
+                su += int(num)
+            num = ''
+    if num != '':
+        su += int(num)
+print(su)
