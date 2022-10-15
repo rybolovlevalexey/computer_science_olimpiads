d969f312a931767964390bfe251f18bf8d979821
Rybolovlev Alexey <rybolovlevalexey@yandex.ru>
2022-10-15 09:58:06 +0300

    норм

--- подготовка к егэ/stepik/практикум по программированию.py
+++ подготовка к егэ/stepik/практикум по программированию.py
@@ -1,8 +1,18 @@
-import random
-
-file = open('data.txt', 'w')
-sp = list()
-for i in range(10**8):
-    num = random.randint(1, 10**6)
-    file.write(str(num) + '\n')
-file.close()
+file = open('nums.txt')
+sp = file.readlines()
+if len(sp) > 1:
+    nums = list()
+    for elem in sp:
+        elem = elem.strip()
+        try:
+            elem = int(elem)
+            nums.append(elem)
+        except Exception:
+            continue
+    print(sum(nums))
+else:
+    st = sp[0].strip()
+    while '  ' in st:
+        st = st.replace('  ', ' ')
+    a, b = map(int, st.split())
+    print(a + b)
