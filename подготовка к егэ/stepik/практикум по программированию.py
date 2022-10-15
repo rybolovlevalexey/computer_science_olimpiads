f2b202609464e635e3fffe3aab68a9f6762a7b07
Rybolovlev Alexey <rybolovlevalexey@yandex.ru>
2022-10-15 22:20:03 +0300

    норм

--- подготовка к егэ/stepik/практикум по программированию.py
+++ подготовка к егэ/stepik/практикум по программированию.py
@@ -1,16 +1,16 @@
-file = open('nums.txt')
+file = open('file.txt')
 sp = file.readlines()
-su = 0
-for line in sp:
-    line = line.strip()
-    num = ''
-    for x in line:
-        if x.isdigit():
-            num += x
-        else:
-            if num != '':
-                su += int(num)
-            num = ''
-    if num != '':
-        su += int(num)
-print(su)
+words = 0
+letters = 0
+for elem in sp:
+    while '  ' in elem:
+        elem = elem.replace('  ', ' ')
+    for wor in elem.split():
+        words += 1
+        for let in wor:
+            if 97 <= ord(let.lower()) <= 122:
+                letters += 1
+print('Input file contains:')
+print(f'{letters} letters')
+print(f'{words} words')
+print(f'{len(sp)} lines')
