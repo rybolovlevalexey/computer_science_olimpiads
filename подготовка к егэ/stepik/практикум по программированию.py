8a694f11b0f1136893ac081367a64819e574ac82
Rybolovlev Alexey <rybolovlevalexey@yandex.ru>
2022-10-15 22:40:15 +0300

    норм

--- подготовка к егэ/stepik/практикум по программированию.py
+++ подготовка к егэ/stepik/практикум по программированию.py
@@ -1,16 +1,11 @@
-file = open('file.txt')
-sp = file.readlines()
-words = 0
-letters = 0
-for elem in sp:
-    while '  ' in elem:
-        elem = elem.replace('  ', ' ')
-    for wor in elem.split():
-        words += 1
-        for let in wor:
-            if 97 <= ord(let.lower()) <= 122:
-                letters += 1
-print('Input file contains:')
-print(f'{letters} letters')
-print(f'{words} words')
-print(f'{len(sp)} lines')
+def read_csv():
+    file = open('data.csv')
+    ans = list()
+    keys = file.readline().strip().split(',')
+    for line in file.readlines():
+        line = line.strip().split(',')
+        dline = dict()
+        for i in range(len(keys)):
+            dline[keys[i]] = line[i]
+        ans.append(dline)
+    return ans
