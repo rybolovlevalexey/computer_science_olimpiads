293b685e55bc95e6c50047dfee21f9d8b3bf1eae
Rybolovlev Alexey <rybolovlevalexey@yandex.ru>
2022-10-15 20:38:15 +0300

    норм

--- подготовка к егэ/stepik/практикум по программированию.py
+++ подготовка к егэ/stepik/практикум по программированию.py
@@ -1,7 +1,2 @@
-file = open('prices.txt')
-sp = file.readlines()
-su = 0
-for elem in sp:
-    name, cnt, cost = elem.split('\t')
-    su += int(cnt) * int(cost)
-print(su)
+text = map(lambda x: x.strip(), open('lines.txt').readlines())
+print(*filter(lambda x: len(x) == len(max(text, key=len)), text), sep='\n')
