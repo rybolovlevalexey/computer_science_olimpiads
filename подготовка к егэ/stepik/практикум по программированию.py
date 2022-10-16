cd2a8fa46ca40844419b0856d07e70918af70681
Rybolovlev Alexey <rybolovlevalexey@yandex.ru>
2022-10-16 14:45:13 +0300

    норм

--- подготовка к егэ/stepik/практикум по программированию.py
+++ подготовка к егэ/stepik/практикум по программированию.py
@@ -1,11 +1,10 @@
-def read_csv():
-    file = open('data.csv')
-    ans = list()
-    keys = file.readline().strip().split(',')
-    for line in file.readlines():
-        line = line.strip().split(',')
-        dline = dict()
-        for i in range(len(keys)):
-            dline[keys[i]] = line[i]
-        ans.append(dline)
-    return ans
+file = open('class_scores.txt', 'r')
+file1 = open('new_scores.txt', 'w')
+for line in file.readlines():
+    fam, score = line.strip().split()
+    score = (int(score) + 5)
+    if score > 100:
+        score = 100
+    file1.write(str(fam) + ' ' + str(score) + '\n')
+file.close()
+file1.close()
