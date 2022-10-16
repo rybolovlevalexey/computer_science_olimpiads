4e6bfe9dcf3cde5703c33ed17bb8493b3492beae
Rybolovlev Alexey <rybolovlevalexey@yandex.ru>
2022-10-16 19:15:49 +0300

    норм

--- подготовка к егэ/stepik/практикум по программированию.py
+++ подготовка к егэ/stepik/практикум по программированию.py
@@ -1,24 +1,15 @@
-letters = {'а': 'a', 'к': 'k', 'х': 'h', 'б': 'b', 'л': 'l', 'ц': 'c', 'в': 'v',
-           'м': 'm', 'ч': 'ch', 'г': 'g', 'н': 'n', 'ш': 'sh',
-           'д': 'd', 'о': 'o', 'щ': 'shh', 'е': 'e', 'п': 'p', 'ъ': '*', 'ё': 'jo', 'р': 'r',
-           'ы': 'y', 'ж': 'zh', 'с': 's', 'ь': "'",
-           'з': 'z', 'т': 't', 'э': 'je', 'и': 'i', 'у': 'u', 'ю': 'ju', 'й': 'j',
-           'ф': 'f', 'я': 'ya'}
-
-file = open('cyrillic.txt', encoding='utf-8')
-file1 = open('transliteration.txt', 'w', encoding='utf-8')
-st = file.read().strip()
-ans_sp = list()
-for line in st.split('\n'):
-    ans = ''
-    for elem in line.strip():
-        if elem.lower() in letters:
-            if elem == elem.upper():
-                app = letters[elem.lower()]
-                ans += app[0].upper() + app[1:]
-            else:
-                ans += letters[elem]
-        else:
-            ans += elem
-    ans_sp.append(ans)
-file1.write('\n'.join(ans_sp))
+flag = True
+name = input()
+file = open(name)
+sp = file.read().strip().split('\n')
+for i in range(0, len(sp)):
+    if i == 0 and sp[i][:3] == 'def':
+        print(sp[i].split()[1][:sp[i].split()[1].index('(')])
+        flag = False
+        continue
+    if len(sp[i].strip()) >= 3 and sp[i].strip()[:3] == 'def':
+        if len(sp[i - 1]) == 0 or sp[i - 1].strip()[0] != '#':
+            print(sp[i].split()[1][:sp[i].split()[1].index('(')])
+            flag = False
+if flag:
+    print('Best Programming Team')
