6f904540e23d4d11bd30f15cf84fa32a6cf4f333
Rybolovlev Alexey <rybolovlevalexey@yandex.ru>
2022-10-16 18:24:04 +0300

    норм

--- подготовка к егэ/stepik/практикум по программированию.py
+++ подготовка к егэ/stepik/практикум по программированию.py
@@ -1,1 +1,15 @@
-print(sum(list(map(lambda x: int(x[1:].strip()), open('ledger.txt').readlines()))))
+forbidden_words = open('forbidden_words.txt')
+bad_words = list(map(lambda x: x.strip(), forbidden_words.read().split()))
+name = input()
+file = open(name)
+oldst = file.read()
+st = oldst.lower()
+for elem in bad_words:
+    st = st.replace(elem, '*' * len(elem))
+ans = ''
+for i in range(len(oldst)):
+    if st[i] == '*':
+        ans += st[i]
+    else:
+        ans += oldst[i]
+print(ans)
