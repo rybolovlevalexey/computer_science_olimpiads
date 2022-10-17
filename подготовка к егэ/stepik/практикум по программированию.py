1d358e05cb602748e4a933386aed94c166c57dff
Rybolovlev Alexey <rybolovlevalexey@yandex.ru>
2022-10-17 16:26:17 +0300

    норм

--- подготовка к егэ/stepik/практикум по программированию.py
+++ подготовка к егэ/stepik/практикум по программированию.py
@@ -1,15 +1,12 @@
-flag = True
-name = input()
-file = open(name)
-sp = file.read().strip().split('\n')
-for i in range(0, len(sp)):
-    if i == 0 and sp[i][:3] == 'def':
-        print(sp[i].split()[1][:sp[i].split()[1].index('(')])
-        flag = False
-        continue
-    if len(sp[i].strip()) >= 3 and sp[i].strip()[:3] == 'def':
-        if len(sp[i - 1]) == 0 or sp[i - 1].strip()[0] != '#':
-            print(sp[i].split()[1][:sp[i].split()[1].index('(')])
-            flag = False
-if flag:
-    print('Best Programming Team')
+def evaluate(coefficients, x):
+    ans = 0
+    i = len(coefficients) - 1
+    for elem in coefficients:
+        ans += elem * x**i
+        i -= 1
+    return ans
+
+
+coef = list(map(int, input().split()))
+x = int(input())
+print(evaluate(coef, x))
