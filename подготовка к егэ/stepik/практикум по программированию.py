843c525b3c7bb1fb26a53ec5f71f463b36f96307
Rybolovlev Alexey <rybolovlevalexey@yandex.ru>
2022-10-18 20:34:27 +0300

    норм

--- подготовка к егэ/stepik/практикум по программированию.py
+++ подготовка к егэ/stepik/практикум по программированию.py
@@ -1,7 +1,7 @@
 from decimal import *
 num = Decimal(input())
 print(num.as_tuple().digits)
-if num >= 1:
+if abs(num) >= 1:
     print(max(num.as_tuple().digits) + min(num.as_tuple().digits))
 else:
     print(max(num.as_tuple().digits))
