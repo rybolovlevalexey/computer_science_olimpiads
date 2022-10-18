1504f097f4f70fb57ec4e9fe6a5d08e883d73a0c
Rybolovlev Alexey <rybolovlevalexey@yandex.ru>
2022-10-18 20:01:39 +0300

    норм

--- подготовка к егэ/stepik/практикум по программированию.py
+++ подготовка к егэ/stepik/практикум по программированию.py
@@ -1,9 +1,19 @@
 import random
-sp = list()
-for i in range(int(input())):
-    sp.append(input())
-friends = sp[::-1]
-if len(sp) % 2 == 1:
-    friends[len(sp) // 2], friends[-1] = friends[-1], friends[len(sp) // 2]
-for i in range(len(sp)):
-    print(sp[i], friends[i], sep=' - ')
+n = int(input())
+m = int(input())
+elements = list(i for i in range(2, 10))
+for i in range(97, 123):
+    let = chr(i)
+    if let != 'l' and let.upper() != 'I' and let != 'o':
+        elements.append(let)
+        elements.append(let.upper())
+for i in range(n):
+    ans = set()
+    ans.add(str(random.choice(list(i for i in range(2, 10)))))
+    ans.add(random.choice(list(set(elements).difference(list(i for i in range(2, 10))))).lower())
+    ans.add(random.choice(list(set(elements).difference(list(i for i in range(2, 10))))).upper())
+    random.shuffle(list(ans))
+    ans = set(ans)
+    while len(ans) < m:
+        ans.add(str(random.choice(elements)))
+    print(''.join(ans))
