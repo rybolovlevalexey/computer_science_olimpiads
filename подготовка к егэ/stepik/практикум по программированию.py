8c113271accfb820ace37ebaa76b05e5a488cdf5
Rybolovlev Alexey <rybolovlevalexey@yandex.ru>
2022-10-18 20:33:57 +0300

    норм

--- подготовка к егэ/stepik/практикум по программированию.py
+++ подготовка к егэ/stepik/практикум по программированию.py
@@ -1,19 +1,7 @@
-import random
-n = int(input())
-m = int(input())
-elements = list(i for i in range(2, 10))
-for i in range(97, 123):
-    let = chr(i)
-    if let != 'l' and let.upper() != 'I' and let != 'o':
-        elements.append(let)
-        elements.append(let.upper())
-for i in range(n):
-    ans = set()
-    ans.add(str(random.choice(list(i for i in range(2, 10)))))
-    ans.add(random.choice(list(set(elements).difference(list(i for i in range(2, 10))))).lower())
-    ans.add(random.choice(list(set(elements).difference(list(i for i in range(2, 10))))).upper())
-    random.shuffle(list(ans))
-    ans = set(ans)
-    while len(ans) < m:
-        ans.add(str(random.choice(elements)))
-    print(''.join(ans))
+from decimal import *
+num = Decimal(input())
+print(num.as_tuple().digits)
+if num >= 1:
+    print(max(num.as_tuple().digits) + min(num.as_tuple().digits))
+else:
+    print(max(num.as_tuple().digits))
