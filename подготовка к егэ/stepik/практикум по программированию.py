1835b5677cb8bc12437b5b791a7af3ec53ec405c
Rybolovlev Alexey <rybolovlevalexey@yandex.ru>
2022-10-20 21:25:57 +0300

    норм

--- подготовка к егэ/stepik/практикум по программированию.py
+++ подготовка к егэ/stepik/практикум по программированию.py
@@ -1,12 +1,18 @@
 from fractions import Fraction
 
-def fack(x):
-    res = 1
-    for j in range(1, x + 1):
-        res *= j
-    return res
+def prost(x, y):
+    if max(x, y) % min(x, y) == 0 and min(x, y) != 1:
+        return False
+    for d in range(2, min(x, y) // 2 + 1):
+        if x % d == 0 and y % d == 0:
+            return False
+    return True
+
 n = int(input())
-ans = Fraction(0)
-for i in range(1, n + 1):
-    ans += Fraction(1, fack(i))
-print(ans)
+for ch in range(n - 1, 0, -1):
+    zn = n - ch
+    if ch > zn:
+        continue
+    if prost(zn, ch):
+        print(Fraction(ch, zn))
+        break
