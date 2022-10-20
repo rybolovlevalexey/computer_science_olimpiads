f01ebe5cb27bd6301e774ece208f5e5584281888
Rybolovlev Alexey <rybolovlevalexey@yandex.ru>
2022-10-20 21:19:08 +0300

    норм

--- подготовка к егэ/stepik/практикум по программированию.py
+++ подготовка к егэ/stepik/практикум по программированию.py
@@ -1,7 +1,12 @@
 from fractions import Fraction
 
+def fack(x):
+    res = 1
+    for j in range(1, x + 1):
+        res *= j
+    return res
 n = int(input())
 ans = Fraction(0)
 for i in range(1, n + 1):
-    ans += Fraction(1, i**2)
+    ans += Fraction(1, fack(i))
 print(ans)
