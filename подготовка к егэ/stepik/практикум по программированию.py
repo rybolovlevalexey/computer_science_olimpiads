e2536e026db79215023ee7fd7ea278942f3a3a24
Rybolovlev Alexey <rybolovlevalexey@yandex.ru>
2022-10-22 18:31:37 +0300

    норм

--- подготовка к егэ/stepik/практикум по программированию.py
+++ подготовка к егэ/stepik/практикум по программированию.py
@@ -1,3 +1,6 @@
-def pretty_print(data, side='-', delimiter='|'):
-    ans = delimiter + " " + f" {delimiter} ".join(list(map(str, data))) + " " + delimiter
-    print(" " + side * (len(ans) - 2) + " \n" + ans + "\n" + " " + side * (len(ans) - 2) + " ")
+n = int(input())
+sp = list()
+for i in range(n):
+    sp.append(list(map(int, input().split('.'))))
+
+print(*map(lambda x: '.'.join(map(lambda y: str(y), x)), sorted(sp, key=lambda x: x[0] * 256**3 + x[1] * 256**2 + x[2] * 256 + x[3])), sep='\n')
