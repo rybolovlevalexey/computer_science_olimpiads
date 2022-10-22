370b6bd4213aa659c339f580e228d340e3febbea
Rybolovlev Alexey <rybolovlevalexey@yandex.ru>
2022-10-22 18:56:51 +0300

    норм

--- подготовка к егэ/stepik/практикум по программированию.py
+++ подготовка к егэ/stepik/практикум по программированию.py
@@ -1,6 +1,15 @@
 n = int(input())
 sp = list()
 for i in range(n):
-    sp.append(list(map(int, input().split('.'))))
-
-print(*map(lambda x: '.'.join(map(lambda y: str(y), x)), sorted(sp, key=lambda x: x[0] * 256**3 + x[1] * 256**2 + x[2] * 256 + x[3])), sep='\n')
+    sp.append(input())
+sp = sorted(sp)
+gem = dict()
+for elem in sp:
+    s = 0
+    for j in elem.upper():
+        s += ord(j) - ord("A")
+    gem[elem] = s
+for value in set(sorted(gem.values())):
+    for key in gem:
+        if gem[key] == value:
+            print(key)
