2c1aadfbf4e22b77c94f54893fff4ce186f9c0f6
Rybolovlev Alexey <rybolovlevalexey@yandex.ru>
2022-10-24 10:14:12 +0300

    норм

--- подготовка к егэ/stepik/практикум по программированию.py
+++ подготовка к егэ/stepik/практикум по программированию.py
@@ -1,10 +1,25 @@
-sp = list(map(int, input().split()))
-for i in range(len(sp) - 1):
-    for j in range(i, len(sp) - 1):
-        a = sum(list(map(int, list(str(sp[j])))))
-        b = sum(list(map(int, list(str(sp[j + 1])))))
-        if a > b:
-            sp[j], sp[j + 1] = sp[j + 1], sp[j]
-        if a == b and sp[j] > sp[j + 1]:
-            sp[j], sp[j + 1] = sp[j + 1], sp[j]
-print(*sp)
+data = [['Tokyo', 35676000, 'primary'],
+        ['New York', 19354922, 'nan'],
+        ['Mexico City', 19028000, 'primary'],
+        ['Mumbai', 18978000, 'admin'],
+        ['Sao Paulo', 18845000, 'admin'],
+        ['Delhi', 15926000, 'admin'],
+        ['Shanghai', 14987000, 'admin'],
+        ['Kolkata', 14787000, 'admin'],
+        ['Los Angeles', 12815475, 'nan'],
+        ['Dhaka', 12797394, 'primary'],
+        ['Buenos Aires', 12795000, 'primary'],
+        ['Karachi', 12130000, 'admin'],
+        ['Cairo', 11893000, 'primary'],
+        ['Rio de Janeiro', 11748000, 'admin'],
+        ['Osaka', 11294000, 'admin'],
+        ['Beijing', 11106000, 'primary'],
+        ['Manila', 11100000, 'primary'],
+        ['Moscow', 10452000, 'primary'],
+        ['Istanbul', 10061000, 'admin'],
+        ['Paris', 9904000, 'primary']]
+
+data = filter(lambda x: x[1] > 10000000 and x[2] == 'primary', data)
+data = map(lambda x: x[0], data)
+data = sorted(data)
+print(f"Cities: {', '.join(data)}")
