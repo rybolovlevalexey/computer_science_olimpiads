00341f5ada0e45b12a1e1147b1eeede66cdac79a
Rybolovlev Alexey <rybolovlevalexey@yandex.ru>
2022-10-24 08:50:56 +0300

    норм

--- подготовка к егэ/stepik/практикум по программированию.py
+++ подготовка к егэ/stepik/практикум по программированию.py
@@ -1,15 +1,6 @@
+athletes = [('Дима', 10, 130, 35), ('Тимур', 11, 135, 39), ('Руслан', 9, 140, 33), ('Рустам', 10, 128, 30), ('Амир', 16, 170, 70), ('Рома', 16, 188, 100), ('Матвей', 17, 168, 68), ('Петя', 15, 190, 90)]
 n = int(input())
-sp = list()
-for i in range(n):
-    sp.append(input())
-sp = sorted(sp)
-gem = dict()
-for elem in sp:
-    s = 0
-    for j in elem.upper():
-        s += ord(j) - ord("A")
-    gem[elem] = s
-for value in set(sorted(gem.values())):
-    for key in gem:
-        if gem[key] == value:
-            print(key)
+answer = {4: sorted(athletes, key=lambda x: x[-1]), 3: sorted(athletes, key=lambda x: x[-2]),
+          2: sorted(athletes, key=lambda x: x[1]), 1: sorted(athletes, key=lambda x: x[0])}
+for elem in answer[n]:
+    print(*elem)
