ef253f54403b8f9a603b7ccb1cd4e7e3a38853a4
Rybolovlev Alexey <rybolovlevalexey@yandex.ru>
2022-10-24 08:55:33 +0300

    норм

--- подготовка к егэ/stepik/практикум по программированию.py
+++ подготовка к егэ/stepik/практикум по программированию.py
@@ -1,6 +1,7 @@
-athletes = [('Дима', 10, 130, 35), ('Тимур', 11, 135, 39), ('Руслан', 9, 140, 33), ('Рустам', 10, 128, 30), ('Амир', 16, 170, 70), ('Рома', 16, 188, 100), ('Матвей', 17, 168, 68), ('Петя', 15, 190, 90)]
-n = int(input())
-answer = {4: sorted(athletes, key=lambda x: x[-1]), 3: sorted(athletes, key=lambda x: x[-2]),
-          2: sorted(athletes, key=lambda x: x[1]), 1: sorted(athletes, key=lambda x: x[0])}
-for elem in answer[n]:
-    print(*elem)
+import math
+
+num = int(input())
+func = input()
+answer = {'квадрат': lambda x: x**2, 'куб': lambda x: x**3, 'корень': lambda x: x**0.5,
+          'модуль': lambda x: abs(x), 'синус': lambda x: math.sin(x)}
+print(answer[func](num))
