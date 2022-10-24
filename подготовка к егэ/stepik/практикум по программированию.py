5475f9647c0f50221aadd9cd3724d34ed8d32ffa
Rybolovlev Alexey <rybolovlevalexey@yandex.ru>
2022-10-24 08:59:22 +0300

    норм

--- подготовка к егэ/stepik/практикум по программированию.py
+++ подготовка к егэ/stepik/практикум по программированию.py
@@ -1,7 +1,3 @@
-import math
-
-num = int(input())
-func = input()
-answer = {'квадрат': lambda x: x**2, 'куб': lambda x: x**3, 'корень': lambda x: x**0.5,
-          'модуль': lambda x: abs(x), 'синус': lambda x: math.sin(x)}
-print(answer[func](num))
+sp = list(map(int, input().split()))
+sp = sorted(sp, key=lambda x: sum(map(int, list(str(x)))))
+print(*sp)
