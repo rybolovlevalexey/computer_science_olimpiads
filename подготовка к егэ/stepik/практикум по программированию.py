c4a98ef3f4a5bbc9cd61d775c0da9002fc6f57ba
Rybolovlev Alexey <rybolovlevalexey@yandex.ru>
2022-10-24 09:03:34 +0300

    норм

--- подготовка к егэ/stepik/практикум по программированию.py
+++ подготовка к егэ/stepik/практикум по программированию.py
@@ -1,3 +1,10 @@
 sp = list(map(int, input().split()))
-sp = sorted(sp, key=lambda x: sum(map(int, list(str(x)))))
+for i in range(len(sp) - 1):
+    for j in range(i, len(sp) - 1):
+        a = sum(list(map(int, list(str(sp[j])))))
+        b = sum(list(map(int, list(str(sp[j + 1])))))
+        if a > b:
+            sp[j], sp[j + 1] = sp[j + 1], sp[j]
+        if a == b and sp[j] > sp[j + 1]:
+            sp[j], sp[j + 1] = sp[j + 1], sp[j]
 print(*sp)
