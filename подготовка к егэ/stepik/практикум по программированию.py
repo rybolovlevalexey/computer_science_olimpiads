630ec2960b090e7c784f72f7a7fafc47f1361b0f
Rybolovlev Alexey <rybolovlevalexey@yandex.ru>
2022-10-24 15:54:17 +0300

    норм

--- подготовка к егэ/stepik/практикум по программированию.py
+++ подготовка к егэ/stepik/практикум по программированию.py
@@ -1,12 +1,11 @@
-sp_x = list()
-sp_y = list()
-sp_z = list()
-for i in range(3):
-    dots = list(map(float, input().split()))
-    if i == 0:
-        sp_x = dots
-    elif i == 1:
-        sp_y = dots
-    else:
-        sp_z = dots
-print(all(list(d <= 4 for d in map(lambda x: x[0]**2 + x[1]**2 + x[2]**2, zip(sp_x, sp_y, sp_z)))))
+a = int(input())
+b = int(input())
+for num in range(a, b + 1):
+    sp = list()
+    for d in str(num):
+        if d == '0':
+            sp.append(False)
+        else:
+            sp.append(num % int(d) == 0)
+    if all(sp):
+        print(num, end=' ')
