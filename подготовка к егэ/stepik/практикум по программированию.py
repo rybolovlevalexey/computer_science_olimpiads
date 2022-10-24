6845e0e824512c6c5f7423531a21637f1280624b
Rybolovlev Alexey <rybolovlevalexey@yandex.ru>
2022-10-24 13:38:07 +0300

    норм

--- подготовка к егэ/stepik/практикум по программированию.py
+++ подготовка к егэ/stepik/практикум по программированию.py
@@ -1,25 +1,8 @@
-data = [['Tokyo', 35676000, 'primary'],
-        ['New York', 19354922, 'nan'],
-        ['Mexico City', 19028000, 'primary'],
-        ['Mumbai', 18978000, 'admin'],
-        ['Sao Paulo', 18845000, 'admin'],
-        ['Delhi', 15926000, 'admin'],
-        ['Shanghai', 14987000, 'admin'],
-        ['Kolkata', 14787000, 'admin'],
-        ['Los Angeles', 12815475, 'nan'],
-        ['Dhaka', 12797394, 'primary'],
-        ['Buenos Aires', 12795000, 'primary'],
-        ['Karachi', 12130000, 'admin'],
-        ['Cairo', 11893000, 'primary'],
-        ['Rio de Janeiro', 11748000, 'admin'],
-        ['Osaka', 11294000, 'admin'],
-        ['Beijing', 11106000, 'primary'],
-        ['Manila', 11100000, 'primary'],
-        ['Moscow', 10452000, 'primary'],
-        ['Istanbul', 10061000, 'admin'],
-        ['Paris', 9904000, 'primary']]
-
-data = filter(lambda x: x[1] > 10000000 and x[2] == 'primary', data)
-data = map(lambda x: x[0], data)
-data = sorted(data)
-print(f"Cities: {', '.join(data)}")
+countries = ['Russia', 'USA', 'UK', 'Germany', 'France', 'India']
+capitals = ['Moscow', 'Washington', 'London', 'Berlin', 'Paris', 'Delhi']
+population = [145_934_462, 331_002_651, 80_345_321, 67_886_011, 65_273_511, 1_380_004_385]
+for i in range(len(countries)):
+    cap = capitals[i]
+    country = countries[i]
+    popul = population[i]
+    print(f'{cap} is the capital of {country}, population equal {popul} people.')
