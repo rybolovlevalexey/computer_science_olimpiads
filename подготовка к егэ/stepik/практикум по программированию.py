26bdbbccce89f64116e8901281c8729d42ce98ae
Rybolovlev Alexey <rybolovlevalexey@yandex.ru>
2022-10-25 13:20:49 +0300

    норм

--- подготовка к егэ/stepik/практикум по программированию.py
+++ подготовка к егэ/stepik/практикум по программированию.py
@@ -1,11 +1,11 @@
-from datetime import timedelta
-file = open('logfile.txt', encoding='UTF-8')
-output = open('output.txt', 'w', encoding='UTF-8')
-for line in file.readlines():
-    name, start, finish = line.strip().split(', ')
-    t_start = timedelta(hours=int(start.split(':')[0]), minutes=int(start.split(':')[1]))
-    t_finish = timedelta(hours=int(finish.split(':')[0]), minutes=int(finish.split(':')[1]))
-    delta = t_finish - t_start
-    if delta >= timedelta(hours=1):
-        output.write(name + '\n')
-output.close()
+import random
+
+n = 10**6
+cnt = 0
+for i in range(n):
+    x = random.uniform(-1, 1)
+    y = random.uniform(-1, 1)
+    if x**2 + y**2 <= 1:
+        cnt += 1
+s = (cnt / n) * 4
+print(s)
