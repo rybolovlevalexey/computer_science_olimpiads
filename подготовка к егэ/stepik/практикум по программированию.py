f4bd4eb79b6751f63b902aa261290ac5d4ca9e0a
Rybolovlev Alexey <rybolovlevalexey@yandex.ru>
2022-10-25 12:33:56 +0300

    норм

--- подготовка к егэ/stepik/практикум по программированию.py
+++ подготовка к егэ/stepik/практикум по программированию.py
@@ -1,9 +1,26 @@
-n = int(input())
+file = open('goats.txt')
+output = open('answer.txt', 'w')
+colors = set()
+file.readline()
+goats = dict()
+flag = False
+cnt = 0
+for elem in file.readlines():
+    elem = elem.strip()
+    if elem == 'GOATS':
+        flag = True
+        continue
+    if not flag:
+        colors.add(elem.split()[0])
+        goats[elem.split()[0]] = 0
+    else:
+        goats[elem.split()[0]] += 1
+    if flag:
+        cnt += 1
 ans = list()
-for i in range(n):
-    k = int(input())
-    sp = list()
-    for j in range(k):
-        sp.append(input().split()[1] == '5')
-    ans.append(any(sp))
-print('YES' if all(ans) else 'NO')
+for key, value in goats.items():
+    if value / cnt > 0.07:
+        ans.append(f'{key} goat')
+ans = sorted(ans)
+output.write('\n'.join(ans))
+output.close()
