6e4acfb1e8a0ddfef286324146c93de69c90c7cc
Rybolovlev Alexey <rybolovlevalexey@yandex.ru>
2022-12-17 12:14:50 +0300

    Алгоритм Барроуза-Виллера

--- Универ/дз по дискре.py
+++ Универ/дз по дискре.py
@@ -1,28 +1,27 @@
-import fractions
-p1 = fractions.Fraction(1, 13) * 10
-for i in range(9):
-    num = fractions.Fraction(48 - i, 51 - i)
-    p1 *= num
+from pprint import pprint
 
-p2 = fractions.Fraction(1, 13) * fractions.Fraction(1, 17) * 45
-for i in range(8):
-    num = fractions.Fraction(48 - i, 50 - i)
-    p2 *= num
-
-p3 = fractions.Fraction(1, 13) * fractions.Fraction(1, 17) * fractions.Fraction(1, 25) * 120
-for i in range(7):
-    num = fractions.Fraction(48 - i, 49 - i)
-    p3 *= num
-
-p4 = fractions.Fraction(1, 13) * fractions.Fraction(1, 17) * fractions.Fraction(1, 25) * 210
-p4 *= fractions.Fraction(1, 49)
-
-print(p1, p2, p3, p4)
-print(p1, 2*p2, 3*p3, 4*p4)
-ans = p1 + 2*p2 + 3*p3 + 4*p4
-print(ans)
-
-ans2 = p1 + 4*p2 + 9*p3 + 16*p4
-print(ans2)
-res = ans2 - ans*ans
-print(res)
+st = st1 = "алексей"
+sp = list()
+sp.append(st1)
+for i in range(len(st) - 1):
+    st1 = st1[1:] + st1[0]
+    sp.append(st1)
+sp = sorted(sp)
+bwt = "".join(tuple(elem[-1] for elem in sp))
+pos = list(i for i in range(len(sp)) if sp[i] == st)[0]
+print(bwt)
+ans = list()
+for i in range(len(st)):
+    line = list()
+    for j in range(len(st)):
+        if j + 1 == len(st):
+            line.append(bwt[i])
+        else:
+            line.append("*")
+    ans.append(line)
+ans = sorted(ans)
+for k in range(len(st) - 1):
+    for i in range(len(st)):  # проход по строкам
+        ans[i][len(st) - 2 - k] = bwt[i]
+    ans = sorted(ans)
+pprint(ans)
