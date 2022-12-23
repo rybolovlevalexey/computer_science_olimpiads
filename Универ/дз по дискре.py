48fee26aa2af373690002132621c94826f01ff35
Rybolovlev Alexey <rybolovlevalexey@yandex.ru>
2022-12-23 09:29:55 +0300

    Алгоритм Хаффмана

--- Универ/дз по дискре.py
+++ Универ/дз по дискре.py
@@ -1,27 +1,16 @@
-from pprint import pprint
-
-st = st1 = "алексей"
-sp = list()
-sp.append(st1)
-for i in range(len(st) - 1):
-    st1 = st1[1:] + st1[0]
-    sp.append(st1)
-sp = sorted(sp)
-bwt = "".join(tuple(elem[-1] for elem in sp))
-pos = list(i for i in range(len(sp)) if sp[i] == st)[0]
-print(bwt)
-ans = list()
-for i in range(len(st)):
-    line = list()
-    for j in range(len(st)):
-        if j + 1 == len(st):
-            line.append(bwt[i])
+st = "АлексейДокажите, что энтропия монетки принимает наибольшее значение для правильной монетки.Рыболовлев"
+st = st.lower()
+letters = dict()
+for elem in st:
+    if elem.isalpha():
+        if elem in letters:
+            letters[elem] += 1
         else:
-            line.append("*")
-    ans.append(line)
-ans = sorted(ans)
-for k in range(len(st) - 1):
-    for i in range(len(st)):  # проход по строкам
-        ans[i][len(st) - 2 - k] = bwt[i]
-    ans = sorted(ans)
-print(''.join(ans[pos]))
+            letters[elem] = 1
+keys = sorted(letters, key=lambda x: letters[x])
+print(letters)
+code = {'с': '0000000', 'ж': '0000001', 'э': '0000010', 'ш': '0000011', 'з': '0000100', 'ы': '0000101', 'й': '000011', 'д': '000100', 'ч': '000101', 'я': '000110', 'б': '000111', 'ь': '01000', 'п': '01001', 'м': '01010', 'в': '01011', 'к': '1000', 'р': '1001', 'а': '0110', 'л': '0111', 'т': '0010', 'н': '0011', 'о': '101', 'и': '110', 'е': '111'}
+ans = ""
+for let in st:
+    ans += code.get(let, "")
+print(ans)
